696ffd8917c7ea5d776a5d8be026a06fb2f2acb6
Benoit Sigoure <tsuna@stumbleupon.com>
2010-11-20 13:33:45 -0800

    Fix an off-by-one in a comparison when adding new data points.
    This was causing the following error:
      AssertionError: Can't grow IncomingDataPoints(...) larger.
    When trying to add another data point exactly 600 seconds into an
    existing row.
    
    Bug reported by Michael Glaesemann.
    
    Change-Id: I038b151f98a1f85ab47619958688f7e227f349e4

--- src/core/IncomingDataPoints.java
+++ src/core/IncomingDataPoints.java
@@ -177,7 +177,7 @@ final class IncomingDataPoints implements WritableDataPoints {
         throw new IllegalArgumentException("New timestamp=" + timestamp
             + " is less than previous=" + last_ts
             + " when trying to add value=" + value + " to " + this);
-      } else if (timestamp - base_time > Const.MAX_TIMESPAN) {
+      } else if (timestamp - base_time >= Const.MAX_TIMESPAN) {
         // Need to start a new row as we've exceeded Const.MAX_TIMESPAN.
         // We force the starting timestamp to be on a MAX_TIMESPAN boundary
         // so that all TSDs create rows with the same base time.  Otherwise
@@ -236,7 +236,7 @@ final class IncomingDataPoints implements WritableDataPoints {
     // We can't have more than 1 value per second, so MAX_TIMESPAN values.
     final int new_size = Math.min(size * 2, Const.MAX_TIMESPAN);
     if (new_size == size) {
-      throw new AssertionError("Can't grow " + this + " larger.");
+      throw new AssertionError("Can't grow " + this + " larger than " + size);
     }
     values = Arrays.copyOf(values, new_size);
     qualifiers = Arrays.copyOf(qualifiers, new_size);
