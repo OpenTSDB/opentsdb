7212c72e56fd913341df63c1244d8bd03cfa1cb8
Benoit Sigoure <tsuna@stumbleupon.com>
2010-11-23 22:48:01 -0800

    Fix another off-by-one in a comparison.
    A RowSeq can hold up to 2^12 = 4096 data points (one every second for
    4096 consecutive seconds).  So the maximum time delta we can allow is
    4095 not 4096, the comparison should have been strict.
    
    This bug was causing the TSD to sometimes discard data points when
    reading from HBase (the write path was unaffected).  When this was
    happening, the TSD was logging an ERROR message like this:
      RowSeq: new timestamp = 1290564600 is < previous=1290568395 in addRow
      with kv=KeyValue(...)
    The error is misleading as the new timestamp wasn't the one given in the
    error message, it was that value + 4096 (so in the example above, the
    data point the TSD is rejecting is at timestamp = 1290568696) because of
    the 12-bit integer overflow caused by the off-by-one.
    
    Change-Id: Iec35e08c66a6f7050e248f6610e646f7f411f0ae

--- src/core/RowSeq.java
+++ src/core/RowSeq.java
@@ -139,8 +139,8 @@ final class RowSeq implements DataPoints {
       qualifiers[index] = qualifier;
       values[index] = extractLValue(qualifier, kv);
       if (index > 0 && timestamp(index - 1) >= timestamp(index)) {
-        LOG.error("new timestamp = " + timestamp(index)
-                  + " is < previous=" + timestamp(index -1)
+        LOG.error("new timestamp = " + timestamp(index) + " (index=" + index
+                  + ") is < previous=" + timestamp(index - 1)
                   + " in addRow with kv=" + kv + " in row=" + row);
         // Undo what we've done so far.
         qualifiers = old_qualifiers;
@@ -160,7 +160,7 @@ final class RowSeq implements DataPoints {
    * different {@link RowSeq} instances.
    */
   static boolean canTimeDeltaFit(final long time_delta) {
-    return time_delta <= 1 << (Short.SIZE - Const.FLAG_BITS);
+    return time_delta < 1 << (Short.SIZE - Const.FLAG_BITS);
   }
 
   /**
