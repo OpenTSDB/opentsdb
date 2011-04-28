1c3d536df047699f9811b21b19714573547cd404
Aravind Gottipati <aravind@stumbleupon.com>
2011-04-28 13:53:15 -0700

    Don't allocate unnecessary memory when validating names.
    Closes #8.
    
    Change-Id: I3f5fee32b90446b5e0120946792f46fb17b9830c

--- src/core/Tags.java
+++ src/core/Tags.java
@@ -283,7 +283,9 @@ public final class Tags {
     if (s == null) {
       throw new IllegalArgumentException("Invalid " + what + ": null");
     }
-    for (char c : s.toCharArray()) {
+    final int n = s.length();
+    for (int i = 0; i < n; i++) {
+      final char c = s.charAt(i);
       if (!(('a' <= c && c <= 'z')
             || ('A' <= c && c <= 'Z')
             || ('0' <= c && c <= '9')
