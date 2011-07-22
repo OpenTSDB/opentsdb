37dd3dfe3becc3cefb5eb37ecaa012105756cd46
Benoit Sigoure <tsuna@stumbleupon.com>
2011-07-22 15:55:52 -0700

    Add a safety check.
    Change-Id: Id8a6a7ef31bd98c7559de79e7aba660dd1618ed6

--- src/core/SpanGroup.java
+++ src/core/SpanGroup.java
@@ -754,6 +754,9 @@ final class SpanGroup implements DataPoints {
                              ? Double.longBitsToDouble(values[prev])
                              : values[prev]);
           final long x1 = timestamps[prev] & TIME_MASK;
+          assert x0 > x1: ("Next timestamp (" + x0 + ") is supposed to be "
+            + " strictly greater than the previous one (" + x1 + "), but it's"
+            + " not.  this=" + this);
           final double r = (y0 - y1) / (x0 - x1);
           //LOG.debug("Rate for " + y1 + " @ " + x1
           //          + " -> " + y0 + " @ " + x0 + " => " + r);
