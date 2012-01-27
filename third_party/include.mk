e0b636a1fffbedee1fb36decf697f1c62017861d
clarsen <clarsen@llnw.com>
2012-01-26 21:04:14 -0500

    JSON, JSONP and JSON-RPC Error helper classes wrapped around Jackson for use in OpenTSDB. Somewhat similar to MJ's work on #85

--- third_party/include.mk
+++ third_party/include.mk
@@ -29,3 +29,4 @@ include third_party/powermock/include.mk
 include third_party/slf4j/include.mk
 include third_party/suasync/include.mk
 include third_party/zookeeper/include.mk
+include third_party/jackson/include.mk
