123e74c7ed25fe3c26c6517fdee931ddcf10d2e4
Benoit Sigoure <tsuna@stumbleupon.com>
2012-02-05 19:46:53 -0800

    Base copyright on an AUTHORS file.
    This is to be consistent with OpenTSDB and many other open-source projects.
    
    Change-Id: If213a42bf69b7f0a67d2b81efb86869cf7dcd327

--- src/Bytes.java
+++ src/Bytes.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010  StumbleUpon, Inc.  All rights reserved.
+ * Copyright (C) 2010-2012  The Async HBase Authors.  All rights reserved.
  * This file is part of Async HBase.
  *
  * Redistribution and use in source and binary forms, with or without
