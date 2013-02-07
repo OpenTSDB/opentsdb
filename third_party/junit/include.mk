bc88c49e1f684f62169cb83dfe59d32fe2a21248
Pierre Riteau <priteau@ci.uchicago.edu>
2013-02-07 15:12:00 +0000

    Update download URL for junit
    Kent Beck has transfered ownership of the repo to junit-team yesterday.

--- third_party/junit/include.mk
+++ third_party/junit/include.mk
@@ -15,7 +15,7 @@
 
 JUNIT_VERSION := 4.10
 JUNIT := third_party/junit/junit-$(JUNIT_VERSION).jar
-JUNIT_BASE_URL := http://cloud.github.com/downloads/KentBeck/junit
+JUNIT_BASE_URL := https://github.com/downloads/junit-team/junit
 
 $(JUNIT): $(JUNIT).md5
 	set dummy "$(JUNIT_BASE_URL)" "$(JUNIT)"; shift; $(FETCH_DEPENDENCY)
