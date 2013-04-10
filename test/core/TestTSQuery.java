5f6edb0d4aff62b82d33cdb4c279e7fc5047be9f
clarsen <clarsen@euphoriaaudio.com>
2013-04-09 20:36:16 -0400

    Add TSQuery.java for parsing and storing state information about timeseries queries Add TSSubQuery.java Add query validation methods to parse the requested timestamps and aggregators Add unit tests for Timeseries Query classes
    Signed-off-by: Chris Larsen <clarsen@euphoriaaudio.com>

---
+++ test/core/TestTSQuery.java
@@ -0,0 +1,112 @@
+// This file is part of OpenTSDB.
+// Copyright (C) 2013  The OpenTSDB Authors.
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 2.1 of the License, or (at your
+// option) any later version.  This program is distributed in the hope that it
+// will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+// General Public License for more details.  You should have received a copy
+// of the GNU Lesser General Public License along with this program.  If not,
+// see <http://www.gnu.org/licenses/>.
+package net.opentsdb.core;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ TSQuery.class })
+public final class TestTSQuery {
+
+  @Test
+  public void constructor() {
+    assertNotNull(new TSQuery());
+  }
+
+  @Test
+  public void validate() {
+    TSQuery q = this.getMetricForValidate();
+    q.validateAndSetQuery();
+    assertEquals(1356998400000L, q.startTime());
+    assertEquals(1356998460000L, q.endTime());
+    assertEquals("sys.cpu.0", q.getQueries().get(0).getMetric());
+    assertEquals("*", q.getQueries().get(0).getTags().get("host"));
+    assertEquals("lga", q.getQueries().get(0).getTags().get("dc"));
+    assertEquals(Aggregators.SUM, q.getQueries().get(0).aggregator());
+    assertEquals(Aggregators.AVG, q.getQueries().get(0).downsampler());
+    assertEquals(300, q.getQueries().get(0).downsampleInterval());
+  }
+  
+  @Test (expected = IllegalArgumentException.class)
+  public void validateNullStart() {
+    TSQuery q = this.getMetricForValidate();
+    q.setStart(null);
+    q.validateAndSetQuery();
+  }
+  
+  @Test (expected = IllegalArgumentException.class)
+  public void validateEmptyStart() {
+    TSQuery q = this.getMetricForValidate();
+    q.setStart("");
+    q.validateAndSetQuery();
+  }
+  
+  @Test (expected = IllegalArgumentException.class)
+  public void validateInvalidStart() {
+    TSQuery q = this.getMetricForValidate();
+    q.setStart("Not a timestamp at all");
+    q.validateAndSetQuery();
+  }
+  
+  @Test
+  public void validateNullEnd() {
+    TSQuery q = this.getMetricForValidate();
+    q.setEnd(null);
+    q.validateAndSetQuery();
+    assertEquals(System.currentTimeMillis(), q.endTime());
+  }
+  
+  @Test
+  public void validateEmptyEnd() {    
+    PowerMockito.mockStatic(System.class);
+    when(System.currentTimeMillis()).thenReturn(1357300800000L);
+    TSQuery q = this.getMetricForValidate();
+    q.setEnd("");
+    q.validateAndSetQuery();
+    assertEquals(1357300800000L, q.endTime());
+  }
+  
+  @Test (expected = IllegalArgumentException.class)
+  public void validateNullQueries() {
+    TSQuery q = this.getMetricForValidate();
+    q.setQueries(null);
+    q.validateAndSetQuery();
+  }
+  
+  @Test (expected = IllegalArgumentException.class)
+  public void validateEmptyQueries() {
+    TSQuery q = this.getMetricForValidate();
+    q.setQueries(new ArrayList<TSSubQuery>());
+    q.validateAndSetQuery();
+  }
+  
+  private TSQuery getMetricForValidate() {
+    final TSQuery query = new TSQuery();
+    query.setStart("1356998400");
+    query.setEnd("1356998460");
+    final ArrayList<TSSubQuery> subs = new ArrayList<TSSubQuery>(1);
+    subs.add(TestTSSubQuery.getMetricForValidate());
+    query.setQueries(subs);
+    return query;
+  }
+}
---
+++ test/core/TestTSSubQuery.java
@@ -0,0 +1,136 @@
+// This file is part of OpenTSDB.
+// Copyright (C) 2013  The OpenTSDB Authors.
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 2.1 of the License, or (at your
+// option) any later version.  This program is distributed in the hope that it
+// will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+// General Public License for more details.  You should have received a copy
+// of the GNU Lesser General Public License along with this program.  If not,
+// see <http://www.gnu.org/licenses/>.
+package net.opentsdb.core;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.junit.Test;
+
+public final class TestTSSubQuery {
+
+  @Test
+  public void constructor() {
+    assertNotNull(new TSSubQuery());
+  }
+
+  @Test
+  public void validate() {
+    TSSubQuery sub = getMetricForValidate();
+    sub.validateAndSetQuery();
+    assertEquals("sys.cpu.0", sub.getMetric());
+    assertEquals("*", sub.getTags().get("host"));
+    assertEquals("lga", sub.getTags().get("dc"));
+    assertEquals(Aggregators.SUM, sub.aggregator());
+    assertEquals(Aggregators.AVG, sub.downsampler());
+    assertEquals(300, sub.downsampleInterval());
+  }
+  
+  @Test
+  public void validateTS() {
+    TSSubQuery sub = getMetricForValidate();
+    sub.setMetric(null);
+    ArrayList<String> tsuids = new ArrayList<String>(1);
+    tsuids.add("ABCD");
+    sub.setTsuids(tsuids);
+    sub.validateAndSetQuery();
+    assertNotNull(sub.getTsuids());
+    assertEquals("*", sub.getTags().get("host"));
+    assertEquals("lga", sub.getTags().get("dc"));
+    assertEquals(Aggregators.SUM, sub.aggregator());
+    assertEquals(Aggregators.AVG, sub.downsampler());
+    assertEquals(300, sub.downsampleInterval());
+  }
+  
+  @Test
+  public void validateNoDS() {
+    TSSubQuery sub = getMetricForValidate();
+    sub.setDownsample(null);
+    sub.validateAndSetQuery();
+    assertEquals("sys.cpu.0", sub.getMetric());
+    assertEquals("*", sub.getTags().get("host"));
+    assertEquals("lga", sub.getTags().get("dc"));
+    assertEquals(Aggregators.SUM, sub.aggregator());
+    assertNull(sub.downsampler());
+    assertEquals(0, sub.downsampleInterval());
+  }
+  
+  @Test (expected = IllegalArgumentException.class)
+  public void validateNullAgg() {
+    TSSubQuery sub = getMetricForValidate();
+    sub.setAggregator(null);
+    sub.validateAndSetQuery();
+  }
+  
+  @Test (expected = IllegalArgumentException.class)
+  public void validateEmptyAgg() {
+    TSSubQuery sub = getMetricForValidate();
+    sub.setAggregator("");
+    sub.validateAndSetQuery();
+  }
+  
+  @Test (expected = IllegalArgumentException.class)
+  public void validateBadAgg() {
+    TSSubQuery sub = getMetricForValidate();
+    sub.setAggregator("Notanagg");
+    sub.validateAndSetQuery();
+  }
+  
+  @Test (expected = IllegalArgumentException.class)
+  public void validateNoMetricOrTsuids() {
+    TSSubQuery sub = getMetricForValidate();
+    sub.setMetric(null);
+    sub.setTsuids(null);
+    sub.validateAndSetQuery();
+  }
+  
+  @Test (expected = IllegalArgumentException.class)
+  public void validateNoMetricOrEmptyTsuids() {
+    TSSubQuery sub = getMetricForValidate();
+    sub.setMetric(null);
+    sub.setTsuids(new ArrayList<String>());
+    sub.validateAndSetQuery();
+  }
+  
+  @Test (expected = IllegalArgumentException.class)
+  public void validateBadDS() {
+    TSSubQuery sub = getMetricForValidate();
+    sub.setDownsample("bad");
+    sub.validateAndSetQuery();
+  }
+  
+  /**
+   * Sets up an object with good, common values for testing the validation
+   * function with an "m" type query (no tsuids). Each test can "set" the 
+   * method it wants to fool with and call .validateAndSetQuery()
+   * <b>Warning:</b> This method is also shared by {@link TestTSQuery} so be
+   * careful if you change any values
+   * @return A sub query object
+   */
+  public static TSSubQuery getMetricForValidate() {
+    final TSSubQuery sub = new TSSubQuery();
+    sub.setAggregator("sum");
+    sub.setDownsample("5m-avg");
+    sub.setMetric("sys.cpu.0");
+    sub.setRate(false);
+    final HashMap<String, String> tags = new HashMap<String, String>();
+    tags.put("host", "*");
+    tags.put("dc", "lga");
+    sub.setTags(tags);
+    return sub;
+  }
+}
