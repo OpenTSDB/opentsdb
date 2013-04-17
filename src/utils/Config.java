c361f4ab1de58f3613b686f11b758c7901283e10
clarsen <clarsen@euphoriaaudio.com>
2013-04-16 20:44:27 -0400

    Add tsd.search.enable and tsd.search.plugin config option defaults Add tsd.core.plugin_path config default
    Signed-off-by: Chris Larsen <clarsen@euphoriaaudio.com>

--- src/utils/Config.java
+++ src/utils/Config.java
@@ -288,6 +288,9 @@ public class Config {
     default_map.put("tsd.network.keep_alive", "true");
     default_map.put("tsd.network.reuse_address", "true");
     default_map.put("tsd.core.auto_create_metrics", "false");
+    default_map.put("tsd.core.plugin_path", "");
+    default_map.put("tsd.search.enable", "false");
+    default_map.put("tsd.search.plugin", "");
     default_map.put("tsd.storage.flush_interval", "1000");
     default_map.put("tsd.storage.hbase.data_table", "tsdb");
     default_map.put("tsd.storage.hbase.uid_table", "tsdb-uid");
