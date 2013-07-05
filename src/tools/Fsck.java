5d08cd6bb7c409c679bda52074afd0844bb88864
clarsen <clarsen@euphoriaaudio.com>
2013-07-05 10:37:59 -0400

    Modify Fsck to support millisecond timestamps
    Signed-off-by: Chris Larsen <clarsen@euphoriaaudio.com>

--- src/tools/Fsck.java
+++ src/tools/Fsck.java
@@ -13,6 +13,9 @@
 package net.opentsdb.tools;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
 
 import com.stumbleupon.async.Callback;
 import com.stumbleupon.async.Deferred;
@@ -28,8 +31,8 @@ import org.hbase.async.PutRequest;
 import org.hbase.async.Scanner;
 
 import net.opentsdb.core.Const;
-import net.opentsdb.core.IllegalDataException;
 import net.opentsdb.core.Internal;
+import net.opentsdb.core.Internal.Cell;
 import net.opentsdb.core.Query;
 import net.opentsdb.core.TSDB;
 import net.opentsdb.utils.Config;
@@ -98,6 +101,11 @@ final class Fsck {
           this.kv = kv;
         }
 
+        public DeleteOutOfOrder(final byte[] key, final byte[] family, 
+            final byte[] qualifier) {
+          this.kv = new KeyValue(key, family, qualifier, new byte[0]);
+        }
+        
         public Deferred<Object> call(final Object arg) {
           return client.delete(new DeleteRequest(table, kv.key(),
                                                  kv.family(), kv.qualifier()));
@@ -108,6 +116,24 @@ final class Fsck {
         }
       }
 
+    /**
+     * Internal class used for examining data points in a row to determine if
+     * we have any duplicates. Can then be used to delete the duplicate columns.
+     */
+    final class DP {
+      
+      long stored_timestamp;
+      byte[] qualifier;
+      boolean compacted;
+      
+      DP(final long stored_timestamp, final byte[] qualifier, 
+          final boolean compacted) {
+        this.stored_timestamp = stored_timestamp;
+        this.qualifier = qualifier;
+        this.compacted = compacted;
+      }
+    }
+    
     int errors = 0;
     int correctable = 0;
 
@@ -125,9 +151,15 @@ final class Fsck {
       final Bytes.ByteMap<Seen> seen = new Bytes.ByteMap<Seen>();
       final Scanner scanner = Internal.getScanner(query);
       ArrayList<ArrayList<KeyValue>> rows;
+      
+      // store every data point for the row in here 
+      final TreeMap<Long, ArrayList<DP>> previous = 
+        new TreeMap<Long, ArrayList<DP>>();
       while ((rows = scanner.nextRows().joinUninterruptibly()) != null) {
         for (final ArrayList<KeyValue> row : rows) {
           rowcount++;
+          previous.clear();
+          
           // Take a copy of the row-key because we're going to zero-out the
           // timestamp and use that as a key in our `seen' map.
           final byte[] key = row.get(0).key().clone();
@@ -156,79 +188,54 @@ final class Fsck {
               LOG.error("Invalid qualifier, must be on 2 bytes or more.\n\t"
                         + kv);
               continue;
-            } else if (qual.length > 2) {
-              if (qual.length % 2 != 0) {
-                errors++;
-                LOG.error("Invalid qualifier for a compacted row, length ("
-                          + qual.length + ") must be even.\n\t" + kv);
-              }
+            } else if (qual.length % 2 != 0) {
+              // likely an annotation or other object
+              // TODO - validate annotations
+              continue;
+            } else if (qual.length >= 4 && !Internal.inMilliseconds(qual[0])) {
+              // compacted row
               if (value[value.length - 1] != 0) {
                 errors++;
-                LOG.error("The last byte of the value should be 0.  Either"
+                LOG.error("The last byte of a compacted should be 0.  Either"
                           + " this value is corrupted or it was written by a"
                           + " future version of OpenTSDB.\n\t" + kv);
                 continue;
               }
-              // Check all the compacted values.
-              short last_delta = -1;
-              short val_idx = 0;  // Where are we in `value'?
-              boolean ooo = false;  // Did we find out of order data?
-              for (int i = 0; i < qual.length; i += 2) {
-                final short qualifier = Bytes.getShort(qual, i);
-                final short delta = (short) ((qualifier & 0xFFFF)
-                                             >>> Internal.FLAG_BITS);
-                if (delta <= last_delta) {
-                  ooo = true;
-                } else {
-                  last_delta = delta;
+              
+              // add every cell in the compacted column to the previously seen
+              // data point tree so that we can scan for duplicate timestamps
+              final ArrayList<Cell> cells = Internal.extractDataPoints(kv); 
+              for (Cell cell : cells) {
+                final long ts = cell.timestamp(base_time);
+                ArrayList<DP> dps = previous.get(ts);
+                if (dps == null) {
+                  dps = new ArrayList<DP>(1);
+                  previous.put(ts, dps);
                 }
-                val_idx += (qualifier & Internal.LENGTH_MASK) + 1;
+                dps.add(new DP(kv.timestamp(), kv.qualifier(), true));
               }
-              prev.setTimestamp(base_time + last_delta);
-              prev.kv = kv;
-              // Check we consumed all the bytes of the value.  The last byte
-              // is metadata, so it's normal that we didn't consume it.
-              if (val_idx != value.length - 1) {
-                errors++;
-                LOG.error("Corrupted value: consumed " + val_idx
-                          + " bytes, but was expecting to consume "
-                          + (value.length - 1) + "\n\t" + kv);
-              } else if (ooo) {
-                final KeyValue ordered;
-                try {
-                  ordered = Internal.complexCompact(kv);
-                } catch (IllegalDataException e) {
-                  errors++;
-                  LOG.error("Two or more values in a compacted cell have the"
-                            + " same time delta but different values.  "
-                            + e.getMessage() + "\n\t" + kv);
-                  continue;
-                }
-                errors++;
-                correctable++;
-                if (fix) {
-                  client.put(new PutRequest(table, ordered.key(),
-                                            ordered.family(),
-                                            ordered.qualifier(),
-                                            ordered.value()))
-                    .addCallbackDeferring(new DeleteOutOfOrder(kv));
-                } else {
-                  LOG.error("Two or more values in a compacted cell are"
-                            + " out of order within that cell.\n\t" + kv);
-                }
-              }
-              continue;  // We done checking a compacted value.
-            } // else: qualifier is on 2 bytes, it's an individual value.
-            final short qualifier = Bytes.getShort(qual);
-            final short delta = (short) ((qualifier & 0xFFFF) >>> Internal.FLAG_BITS);
-            final long timestamp = base_time + delta;
+              
+              // TODO - validate the compaction
+              continue;
+            } // else: qualifier is on 2 or 4 bytes, it's an individual value.
+
+            final long timestamp = 
+              Internal.getTimestampFromQualifier(qual, base_time);
+            ArrayList<DP> dps = previous.get(timestamp);
+            if (dps == null) {
+              dps = new ArrayList<DP>(1);
+              previous.put(timestamp, dps);
+            }
+            dps.add(new DP(kv.timestamp(), kv.qualifier(), false));
+            
             if (value.length > 8) {
               errors++;
               LOG.error("Value more than 8 byte long with a 2-byte"
                         + " qualifier.\n\t" + kv);
             }
             // TODO(tsuna): Don't hardcode 0x8 / 0x3 here.
-            if ((qualifier & (0x8 | 0x3)) == (0x8 | 0x3)) {  // float | 4 bytes
+            if (qual.length == 2 && 
+                Internal.getFlagsFromQualifier(qual) == (0x8 | 0x3)) {  // float | 4 bytes
               // The qualifier says the value is on 4 bytes, and the value is
               // on 8 bytes, then the 4 MSBs must be 0s.  Old versions of the
               // code were doing this.  It's kinda sad.  Some versions had a
@@ -261,51 +268,85 @@ final class Fsck {
                           + " bytes.\n\t" + kv);
               }
             }
-            if (timestamp <= prev.timestamp()) {
+          }
+
+          // scan for dupes
+          for (Map.Entry<Long, ArrayList<DP>> time_map : previous.entrySet()) {
+            if (time_map.getValue().size() < 2) {
+              continue;
+            }
+            
+            // for now, delete the non-compacted dupes
+            int compacted = 0;
+            long earliest_value = Long.MAX_VALUE;
+            for (DP dp : time_map.getValue()) {
+              if (dp.compacted) {
+                compacted++;
+              }
+              if (dp.stored_timestamp < earliest_value) {
+                earliest_value = dp.stored_timestamp;
+              }
+            }
+            
+            // if there are more than one compacted columns with the same
+            // timestamp, something went pear shaped and we need more work to
+            // figure out what to do
+            if (compacted > 1) {
+              errors++;
+              buf.setLength(0);
+              buf.append("More than one compacted column had a value for the same timestamp: ")
+                 .append("timestamp: (")
+                 .append(time_map.getKey())
+                 .append(")\n");
+              for (DP dp : time_map.getValue()) {
+                buf.append("    ")
+                   .append(Arrays.toString(dp.qualifier))
+                   .append("\n");
+              }
+              LOG.error(buf.toString());
+            } else {
               errors++;
               correctable++;
               if (fix) {
-                final byte[] newkey = kv.key().clone();
-                // Fix the timestamp in the row key.
-                final long new_base_time = (timestamp - (timestamp % Const.MAX_TIMESPAN));
-                Bytes.setInt(newkey, (int) new_base_time, metric_width);
-                final short newqual = (short) ((timestamp - new_base_time) << Internal.FLAG_BITS
-                                               | (qualifier & Internal.FLAGS_MASK));
-                final DeleteOutOfOrder delooo = new DeleteOutOfOrder(kv);
-                if (timestamp < prev.timestamp()) {
-                  client.put(new PutRequest(table, newkey, kv.family(),
-                                            Bytes.fromShort(newqual), value))
-                    // Only delete the offending KV once we're sure that the new
-                    // KV has been persisted in HBase.
-                    .addCallbackDeferring(delooo);
+                if (compacted < 1) {
+                  // keep the earliest value
+                  boolean matched = false;
+                  for (DP dp : time_map.getValue()) {
+                    if (dp.stored_timestamp == earliest_value && !matched) {
+                      matched = true;
+                      continue;
+                    }
+                    final DeleteOutOfOrder delooo = 
+                      new DeleteOutOfOrder(row.get(0).key(), 
+                          "t".getBytes(), dp.qualifier);
+                    delooo.call(null);
+                  }
                 } else {
-                  // We have two data points at exactly the same timestamp.
-                  // This can happen when only the flags differ.  This is
-                  // typically caused by one data point being an integer and
-                  // the other being a floating point value.  In this case
-                  // we just delete the duplicate data point and keep the
-                  // first one we saw.
-                  delooo.call(null);
+                  // keep the compacted value
+                  for (DP dp : time_map.getValue()) {
+                    if (dp.compacted) {
+                      continue;
+                    }
+                    
+                    final DeleteOutOfOrder delooo = 
+                      new DeleteOutOfOrder(row.get(0).key(),
+                          "t".getBytes(), dp.qualifier);
+                    delooo.call(null);
+                  }
                 }
               } else {
                 buf.setLength(0);
-                buf.append(timestamp < prev.timestamp()
-                           ? "Out of order data.\n\t"
-                           : "Duplicate data point with different flags.\n\t")
-                  .append(timestamp)
-                  .append(" (").append(DumpSeries.date(timestamp))
-                  .append(") @ ").append(kv).append("\n\t");
-                DumpSeries.formatKeyValue(buf, tsdb, kv, base_time);
-                buf.append("\n\t  was found after\n\t").append(prev.timestamp)
-                  .append(" (").append(DumpSeries.date(prev.timestamp))
-                  .append(") @ ").append(prev.kv).append("\n\t");
-                DumpSeries.formatKeyValue(buf, tsdb, prev.kv,
-                                          Bytes.getUnsignedInt(prev.kv.key(), metric_width));
+                buf.append("More than one column had a value for the same timestamp: ")
+                   .append("timestamp: (")
+                   .append(time_map.getKey())
+                   .append(")\n");
+                for (DP dp : time_map.getValue()) {
+                  buf.append("    ")
+                     .append(Arrays.toString(dp.qualifier))
+                     .append("\n");
+                }
                 LOG.error(buf.toString());
               }
-            } else {
-              prev.setTimestamp(timestamp);
-              prev.kv = kv;
             }
           }
         }
@@ -331,24 +372,24 @@ final class Fsck {
    * The last data point we've seen for a particular time series.
    */
   private static final class Seen {
-    /** A 32-bit unsigned integer that holds a UNIX timestamp in seconds.  */
-    private int timestamp;
+    /** A 32-bit unsigned integer that holds a UNIX timestamp in milliseconds.  */
+    private long timestamp;
     /** The raw data point (or points if the KV contains more than 1).  */
     KeyValue kv;
 
     private Seen(final long timestamp, final KeyValue kv) {
-      this.timestamp = (int) timestamp;
+      this.timestamp = timestamp;
       this.kv = kv;
     }
 
     /** Returns the UNIX timestamp (in seconds) as a 32-bit unsigned int.  */
     public long timestamp() {
-      return timestamp & 0x00000000FFFFFFFFL;
+      return timestamp;
     }
 
     /** Updates the UNIX timestamp (in seconds) with a 32-bit unsigned int.  */
     public void setTimestamp(final long timestamp) {
-      this.timestamp = (int) timestamp;
+      this.timestamp = timestamp;
     }
   }
 
