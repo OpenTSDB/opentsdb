2a30c389138e3997c42585bd81f5b94a08b501b6
clarsen <clarsen@euphoriaaudio.com>
2013-07-22 19:58:36 -0400

    Fix TSDB.addPoint() unit tests for variable length encoding
    Signed-off-by: Chris Larsen <clarsen@euphoriaaudio.com>

--- test/core/TestTSDB.java
+++ test/core/TestTSDB.java
@@ -345,9 +345,9 @@ public final class TestTSDB {
     tsdb.addPoint("sys.cpu.user", 1356998400, 42, tags).joinUninterruptibly();
     final byte[] row = new byte[] { 0, 0, 1, 0x50, (byte) 0xE2, 0x27, 0, 
         0, 0, 1, 0, 0, 1};
-    final byte[] value = storage.getColumn(row, new byte[] { 0, 7 });
+    final byte[] value = storage.getColumn(row, new byte[] { 0, 0 });
     assertNotNull(value);
-    assertEquals(42, Bytes.getLong(value));
+    assertEquals(42, value[0]);
   }
   
   @Test
@@ -361,9 +361,9 @@ public final class TestTSDB {
     }
     final byte[] row = new byte[] { 0, 0, 1, 0x50, (byte) 0xE2, 0x27, 0, 
         0, 0, 1, 0, 0, 1};
-    final byte[] value = storage.getColumn(row, new byte[] { 0, 7 });
+    final byte[] value = storage.getColumn(row, new byte[] { 0, 0 });
     assertNotNull(value);
-    assertEquals(1, Bytes.getLong(value));
+    assertEquals(1, value[0]);
     assertEquals(50, storage.numColumns(row));
   }
   
@@ -376,9 +376,9 @@ public final class TestTSDB {
     final byte[] row = new byte[] { 0, 0, 1, 0x50, (byte) 0xE2, 0x27, 0, 
         0, 0, 1, 0, 0, 1};
     final byte[] value = storage.getColumn(row, new byte[] { (byte) 0xE0, 
-        (byte) 0xF7 });
+        (byte) 0xF0 });
     assertNotNull(value);
-    assertEquals(42, Bytes.getLong(value));
+    assertEquals(42, value[0]);
   }
   
   @Test
@@ -390,9 +390,9 @@ public final class TestTSDB {
     tsdb.addPoint("sys.cpu.user", 1356998400, 24, tags).joinUninterruptibly();
     final byte[] row = new byte[] { 0, 0, 1, 0x50, (byte) 0xE2, 0x27, 0, 
         0, 0, 1, 0, 0, 1};
-    final byte[] value = storage.getColumn(row, new byte[] { 0, 7 });
+    final byte[] value = storage.getColumn(row, new byte[] { 0, 0 });
     assertNotNull(value);
-    assertEquals(24, Bytes.getLong(value));
+    assertEquals(24, value[0]);
   }
   
   @Test (expected = NoSuchUniqueName.class)
@@ -491,10 +491,10 @@ public final class TestTSDB {
     tsdb.addPoint("sys.cpu.user", 1356998400, 42.5F, tags).joinUninterruptibly();
     final byte[] row = new byte[] { 0, 0, 1, 0x50, (byte) 0xE2, 0x27, 0, 
         0, 0, 1, 0, 0, 1};
-    byte[] value = storage.getColumn(row, new byte[] { 0, 7 });
+    byte[] value = storage.getColumn(row, new byte[] { 0, 0 });
     assertEquals(2, storage.numColumns(row));
     assertNotNull(value);
-    assertEquals(42, Bytes.getLong(value));
+    assertEquals(42, value[0]);
     value = storage.getColumn(row, new byte[] { 0, 11 });
     assertNotNull(value);
     // should have 7 digits of precision
