724691e7fd59fe934e36e9b488a895f53f55ebde
clarsen <clarsen@euphoriaaudio.com>
2013-07-31 18:22:11 -0400

    Add setters to RateOptions for JSON de/serialization as well as some javadocs
    Signed-off-by: Chris Larsen <clarsen@euphoriaaudio.com>

--- src/core/RateOptions.java
+++ src/core/RateOptions.java
@@ -20,6 +20,7 @@ package net.opentsdb.core;
  * <p/>
  * These options will only be utilized if the query is for a rate calculation
  * and if the "counter" options is set to true.
+ * @since 2.0
  */
 public class RateOptions {
   public static final long DEFAULT_RESET_VALUE = 0;
@@ -29,21 +30,21 @@ public class RateOptions {
    * values are counters and thus non-zero, always increasing and wrap around at
    * some maximum
    */
-  private final boolean counter;
+  private boolean counter;
 
   /**
    * If calculating a rate of change over a metric that is a counter, then this
    * value specifies the maximum value the counter will obtain before it rolls
    * over. This value will default to Long.MAX_VALUE.
    */
-  private final long counter_max;
+  private long counter_max;
 
   /**
    * Specifies the the rate change value which, if exceeded, will be considered
    * a data anomaly, such as a system reset of the counter, and the rate will be
    * returned as a zero value for a given data point.
    */
-  private final long reset_value;
+  private long reset_value;
 
   /**
    * Ctor
@@ -71,18 +72,36 @@ public class RateOptions {
     this.reset_value = reset_value;
   }
   
+  /** @return Whether or not the counter flag is set */
   public boolean isCounter() {
     return counter;
   }
 
+  /** @return The counter max value */
   public long getCounterMax() {
     return counter_max;
   }
 
+  /** @return The optional reset value for anomaly suppression */
   public long getResetValue() {
     return reset_value;
   }
 
+  /** @param counter Whether or not the time series should be considered counters */
+  public void setIsCounter(boolean counter) {
+    this.counter = counter;
+  }
+  
+  /** @param counter_max The value at which counters roll over */
+  public void setCounterMax(long counter_max) {
+    this.counter_max = counter_max;
+  }
+  
+  /** @param reset_value A difference that may be an anomaly so suppress it */
+  public void setResetValue(long reset_value) {
+    this.reset_value = reset_value;
+  }
+  
   /**
    * Generates a String version of the rate option instance in a format that 
    * can be utilized in a query.
