11f5d0af65fe17f9c3af1181e4b2c0f24bd786e4
Randy Kaelber <rkaelber@llnw.com>
2013-09-06 11:26:33 -0700

    First version that unifies TSDB protocol specific code and the lower level infrastructure code.

--- tools/tsdbtool.py
+++ tools/tsdbtool.py
@@ -13,7 +13,7 @@ import json
 import StringIO
 from copy import deepcopy
 
-
+DEBUG=True
 
 
 # 3rd party libraries and tools
@@ -24,6 +24,23 @@ version = '0.2.0'
 
 custom_headers = {'User-Agent': '%s/%s' % (product_name, version)}
 
+# headers needed for a given content protocol
+protocol_header_map = {
+                'wwwform': {'Content-Type':'x-www-form-urlencoded',
+                           },
+                'json':    {'Content-Type':'application/json',
+                            'Accept':'text/plain,application/json',
+                           },
+                None:      {},
+               }
+
+
+def get_protocol_headers(protocol):
+    """Returns the HTTP headers needed for the supplied protocol.
+"""
+    if not protocol_header_map.has_key(protocol):
+        raise ValueError("Protocol '%s' is unsupported." % protocol)
+    return protocol_header_map[protocol]
 
 def my_ctime(t, timetype='local', format='%Y-%m-%d %H:%M:%S'):
     if timetype == 'local':
@@ -103,33 +120,48 @@ there is none, assume http.
             pass
 
 
-    def _post(self, url, data):
+    def _post(self, url, data, protocol=None):
+        """Internal HTTP POST method.
+    
+    url - URL to post to.
+
+    data - The data to post. Can be a dictionary or string.
+
+    protocol - protocol used to communicate to remote server which may change
+
+"""
         self._last_use = time.time()
         response = None
-        print "DEBUG: POST %s data: %s" % (url, data) 
+        headers = get_protocol_headers(protocol)
+        if DEBUG: print "DEBUG: POST %s data:\n'%s'\nheaders: %s" % (url, data, headers)
         try:
             response = self._connection.post(url, data,
-                                             timeout = self._options['timeout']
+                                             timeout = self._options['timeout'],
+                                             headers=headers,
                                             )
-        except requests.exceptions.RequestException:
-            logger.error("Post failed") # TODO
+        except requests.exceptions.RequestException as e:
+            logger.error("Post failed: %s" % repr(e)) 
             self._is_up = False 
+            raise e
         else:
             self._is_up = True
             self._last_success = self._last_use
         return response
 
-    def _get(self, url):
+    def _get(self, url, protocol=None):
         self._last_use = time.time()
         response = None
-        print "DEBUG: GET %s" % url
+        headers = get_protocol_headers(protocol)
+        if DEBUG: print "DEBUG: GET %s" % url
         try:
             response = self._connection.get(url,
                                             timeout = self._options['timeout'],
+                                            headers=headers,
                                            )
-        except requests.exceptions.RequestException:
-            logger.error("Get failed") # TODO
+        except requests.exceptions.RequestException as e:
+            logger.error("Get failed: %s" % repr(e)) 
             self._is_up = False
+            raise e
         else:
             self._is_up = True
             self._last_success = self._last_use
@@ -147,16 +179,17 @@ to define it.
         response = self._get('%s/' % (self._host))
         return response
 
-    def post(self, query, data):
+    def post(self, query, data, protocol=None):
         if not self._connection:
             self._connect()
-        response = self._post('%s/%s' % (self._host, query), data)
+        response = self._post('%s/%s' % (self._host, query), data, 
+                              protocol=protocol)
         return response        
 
-    def get(self, query):
+    def get(self, query, protocol=None):
         if not self._connection:
             self._connect()
-        response = self._get('%s/%s' % (self._host, query))
+        response = self._get('%s/%s' % (self._host, query), protocol)
         return response
 
     def is_available(self, force=False):
@@ -169,7 +202,8 @@ a reconnect try even if we're in the retry interval.
         #if we're still within the retry interval, we're down.
 
         now = time.time()
-        if (now - self._last_use) < self._options['retry-interval']:
+        if not force and ((now - self._last_use) < 
+                          self._options['retry-interval']):
             return False
 
         # enough time has elapsed. Let's try again
@@ -224,7 +258,7 @@ imply higher priority."
 """),
 
 "timeout":
-    (float, 15.0,
+    (float, 300.0,
     """A value that determines how long to wait for a TCP connection, in seconds.
 """),
 "balance":
@@ -381,7 +415,8 @@ Returns the host connection for the host.
         return self._hostconns[ret_host]
 
 
-    def _run_http_verb(self, verb, query, data=None, retry=None):
+    def _run_http_verb(self, verb, query, data=None, protocol=None,
+                       retry=None):
         verb = verb.upper()
         if verb not in ('GET','POST'):
             raise ValueError("Unsupported HTTP verb '%s'" % verb)
@@ -397,50 +432,28 @@ Returns the host connection for the host.
             if not node:
                 continue
             if verb == 'GET':
-                response = node.get(query)
+                response = node.get(query, protocol=protocol)
             elif verb == 'POST':
-                response = node.post(query, data)
+                response = node.post(query, data=data, protocol=protocol)
             else:
                 raise ValueError("Didn't we check %s already?" % verb)
         return response
 
         
-    def get_from_pool(self, query, retry=None):
+    def get_from_pool(self, query, protocol=None, retry=None):
         """Run a get against TSDB. This method will retry the operation 
 against another host according to the balancing strategy up to the number of 
 times specified in the retry config or the retry argument, if supplied. 
 Returns None if the query fails."""
-        return self._run_http_verb('GET', query)
+        return self._run_http_verb('GET', query, protocol)
 
 
-    def post_to_pool(self, query, data=None, retry=None):
+    def post_to_pool(self, query, data=None, protocol=None, retry=None):
         """Run a post against TSDB. This method will retry the operation 
 against another host according to the balancing strategy up to the number of 
 times specified in the retry config or the retry argument, if supplied. 
 Returns None if the query fails."""
-        return self._run_http_verb('POST', query, data)
-
-
-        if retry == None:
-            retry = self._options['retry']
-        try:
-            tries = int(retry + 1)
-        except ValueError:
-            logger.error("get_from_pool invalid argument for retry: %s" % retry)
-            return None
-        if tries < 1:
-            logger.error("get_from_pool invalid argument for retry: %s" % retry)
-            return None
-        response = None
-        last_node = None
-        for t in range(tries):
-            node = self._find_next_host()
-            if not node:
-                continue
-            response = node.get(query)
-            if response:
-                break
-        return response
+        return self._run_http_verb('POST', query, data, protocol)
 
     def __repr__(self):
         rstr = "<TSDBPool instance, hosts in pool: %s, selection strategy %s>" % (self._hostlist, self._options['balance'])
@@ -470,7 +483,7 @@ this library should query."""
         cfh.close()
         self._parse_config()
         if init:
-            self.get_pool()
+            self.return_pool()
         else:
             self._TSDPool = None
         
@@ -503,30 +516,33 @@ not strictly JSON as comment lines that start with # or // are also accepted
         self._globalconf = global_conf
         self._hostconf = config['hosts'] 
          
-    def _get(self, query):
+    def get(self, query, protocol=None):
         """GET against the pool of TSDs in this configuration.
 """
         if not self._TSDPool:
             self.return_pool()
-        return self._TSDPool.get_from_pool(query)
+        return self._TSDPool.get_from_pool(query, protocol=protocol)
 
 
-    def _post(self, query, data=None):
+    def post(self, query, data=None, protocol=None):
         if not self._TSDPool:
             self.return_pool()
-        return self._TSDPool.post_to_pool(query, data)
+        return self._TSDPool.post_to_pool(query, data, protocol)
 
+    def head(self, query, data=None, protocol=None):
+        raise NotImplementedError("HEAD verb not implemented.")
 
-    def query(self, start, queries, end=None, noannotations=False, 
-              globalannotations=False, msresolution=False, showtsuids=False):
-        """Run a TSD query against the pool of TSDs we have configured.
-"""
+    def put(self, query, data=None, protocol=None):
+        raise NotImplementedError("PUT verb not implemented.")
 
-        if type(queries) == str: # allow a single query to be passed as a str
-            queries = (queries,)
-                
-                
-        pass # TODO
+    def delete(self, query, data=None, protocol=None):
+        raise NotImplementedError("DELETE verb not implemented.")
+
+    def trace(self, query, data=None, protocol=None):
+        raise NotImplementedError("TRACE verb not implemented.")
+
+    def connect(self, query, data=None, protocol=None):
+        raise NotImplementedError("CONNECT verb not implemented.")
 
     def return_pool(self):
         """Returns the TSD server pool associated with this configuration.
@@ -535,7 +551,222 @@ not strictly JSON as comment lines that start with # or // are also accepted
             self._TSDPool = TSDBPool(self._hostconf, self._globalconf)
         return self._TSDPool
 
+
+class TSD_protocol(object):
+    """TSD_protocol abstracts the TSD query mechanics away from the programmer,
+turning it into a fairly easy to use method-based interface.
+""" 
+    def __init__(self, config):
+        """TSD_protocol constructor.
+
+Required arguments:
+
+    config - either the name of a configuration file, or an instance of the
+             TSDBPoolConfig class that has been previously configured.
+
+Returns an instance of the TSD_protocol.
+"""
+        if type(config) == str:
+            self._config = TSDBPoolConfig(config)
+        else:
+            self._config = config
+        self._valid_aggregators = None
+
+    def _validate_aggregator(self, aggregator):
+        """Check if the aggregator is known to the TSD pool.
+
+Required arguments:
+
+    aggregator - The aggregator to validate
+
+Does not return a value, but raises a ValueError if the aggregator is not
+valid, or if there was a problem retrieving valid aggregators.
+"""
+
+        if not self._valid_aggregators:
+            agresponse = self.api_aggregators()
+            if agresponse.status_code != 200:
+                raise ValueError("There was a problem with the request.",
+                                 agresponse.status_code,
+                                 agresponse
+                                )
+            self._valid_aggregators = self.api_aggregators().json()
+        if aggregator not in self._valid_aggregators:
+            raise ValueError("Aggregator must be one of %s" %
+                             self._valid_aggregators)
+
+    def api_aggregators(self):
+        """Implements the OpenTSDB v2 API call to /api/aggregators
+
+    No arguments. Returns a Request response that includes a list of all 
+supported aggregators.
+"""
+        return self._config.get("api/aggregators")
+
+    def api_query(self, start, subqueries, end=None, noannotations=False,
+              globalannotations=False, msresolution=False, showtsuids=False):
+
+        """Implements the OpenTSDB v2 API call to /api/query.
+
+Required arguments:
+
+    start - any valid time that OpenTSDB understands.
+
+    subqueries - an array of subqueries. You can use the make_subquery()
+                 method in this class to generate suitable entries for this
+                 array.
+
+Optional arguments:
+
+    end - any valid end time that OpenTSDB understands. Should be later than
+          start time or OpenTSDB will send an error response.
+
+    noannotations - If set to true, annotations will be suppressed.
+
+    globalannotations - return any global annotations with the query if true.
+
+    msresolution - If true, data points will be output to millisecond
+                   resolution. Otherwise, the default resolution is 1 second.
+
+    showtsuids - If true, the TSU IDs of the timeseries will also be returned.
+
+The above descriptions are only to jog your memory of the purposes of the
+arguments. For more detailed information, consult the OpenTSDB docs.
+
+Returns a Requests response instance that contains the results of the query.
+    
+"""
+        if len(subqueries) == 0:
+            raise ValueError("subqueries must contain at least one entry.")
+        request_object = {'start': start,
+                          'queries': subqueries,
+                         }
+        if end: request_object['end'] = end
+        if noannotations: request_object['noAnnotations'] = True
+        if globalannotations: request_object['globalAnnotations'] = True
+        if msresolution: request_object['msResolution'] = True
+        if showtsuids: request_object['showTSUIDs'] = True
+        request_json = json.dumps(request_object, indent=4)
+        return self._config.post("api/query", 
+                                 request_json,
+                                 protocol="json",
+                                )
+
+    def api_suggest(self, datatype, q, maximum=None):
+        """Implements the OpenTSDB v2 API call to /api/suggest
+
+Required arguments:
+
+    datatype - the type of data to search for. Can be 'metrics' for metrics,
+               'tagk' for tag keys, or'tagv' for tag values. This argument
+               is called 'type' in the API but is renamed here to avoid
+               conflict with the python 'type' type.
+
+    q - the string to search with.
+
+Optional arguments:
+
+    maximum - maximum number of suggested results to return. Must be a 
+              positive integer. This argument is called 'max' in the API
+              but is renamed here to avoid conflict with the python 'max'
+              function.
+
+    Returns a Requests Response object that contains the result of the query. 
+    A 200 status code will reflect a successful query. 400 is usually the 
+    result of a failed requirement in the query, and 500 usually represents 
+    a server problem.
+
+"""
+        datatype = datatype.lower()
+        valid_types = ('metrics','tagk','tagv')
+        if datatype not in valid_types:
+            raise ValueError("type '%s' not one of %s" % 
+                             (datatype, valid_types))
+        suggest_query = {'type':datatype, 'q':q}
+        if maximum:
+            try:
+                if int(maximum) < 1:
+                    raise ValueError
+            except (ValueError, TypeError):
+                raise ValueError("Invalid value '%s' for maximum." % maximum)
+            suggest_query['max'] = int(maximum)
+        return self._config.post('api/suggest',
+                                 json.dumps(suggest_query),
+                                 protocol='json')  
+
+
+    def make_metrics_subquery(self, aggregator, metric, rate=None, 
+                             counter=None, countermax=None, resetvalue=None, 
+                             downsample=None, tags=None):
+        """Creates the data structure for a metrics subquery, suitable for parsing
+down into the JSON request object.
+
+Required arguments:
+    aggregator - The aggregator function to use in this subquery. This will
+                 be validated against a list of aggregators supplied by
+                 OpenTSDB.
+
+    metric - The metric you wish to query.
+
+Optional arguments:
+    rate - convert the data into deltas before returning.
+
+    counter - the data is a monotonically increasing counter that may
+              roll over.
+
+    countermax - the maximum value for the counter.
+
+    resetvalue - if the rate value exceeds this number, it will be clamped
+                 to zero.
+
+    downsample - a downsampling function.
+
+    ags - a map containing tag/value pairs.
+
+The above descriptions are only to jog your memory of the purposes of the
+arguments. For more detailed information, consult the OpenTSDB docs.
+
+Returns a dictionary suitable for use in a sub-query array for use in the 
+api_query() method.
+"""
+        self._validate_aggregator(aggregator)
+        subquery = {"aggregator":aggregator,
+                    "metric":metric}
+        if rate: subquery['rate']= True
+        if downsample: subquery['downsample']= downsample
+        if tags: 
+            subquery['tags']= tags
+        # Remove this else clause before flight. OpenTSDB bug workaround TODO
+        else:
+            subquery['tags'] = {'host':'*'}
+        if counter or countermax or resetvalue:
+            rateoptions = {}
+            if counter: rateoptions['counter'] = True
+            if countermax: rateoptions['counterMax'] = int(countermax)
+            if resetvalue: rateoptions['resetValue'] = int(resetvalue)
+            subquery['rateOptions'] = rateoptions
+    
+        return subquery                          
+                
+    def make_tsuid_subquery(self, aggregator, tsuids):
+        """Creates the data structure for a TSU ID subquery, suitable for parsing
+down into the JSON request object.
+
+Required arguments:
+    aggregator - The aggregator function to use in this subquery. This will
+                 be validated against a list of aggregators supplied by
+                 OpenTSDB.
+
+    tsuids - A list of hexadecimal encoded TSU IDs
+
+Returns a dictionary suitable for use in a sub-query array for use in the
+api_query() method.
+"""
+        self._validate_aggregator(aggregator)
+        return {'aggregator':aggregator, 'tsuids':tsuids}    
         
-def test(conf_file="llnw-prod.config"):
-    return TSDBPoolConfig(conf_file)
+
+        
+def test(conf_file="llnw-dev.config"):
+    return TSD_protocol(conf_file)
 
