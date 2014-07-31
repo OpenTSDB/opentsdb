f32b40078555ffa7eaa0b3354f274510455afce1
Johan Zeeck <johan.zeeck@tre.se>
2014-07-31 15:09:55 +0200

    test migrated fom TestUniqueId adapted for the new implementation complete

--- test/storage/TestHBaseStore.java
+++ test/storage/TestHBaseStore.java
@@ -14,11 +14,10 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 import static org.powermock.api.mockito.PowerMockito.mock;
 
@@ -109,7 +108,7 @@ public class TestHBaseStore extends TestTsdbStore {
     when(client.atomicIncrement(incrementForRow(MAX_UID))).thenReturn(Deferred.
             fromResult(6L));
     // next is compare and set make it return false and catch the exception
-    when(client.compareAndSet(any(PutRequest.class), eq(HBaseClient.EMPTY_ARRAY))).
+    when(client.compareAndSet(anyPut(), emptyArray())).
             thenReturn(Deferred.fromResult(false));
 
     try {
@@ -134,152 +133,100 @@ public class TestHBaseStore extends TestTsdbStore {
     }
     final InOrder order = inOrder(client);
     order.verify(client).atomicIncrement(incrementForRow(MAX_UID));
-    order.verify(client).compareAndSet(any(PutRequest.class),
-            eq(HBaseClient.EMPTY_ARRAY));
-    verify(client, times(1)).compareAndSet(any(PutRequest.class),
-            eq(HBaseClient.EMPTY_ARRAY));
+    order.verify(client).compareAndSet(anyPut(),
+            emptyArray());
+    verify(client, times(1)).compareAndSet(anyPut(),
+            emptyArray());
     verify(client, times(1)).atomicIncrement(incrementForRow(MAX_UID));
     verify(client, never()).get(any(GetRequest.class));
-    verify(client, never()).put(any(PutRequest.class));
+    verify(client, never()).put(anyPut());
   }
 
 
   @Test  // Test the creation of an ID when unable to increment MAXID
   public void allocateUIDUnableToIncrementMaxId() throws Exception {
-    PowerMockito.mockStatic(Thread.class);
+    HBaseException hbe = fakeHBaseException();
 
-    uid = new UniqueId(client, table, UniqueIdType.METRIC);
-
-    when(client.get(anyGet()))      // null  =>  ID doesn't exist.
-      .thenReturn(Deferred.<ArrayList<KeyValue>>fromResult(null));
+    when(client.atomicIncrement(incrementForRow(MAX_UID)))
+      .thenReturn(Deferred.<Long>fromError(hbe));
     // Watch this! ______,^   I'm writing C++ in Java!
 
-    HBaseException hbe = fakeHBaseException();
-    when(client.atomicIncrement(incrementForRow(MAX_UID)))
-      .thenThrow(hbe);
-    PowerMockito.doNothing().when(Thread.class); Thread.sleep(anyInt());
-
     try {
-      uid.getOrCreateId("foo");
+      //need to join to get the exception to be thrown
+      byte[] uid = tsdb_store.allocateUID(foo_array,
+              UniqueId.UniqueIdType.METRIC,
+              UniqueId.UniqueIdType.METRIC.width).joinUninterruptibly();
       fail("HBaseException should have been thrown!");
     } catch (HBaseException e) {
-      assertSame(hbe, e);
+      assertSame(hbe,e);
     }
   }
 
-  /*
+
   @Test  // Test the creation of an ID with a race condition.
-  @PrepareForTest({HBaseStore.class, Deferred.class})
-   public void getOrCreateIdAssignIdWithRaceCondition() {
-    // Simulate a race between client A and client B.
-    // A does a Get and sees that there's no ID for this name.
-    // B does a Get and sees that there's no ID too, and B actually goes
-    // through the entire process to create the ID.
-    // Then A attempts to go through the process and should discover that the
-    // ID has already been assigned.
+  public void getOrCreateIdAssignIdWithRaceCondition() {
+  // Simulate a race between client A and client B.
+  // A does a Get and sees that there's no ID for this name.
+  // B does a Get and sees that there's no ID too, and B actually goes
+  // through the entire process to create the ID.
+  // Then A attempts to go through the process and should discover that the
+  // ID has already been assigned.
 
-    uid = new UniqueId(client, table, UniqueIdType.METRIC); // Used by client A.
-    HBaseStore client_b = mock(HBaseStore.class); // For client B.
-    final UniqueId uid_b = new UniqueId(client_b, table, UniqueIdType.METRIC);
+  //make sur ethe atomicIncrement pass with valid long
+  when(client.atomicIncrement(incrementForRow(MAX_UID))).thenReturn(Deferred.
+    fromResult(6L));
+  // First call to compareAndSet should return true, basically we would create
+  // the reverse mapping.
+  // However the second call to compare and set should return false, we fail to
+  // create the forward mapping similar this is what should happen is another
+  // client created the before the first instance was finished.
+  // A warning should then be logged and a client.get request should happen.
+  when(client.compareAndSet(anyPut(),emptyArray()))
+          .thenReturn(Deferred.fromResult(true))
+          .thenReturn(Deferred.fromResult(false));
+  // For simplicity return an empty ArrayList, then the call back should return
+  // null
+  when(client.get(any(GetRequest.class)))
+          .thenReturn(Deferred.fromResult(new ArrayList<KeyValue>()));
 
-    final byte[] id = { 0, 0, 5 };
-    final byte[] byte_name = { 'f', 'o', 'o' };
-    final ArrayList<KeyValue> kvs = new ArrayList<KeyValue>(1);
-    kvs.add(new KeyValue(byte_name, ID, kind_array, id));
-
-    @SuppressWarnings("unchecked")
-    final Deferred<ArrayList<KeyValue>> d = PowerMockito.spy(new Deferred<ArrayList<KeyValue>>());
-    when(client.get(anyGet()))
-      .thenReturn(d)
-      .thenReturn(Deferred.fromResult(kvs));
-
-    final Answer<byte[]> the_race = new Answer<byte[]>() {
-      public byte[] answer(final InvocationOnMock unused_invocation) throws Exception {
-        // While answering A's first Get, B doest a full getOrCreateId.
-        assertArrayEquals(id, uid_b.getOrCreateId("foo"));
-        d.callback(null);
-        return (byte[]) ((Deferred) d).join();
-      }
-    };
-
-    // Start the race when answering A's first Get.
-    try {
-      PowerMockito.doAnswer(the_race).when(d).joinUninterruptibly();
+  try {
+    byte[] uid = tsdb_store.allocateUID(foo_array, UniqueId.UniqueIdType.METRIC,
+            UniqueId.UniqueIdType.METRIC.width).joinUninterruptibly();
+    if (null != uid)
+      fail("Should have received null but received uid = " + uid);
     } catch (Exception e) {
-      fail("Should never happen: " + e);
-    }
-
-    when(client_b.get(anyGet())) // null => ID doesn't exist.
-      .thenReturn(Deferred.<ArrayList<KeyValue>>fromResult(null));
-    // Watch this! ______,^ I'm writing C++ in Java!
-
-    when(client_b.atomicIncrement(incrementForRow(MAXID)))
-      .thenReturn(Deferred.fromResult(5L));
-
-    when(client_b.compareAndSet(anyPut(), emptyArray()))
-      .thenReturn(Deferred.fromResult(true))
-      .thenReturn(Deferred.fromResult(true));
-
-    // Now that B is finished, A proceeds and allocates a UID that will be
-    // wasted, and creates the reverse mapping, but fails at creating the
-    // forward mapping.
-    when(client.atomicIncrement(incrementForRow(MAXID)))
-      .thenReturn(Deferred.fromResult(6L));
-
-    when(client.compareAndSet(anyPut(), emptyArray()))
-      .thenReturn(Deferred.fromResult(true)) // Orphan reverse mapping.
-      .thenReturn(Deferred.fromResult(false)); // Already CAS'ed by A.
-
-    // Start the execution.
-    assertArrayEquals(id, uid.getOrCreateId("foo"));
-
-    // Verify the order of execution too.
-    final InOrder order = inOrder(client, client_b);
-    order.verify(client).get(anyGet()); // 1st Get for A.
-    order.verify(client_b).get(anyGet()); // 1st Get for B.
-    order.verify(client_b).atomicIncrement(incrementForRow(MAXID));
-    order.verify(client_b, times(2)).compareAndSet(anyPut(), // both mappings.
-                                                   emptyArray());
-    order.verify(client).atomicIncrement(incrementForRow(MAXID));
-    order.verify(client, times(2)).compareAndSet(anyPut(), // both mappings.
-                                                 emptyArray());
-    order.verify(client).get(anyGet()); // A retries and gets it.
+      fail("Received unexpected exception = " + e);
   }
+  // Verify the order of execution too.
+  final InOrder order = inOrder(client);
+  order.verify(client, times(1)).atomicIncrement(incrementForRow(MAX_UID));
+  order.verify(client, times(2)).compareAndSet(anyPut(),
+          emptyArray());
+  order.verify(client, times(1)).get(any(GetRequest.class));
 
+  }
 
   @Test  // ICV throws an exception, we can't get an ID.
-  public void getOrCreateIdWithICVFailure() {
-    uid = new UniqueId(client, table, UniqueIdType.METRIC);
-    final Config config = mock(Config.class);
-    when(config.enable_realtime_uid()).thenReturn(false);
-    final TSDB tsdb = mock(TSDB.class);
-    when(tsdb.getConfig()).thenReturn(config);
-    uid.setTSDB(tsdb);
+  public void getOrCreateIdWithICVFailure() throws Exception{
+  // Watch this! ______,^   I'm writing C++ in Java!
 
-    when(client.get(anyGet()))      // null  =>  ID doesn't exist.
-      .thenReturn(Deferred.<ArrayList<KeyValue>>fromResult(null));
-    // Watch this! ______,^   I'm writing C++ in Java!
+  when(client.atomicIncrement(incrementForRow(MAX_UID)))
+    .thenReturn(Deferred.fromResult(5L));
 
-    // Update once HBASE-2292 is fixed:
-    HBaseException hbe = fakeHBaseException();
-    when(client.atomicIncrement(incrementForRow(MAXID)))
-      .thenReturn(Deferred.<Long>fromError(hbe))
-      .thenReturn(Deferred.fromResult(5L));
-
-    when(client.compareAndSet(anyPut(), emptyArray()))
-      .thenReturn(Deferred.fromResult(true))
-      .thenReturn(Deferred.fromResult(true));
+  when(client.compareAndSet(anyPut(), emptyArray()))
+    .thenReturn(Deferred.fromResult(true))
+    .thenReturn(Deferred.fromResult(true));
 
     final byte[] id = { 0, 0, 5 };
-    assertArrayEquals(id, uid.getOrCreateId("foo"));
-    verify(client, times(1)).get(anyGet()); // Initial Get.
-    // First increment (failed) + retry.
-    verify(client, times(2)).atomicIncrement(incrementForRow(MAXID));
+    assertArrayEquals(id, tsdb_store.allocateUID(foo_array,
+      UniqueId.UniqueIdType.METRIC, UniqueId.UniqueIdType.METRIC.width)
+      .joinUninterruptibly());
+    verify(client, times(1)).atomicIncrement(incrementForRow(MAX_UID));
     // Reverse + forward mappings.
-    verify(client, times(2)).compareAndSet(anyPut(), emptyArray());
+    verify(client, times(2)).compareAndSet(anyPut(),
+            emptyArray());
   }
 
-  */
   private static AtomicIncrementRequest incrementForRow(final byte[] p_row) {
     return argThat(new ArgumentMatcher<AtomicIncrementRequest>() {
       public boolean matches(Object incr) {
@@ -299,5 +246,12 @@ public class TestHBaseStore extends TestTsdbStore {
     when(hbe.getMessage())
             .thenReturn("fake exception");
     return hbe;
+  }
+
+  private byte[] emptyArray() {
+    return eq(HBaseClient.EMPTY_ARRAY);
+  }
+  private PutRequest anyPut() {
+    return any(PutRequest.class);
   }
 }
