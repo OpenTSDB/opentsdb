3b5ce525845934155fa4ad890f6d7b490c7db479
Andreas Falk <mail@andreasfalk.se>
2014-10-29 11:17:28 +0100

    Add missing Iterator import in HBaseStore

--- src/storage/hbase/HBaseStore.java
+++ src/storage/hbase/HBaseStore.java
@@ -15,6 +15,7 @@ package net.opentsdb.storage.hbase;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
