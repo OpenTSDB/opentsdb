b7b207c5f55214aedb351b7453b2c2e3cc27ba98
Johan Zeeck <johan.zeeck@gmail.com>
2015-01-14 13:49:36 +0100

    Added the classes for the CassandraStore. They do not implement anything at the moment.

---
+++ test/storage/cassandra/TestCassandraStore.java
@@ -0,0 +1,17 @@
+package net.opentsdb.storage.cassandra;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class TestCassandraStore {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
