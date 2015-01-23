9531a13e3e85e97451387612127037060975701f
Johan Zeeck <johan.zeeck@gmail.com>
2015-01-23 16:32:37 +0100

    Work done on the CassndraStore.

--- src/storage/cassandra/CassandraStore.java
+++ src/storage/cassandra/CassandraStore.java
@@ -1,16 +1,24 @@
 package net.opentsdb.storage.cassandra;
 
+import com.datastax.driver.core.BatchStatement;
+import com.datastax.driver.core.BoundStatement;
+import com.datastax.driver.core.CloseFuture;
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Host;
 import com.datastax.driver.core.Metadata;
+import com.datastax.driver.core.PreparedStatement;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.ResultSetFuture;
 import com.datastax.driver.core.Session;
 import com.google.common.base.Optional;
-import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableList;
-import com.google.common.net.HostAndPort;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
 import com.stumbleupon.async.Deferred;
+import net.opentsdb.core.Const;
 import net.opentsdb.core.DataPoints;
 import net.opentsdb.core.Query;
+import net.opentsdb.core.RowKey;
 import net.opentsdb.core.TSDB;
 import net.opentsdb.meta.Annotation;
 import net.opentsdb.meta.TSMeta;
@@ -21,13 +29,10 @@ import net.opentsdb.tree.Branch;
 import net.opentsdb.tree.Leaf;
 import net.opentsdb.tree.Tree;
 import net.opentsdb.tree.TreeRule;
+import net.opentsdb.uid.UniqueId;
 import net.opentsdb.uid.UniqueIdType;
-import net.opentsdb.utils.Config;
-import org.hbase.async.AtomicIncrementRequest;
-import org.hbase.async.DeleteRequest;
 import org.hbase.async.GetRequest;
 import org.hbase.async.KeyValue;
-import org.hbase.async.PutRequest;
 import org.hbase.async.Scanner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,6 +40,8 @@ import org.slf4j.LoggerFactory;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * The CassandraStore that implements the client interface required by TSDB.
@@ -53,6 +60,31 @@ public class CassandraStore implements TsdbStore {
      * The current Cassandra session.
      */
     private Session session;
+    /**
+     * The statement used by the {@link #addPoint} method.
+     */
+    private PreparedStatement add_point_statement;
+    /**
+     * The statement used by the {@link #allocateUID} method.
+     */
+
+    private PreparedStatement get_max_uid_statement;
+    private PreparedStatement increment_uid_statement;
+    private PreparedStatement create_new_uid_name_statement;
+    private PreparedStatement create_new_name_uid_statement;
+
+    private PreparedStatement lock_uid_name_combination;
+
+    private PreparedStatement update_uid_name_statement;
+    private PreparedStatement update_name_uid_statement;
+    /**
+     * The statement used when trying to
+     */
+    private PreparedStatement get_name_statement;
+    private PreparedStatement get_id_statement;
+
+    private PreparedStatement insert_tags_statement;
+
 
     public CassandraStore(final Cluster cluster) {
 
@@ -67,7 +99,52 @@ public class CassandraStore implements TsdbStore {
             LOG.info("Datacenter: {}; Host: {}; Rack: {}",
                     host.getDatacenter(), host.getAddress(), host.getRack());
         }
+        prepareStatements();
     }
+
+    private void prepareStatements() {
+        checkNotNull(session);
+        checkNotNull(cluster);
+        String CQL = "INSERT INTO \"tsdb\".\"data\" (tsuid, basetime, " +
+                "timestamp, flags, val) VALUES (?, ?, ?, ?, ?);";
+        add_point_statement = session.prepare(CQL);
+
+        CQL = "UPDATE \"tsdb\".\"max_uid_type\" SET max = max + 1 WHERE " +
+                "type=?;";
+        increment_uid_statement = session.prepare(CQL);
+
+        CQL = "SELECT * FROM \"tsdb\".\"max_uid_type\" WHERE type=?;";
+        get_max_uid_statement = session.prepare(CQL);
+
+        CQL = "INSERT INTO tsdb.uid_id (uid, type, name) VALUES (?, ?, ?);";
+        create_new_uid_name_statement = session.prepare(CQL);
+
+        CQL = "INSERT INTO tsdb.name_id (name, type, uid) VALUES (?, ?, ?);";
+        create_new_name_uid_statement = session.prepare(CQL);
+
+        CQL = "INSERT INTO tsdbunique.uid_name_id (key, type) VALUES (?, ?) " +
+                "IF NOT EXISTS;";
+        lock_uid_name_combination = session.prepare(CQL);
+
+        CQL = "UPDATE tsdb.uid_id SET name = ? WHERE uid = ? AND type = ?;";
+        update_uid_name_statement = session.prepare(CQL);
+
+        CQL = "BEGIN BATCH " +
+                "DELETE FROM tsdb.name_id WHERE name = ? AND type= ? " +
+                "INSERT INTO tsdb.name_id (name, type, uid) VALUES (?, ?, ?) " +
+              "APPLY BATCH;";
+        update_name_uid_statement = session.prepare(CQL);
+
+        CQL = "SELECT * FROM tsdb.uid_id WHERE uid = ? AND type = ?;";
+        get_name_statement = session.prepare(CQL);
+
+        CQL = "SELECT * FROM tsdb.name_id WHERE name = ? AND type = ?;";
+        get_id_statement = session.prepare(CQL);
+
+        CQL = "INSERT INTO tsdb.tags_data (uid, type, tsuid) VALUES (?, ?, ?);";
+        insert_tags_statement = session.prepare(CQL);
+    }
+
     @Override
     public Deferred<Annotation> getAnnotation(byte[] tsuid, long start_time) {
         return null;
@@ -104,13 +181,70 @@ public class CassandraStore implements TsdbStore {
     }
 
     @Override
-    public Deferred<Object> addPoint(byte[] tsuid, byte[] value, long timestamp, short flags) {
-        return null;
+    public Deferred<Object> addPoint(final byte[] tsuid, byte[] value,
+                                     long timestamp, short flags) {
+
+        final long base_time = buildBaseTime(timestamp);
+
+
+        final ResultSetFuture future = session.executeAsync(add_point_statement
+                .bind(tsuid, base_time, timestamp, flags, value));
+
+        final byte[] metric_uid = RowKey.metric(tsuid);
+        final Map<byte[], byte[]> tags_uids = RowKey.tags(tsuid);
+
+        final Deferred<Object> d = new Deferred<Object>();
+
+        Futures.addCallback(future, new FutureCallback<ResultSet>() {
+            @Override
+            public void onSuccess(ResultSet rows) {
+                d.callback(null);
+                session.executeAsync(insert_tags_statement.bind(
+                        UniqueId.uidToLong(metric_uid, UniqueIdType.METRIC.width),
+                        UniqueIdType.METRIC, tsuid));
+                for (Map.Entry<byte[], byte[]> entry : tags_uids.entrySet())
+                {
+                    session.executeAsync(insert_tags_statement.bind(
+                            UniqueId.uidToLong(entry.getKey(), UniqueIdType.TAGK.width),
+                            UniqueIdType.TAGK, tsuid));
+
+                    session.executeAsync(insert_tags_statement.bind(
+                            UniqueId.uidToLong(entry.getValue(), UniqueIdType
+                                    .TAGV.width),
+                            UniqueIdType.TAGV, tsuid));
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                d.callback(throwable);
+            }
+        });
+
+        return d;
     }
 
     @Override
     public Deferred<Object> shutdown() {
-        return null;
+        List<CloseFuture> close = new ArrayList<CloseFuture>();
+        close.add(session.closeAsync());
+        close.add(cluster.closeAsync());
+
+        final Deferred<Object> d = new Deferred<Object>();
+        
+        Futures.addCallback(Futures.allAsList(close), new
+                FutureCallback<List<Void>>() {
+            @Override
+            public void onSuccess(List<Void> voids) {
+                d.callback(null);
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                d.callback(throwable);
+            }
+        });
+        return d;
     }
 
     @Override
@@ -119,13 +253,55 @@ public class CassandraStore implements TsdbStore {
     }
 
     @Override
-    public Deferred<Optional<byte[]>> getId(String name, UniqueIdType type) {
-        return null;
+    public Deferred<Optional<byte[]>> getId(final String name, final
+    UniqueIdType
+            type) {
+        ResultSetFuture f = session.executeAsync(get_id_statement.bind(
+                name, type.qualifier));
+
+        final Deferred<Optional<byte[]>> d = new Deferred<Optional<byte[]>>();
+
+        Futures.addCallback(f, new FutureCallback<ResultSet>() {
+            @Override
+            public void onSuccess(ResultSet rows) {
+                final long uid = rows.one().getLong("uid");
+                d.callback(Optional.of(UniqueId.longToUID(uid, type.width)));
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                d.callback(throwable);
+            }
+        });
+        return d;
     }
 
     @Override
-    public Deferred<Optional<String>> getName(byte[] id, UniqueIdType type) {
-        return null;
+    public Deferred<Optional<String>> getName(final byte[] id, final
+    UniqueIdType type) {
+
+        ResultSetFuture f = session.executeAsync(get_name_statement.bind(
+                UniqueId.uidToLong(id), type.qualifier));
+
+        final Deferred<Optional<String>> d = new Deferred<Optional<String>>();
+
+        Futures.addCallback(f, new FutureCallback<ResultSet>() {
+            @Override
+            public void onSuccess(ResultSet rows) {
+                if (!rows.isExhausted()) {
+                    final String name = rows.one().getString("name");
+                    d.callback(Optional.of(name));
+                    return;
+                }
+                d.callback(Optional.absent());
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                d.callback(throwable);
+            }
+        });
+        return d;
     }
 
     @Override
@@ -154,13 +330,134 @@ public class CassandraStore implements TsdbStore {
     }
 
     @Override
-    public Deferred<byte[]> allocateUID(String name, UniqueIdType type) {
-        return null;
-    }
+    public Deferred<byte[]> allocateUID(final String name, final UniqueIdType
+            type) {
 
+        final Deferred<byte[]> d = new Deferred<byte[]>();
+
+        final ResultSetFuture future = session.executeAsync(get_max_uid_statement.bind
+                (type.qualifier));
+
+        Futures.addCallback(future, new FutureCallback<ResultSet>() {
+            @Override
+            public void onSuccess(ResultSet rows) {
+                // we have received the value.
+                final long next_uid = rows.one().getLong("max") + 1;
+                //next step is to lock down this resource in a non blocking way.
+                BatchStatement lock_batch = new BatchStatement();
+                lock_batch.add(lock_uid_name_combination.bind(UniqueId
+                        .uidToString(UniqueId.longToUID(next_uid, type.width)), type
+                        .qualifier));
+                lock_batch.add(lock_uid_name_combination.bind(
+                        name, type.qualifier));
+                // We have a 'lock' table that we write to. We write two
+                // rows one for each the name and the uid with the
+                // UniqueIdType as an additional key.
+                // If this write is successful we know there are no duplicate
+                // values for the name and key for this UID type.
+                // Thus onSuccess we will then go ahead and write this
+                // resource to the original table.
+                Futures.addCallback(
+                        session.executeAsync(lock_batch),
+                        new FutureCallback<ResultSet>() {
+                            @Override
+                            public void onSuccess(ResultSet rows) {
+                                // We want this process to just write so no need to
+                                // keep track of this callback at from this point.
+
+                                if (rows.wasApplied()) {
+                                    session.executeAsync(increment_uid_statement.bind(type.qualifier));
+                                    session.executeAsync(create_new_name_uid_statement.bind(name, type.qualifier, next_uid));
+                                    session.executeAsync(create_new_uid_name_statement.bind(next_uid, type.qualifier, name));
+                                    //TODO (zeeck) maybe check callback?
+                                    // returned the used UID
+                                    // we will assume the write will be fine, Cassandra
+                                    // can do a lot of writes without issues so if we
+                                    // fail from this point there are some major issues!
+                                    d.callback(UniqueId.longToUID(next_uid, type.width));
+                                    return;
+                                }
+                                LOG.warn("Race condition creating mapping for uid " +
+                                        "{}. Another TSDB instance must have " +
+                                        "allocated this uid concurrently or " +
+                                                "name is already taken for " +
+                                                "this uid.", next_uid);
+                            }
+
+                            @Override
+                            public void onFailure(Throwable throwable) {
+                                LOG.error("had a failure to communicate with " +
+                                        "Cassandra.", throwable);
+                                d.callback(throwable);
+                    }
+                });
+
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                // for some reason we could not get the max uid value.
+                LOG.error("Could not fetch max UID value.", throwable);
+                d.callback(throwable);
+            }
+        });
+
+            return d;
+        }
+
+    /**
+     * For all intents and purposes this function works as a rename. In the
+     * HBase implementation the other method {@link #allocateUID} uses this
+     * method that basically overwrites the value no matter what. This method
+     * is also used by the function {@link net.opentsdb.uid.UniqueId#rename}.
+     *
+     * TODO #zeeck this method should be considered to be changed to rename and
+     * the implementation changed in the HBaseStore. One of tre prerequisites
+     * of this function is that the UID already exists.
+     *
+     * @param name The name to write.
+     * @param uid The uid to use.
+     * @param type The type of UID
+     * @return The uid that was used.
+     */
     @Override
-    public Deferred<byte[]> allocateUID(String name, byte[] uid, UniqueIdType type) {
-        return null;
+    public Deferred<byte[]> allocateUID(final String name, final byte[] uid,
+                                        final UniqueIdType type) {
+
+        // Get old name
+        ResultSetFuture f = session.executeAsync(get_name_statement.bind(
+                UniqueId.uidToLong(uid), type.qualifier));
+
+        final Deferred<byte[]> d = new Deferred<byte[]>();
+
+        //CQL = "UPDATE tsdb.uid_id SET name = ? WHERE uid = ? AND type = ?;";
+        final BoundStatement s1 = new BoundStatement(update_uid_name_statement)
+                .bind(name, UniqueId.uidToLong(uid), type.qualifier);
+
+        Futures.addCallback(f, new FutureCallback<ResultSet>() {
+            @Override
+            public void onSuccess(ResultSet rows) {
+                final String old_name = rows.one().getString("name");
+                session.executeAsync(s1);
+                BoundStatement s = new BoundStatement
+                        (update_name_uid_statement);
+                // CQL =
+                // BEGIN BATCH
+                // DELETE FROM tsdb.name_id WHERE name = ? AND type = ?
+                // INSERT INTO tsdb.name_id (name, type, uid) VALUES (?, ?, ?)
+                // APPLY BATCH;
+                session.executeAsync(s.bind(old_name, type.qualifier,
+                        name, type.qualifier, UniqueId.uidToLong(uid)));
+                //TODO (zeeck) maybe check if this was ok
+                d.callback(uid);
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                d.callback(throwable);
+            }
+        });
+        return d;
     }
 
     @Override
@@ -321,5 +618,17 @@ public class CassandraStore implements TsdbStore {
     @Override
     public Deferred<Object> setTSMetaCounter(byte[] tsuid, long number) {
         return null;
+    }
+
+    /**
+     * Calculate the base time based on a timestamp to be used in a row key.
+     */
+    public static long buildBaseTime(final long timestamp) {
+        if ((timestamp & Const.SECOND_MASK) != 0) {
+            // drop the ms timestamp to seconds to calculate the base timestamp
+            return ((timestamp / 1000) - ((timestamp / 1000) % Const.MAX_TIMESPAN));
+        } else {
+            return (timestamp - (timestamp % Const.MAX_TIMESPAN));
+        }
     }
 }
--- test/storage/cassandra/TestCassandraStore.java
+++ test/storage/cassandra/TestCassandraStore.java
@@ -1,17 +1,69 @@
 package net.opentsdb.storage.cassandra;
 
+import com.google.common.base.Optional;
+import net.opentsdb.core.StoreSupplier;
+import net.opentsdb.storage.TsdbStore;
+import net.opentsdb.uid.UniqueIdType;
+import net.opentsdb.utils.Config;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class TestCassandraStore {
+
+    TsdbStore store;
 
     @Before
     public void setUp() throws Exception {
 
+        StoreSupplier ss = new StoreSupplier(new Config(false));
+        store = ss.get();
     }
 
     @After
     public void tearDown() throws Exception {
 
+    }
+
+    @Test
+    public void constructor() throws IOException {
+        StoreSupplier ss = new StoreSupplier(new Config(false));
+        store = ss.get();
+    }
+
+    @Test
+    public void allocateUID() {
+        store.allocateUID("new",UniqueIdType.METRIC);
+    }
+
+    @Test
+    public void allocateUIDRename() {
+        store.allocateUID("renamed",new byte[]{0,0,4},UniqueIdType.METRIC);
+    }
+
+    @Test
+    public void getName() throws Exception {
+        //TODO (zeeck) make timeout ot a const
+        Optional<String> name;
+        name = store.getName(new byte[] {0, 0, 1}, UniqueIdType.METRIC)
+                .joinUninterruptibly(20);
+        assertEquals("sys",name.get());
+        name = store.getName(new byte[] {0, 0, 2}, UniqueIdType.METRIC)
+                .joinUninterruptibly(20);
+        assertEquals("cpu0",name.get());
+        name = store.getName(new byte[] {0, 0, 3}, UniqueIdType.METRIC)
+                .joinUninterruptibly(20);
+        assertEquals("cpu1",name.get());
+        name = store.getName(new byte[] {0, 0, 4}, UniqueIdType.METRIC)
+                .joinUninterruptibly(20);
+        assertEquals("renamed",name.get());
+        name = store.getName(new byte[] {0, 0, 5}, UniqueIdType.METRIC)
+                .joinUninterruptibly(20);
+        assertFalse(name.isPresent());
     }
 }
