789ef92168d95f90c3d88bcabfa4cc58d39e9fdf
Johan Zeeck <johan.zeeck@gmail.com>
2015-01-26 11:13:37 +0100

    Fixing tests and cleaning upp Cassandra things.

--- test/storage/cassandra/TestCassandraStore.java
+++ test/storage/cassandra/TestCassandraStore.java
@@ -1,40 +1,117 @@
 package net.opentsdb.storage.cassandra;
 
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
 import com.google.common.base.Optional;
+import com.google.common.base.Splitter;
+import com.google.common.net.HostAndPort;
 import net.opentsdb.core.StoreSupplier;
-import net.opentsdb.storage.TsdbStore;
 import net.opentsdb.uid.UniqueIdType;
 import net.opentsdb.utils.Config;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 
 public class TestCassandraStore {
 
-    TsdbStore store;
+    private CassandraStore store;
+    private static Config config;
+    private Session session;
 
-    @Before
-    public void setUp() throws Exception {
+    @BeforeClass
+    public static void oneTimeSetUp() throws IOException {
+        Map<String, String> overrides = new HashMap<String, String>();
+        overrides.put("tsd.storage.adapter", "Cassandra");
+        overrides.put("tsd.storage.cassandra.clusters", "127.0.0.1");
 
-        StoreSupplier ss = new StoreSupplier(new Config(false));
-        store = ss.get();
+        config = new Config(false, overrides);
     }
 
-    @After
-    public void tearDown() throws Exception {
+    /**
+     * Use this to connect to cassandra.
+     */
+    private void setUpCassandraConnection() {
+        Cluster.Builder builder = Cluster.builder();
+        Iterable<String> nodes = Splitter.on(',').trimResults()
+                .omitEmptyStrings()
+                .split(config.getString("tsd.storage.cassandra.clusters"));
+        for (String node : nodes) {
+            try {
+                HostAndPort host = HostAndPort.fromString(node);
 
+                builder.addContactPoint(host.getHostText()).withPort(host
+                        .getPortOrDefault(CassandraConst
+                                .DEFAULT_CASSANDRA_PORT));
+            }
+            catch (IllegalArgumentException e) {
+                throw new IllegalArgumentException("There was an error in the" +
+                        " configuration file in the field 'tsd.storage" +
+                        ".cassandra.clusters'.", e);
+            }
+        }
+        Cluster cluster = builder.build();
+        store = new CassandraStore(cluster);
+        session = cluster.connect();
+    }
+
+    /**
+     * Use this to clear the data in your localhost version of Cassandra
+     */
+    private void clearData() {
+        session.execute("TRUNCATE tsdb.data");
+        session.execute("TRUNCATE tsdb.tags_data");
+        session.execute("TRUNCATE tsdb.uid_id");
+        session.execute("TRUNCATE tsdb.name_id");
+        session.execute("TRUNCATE tsdb.max_uid_type");
+        session.execute("TRUNCATE tsdbunique.uid_name_id");
     }
 
     @Test
     public void constructor() throws IOException {
-        StoreSupplier ss = new StoreSupplier(new Config(false));
-        store = ss.get();
+        Cluster.Builder builder = Cluster.builder();
+
+        Iterable<String> nodes = Splitter.on(',').trimResults()
+                .omitEmptyStrings()
+                .split(config.getString("tsd.storage.cassandra.clusters"));
+
+        for (String node : nodes) {
+            try {
+                HostAndPort host = HostAndPort.fromString(node);
+
+                builder.addContactPoint(host.getHostText()).withPort(host
+                        .getPortOrDefault(CassandraConst
+                                .DEFAULT_CASSANDRA_PORT));
+            }
+            catch (IllegalArgumentException e) {
+                throw new IllegalArgumentException("There was an error in the" +
+                        " configuration file in the field 'tsd.storage" +
+                        ".cassandra.clusters'.", e);
+            }
+        }
+        assertNotNull(new CassandraStore(builder.build()));
     }
+
+    @Test (expected = NullPointerException.class)
+    public void constructorNull() throws IOException {
+        new CassandraStore(null);
+    }
+
+    @Test
+    public void addPoint() {
+        setUpCassandraConnection();
+        clearData();
+
+
+        //'001', 1356998400, 1356998400, 'a', 'data1'
+    }
+
 
     @Test
     public void allocateUID() {
