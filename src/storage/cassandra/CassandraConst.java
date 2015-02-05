a5dddcc8dd676e6abcf260c3950dccdc67988331
Andreas Falk <mail@andreasfalk.se>
2015-02-05 11:08:38 +0100

    Reformat cassandra code

--- src/storage/cassandra/CassandraConst.java
+++ src/storage/cassandra/CassandraConst.java
@@ -4,10 +4,10 @@ package net.opentsdb.storage.cassandra;
  * Constants used by Cassandra
  */
 public class CassandraConst {
-    /**
-     * The default Cassandra Port used by the cassandra by default if the
-     * port was not specified in the configuration file.
-     */
-    public static final int DEFAULT_CASSANDRA_PORT = 9042;
-    public static final long CASSANDRA_TIMEOUT = 50;
+  /**
+   * The default Cassandra Port used by the cassandra by default if the port was
+   * not specified in the configuration file.
+   */
+  public static final int DEFAULT_CASSANDRA_PORT = 9042;
+  public static final long CASSANDRA_TIMEOUT = 50;
 }
