e6727c4de244bcab07dd9d954c48848223d2baa0
Bryan <bryan4887@gmail.com>
2015-03-13 19:10:24 -0600

    Fixed problems and simplified.

--- src/examples/AddDataExample.java
+++ src/examples/AddDataExample.java
@@ -1,5 +1,5 @@
 // This file is part of OpenTSDB.
-// Copyright (C) 2010-2012  The OpenTSDB Authors.
+// Copyright (C) 2015  The OpenTSDB Authors.
 //
 // This program is free software: you can redistribute it and/or modify it
 // under the terms of the GNU Lesser General Public License as published by
@@ -22,7 +22,6 @@ import com.stumbleupon.async.Callback;
 import com.stumbleupon.async.Deferred;
 
 import net.opentsdb.core.TSDB;
-import net.opentsdb.core.WritableDataPoints;
 import net.opentsdb.uid.NoSuchUniqueName;
 import net.opentsdb.uid.UniqueId.UniqueIdType;
 import net.opentsdb.utils.Config;
@@ -30,18 +29,26 @@ import net.opentsdb.utils.Config;
 /**
  * Examples for how to add points to the tsdb.
  * 
- * @author Bryan Hernandez
- * 
  */
 public class AddDataExample {
+  private static String pathToConfigFile;
+  
+  public static void processArgs(String[] args) {
+ // Set these as arguments so you don't have to keep path information in
+    // source files
+    if (args == null) {
+      System.err.println("First (and only) argument must be the full path to the opentsdb.conf file. (e.g. /User/thisUser/opentsdb/src/opentsdb.conf");
+    } else {
+      pathToConfigFile = args[0];
+    }
+  }
 
   public static void main(String[] args) throws Exception {
 
-    // Set these as arguments so you don't have to keep path information in
-    // source files
-    String pathToConfigFile = args[0]; // e.g.
-                                       // "/User/thisUser/opentsdb/src/opentsdb.conf"
-
+    
+    
+    processArgs(args);
+    
     // Create a config object with a path to the file for parsing. Or manually
     // override settings.
     // e.g. config.overrideConfig("tsd.storage.hbase.zk_quorum", "localhost");
@@ -54,12 +61,14 @@ public class AddDataExample {
     byte[] byteMetricUID; // we don't actually need this for the first
                           // .addPoint() call below.
     // TODO: Ideally we could just call a not-yet-implemented tsdb.uIdExists()
-    // function.
+    // function. 
+    // Note, however, that this is optional. If autometric is enabled,  the UID will be assigned in call to addPoint().
     try {
       byteMetricUID = tsdb.getUID(UniqueIdType.METRIC, metricName);
     } catch (IllegalArgumentException iae) {
       System.out.println("Metric name not valid.");
       iae.printStackTrace();
+      System.exit(1);
     } catch (NoSuchUniqueName nsune) {
       // If not, great. Create it.
       byteMetricUID = tsdb.assignUid("metric", metricName);
@@ -72,135 +81,70 @@ public class AddDataExample {
     Map<String, String> tags = new HashMap<String, String>(1);
     tags.put("dummy-key", "dummy-val1");
 
-    // This is an optional errorback to handle when there is a failure.
-    class errBack implements Callback<String, Exception> {
-      public String call(final Exception e) throws Exception {
-        String message = ">>>>>>>>>>>Failure!>>>>>>>>>>>";
-        System.out.println(message);
-        return message;
-      }
-    }
-    ;
+   
 
+    
     // Start timer
-    long startTime1 = System.nanoTime();
-    // Write datum to tsdb
-    Deferred<Object> deferred = tsdb.addPoint(metricName, timestamp, value,
-        tags);
-
+    long startTime1 = System.currentTimeMillis();
+    
+    int n = 100;
+    ArrayList<Deferred<Object>> deferreds = new ArrayList<Deferred<Object>>(n);
+    for (int i = 0; i < n; i++) {
+      Deferred<Object> deferred = tsdb.addPoint(metricName, timestamp, value + i, tags);
+      deferreds.add(deferred);
+      
+    }
+    
     // Add the callbacks to the deferred object. (They might have already
     // returned, btw)
     // This will cause the calling thread to wait until the add has completed.
-    deferred.addErrback(new errBack());
-
+    
+    System.out.println("Waiting for deferred result to return...");
+    Deferred.groupInOrder(deferreds)
+        .addErrback(new AddDataExample().new errBack())
+        .addCallback(new AddDataExample().new succBack())
+        .join();
+    
+    // Block the thread until the deferred returns it's result.
+//    deferred.join();
+    
     // End timer.
-    long elapsedTime1 = (System.nanoTime() - startTime1) / (1000 * 1000);
-    System.out.println("\nAdding 1 point took: " + elapsedTime1
+    long elapsedTime1 = System.currentTimeMillis() - startTime1;
+    System.out.println("\nAdding " + n + " points took: " + elapsedTime1
         + " milliseconds.\n");
 
-    /*
-     * Now let's input a stream of data
-     */
-
-    // create an array for storing the deferreds of each addPoint() call
-    final int nDataPoints = 100;
-    final ArrayList<Deferred<Object>> deferreds = new ArrayList<Deferred<Object>>(
-        nDataPoints);
-    // Initialize which time series you're going to write to
-    WritableDataPoints wdp = tsdb.newDataPoints();
-    wdp.setSeries(metricName, tags); // you can recall this at any time to
-                                     // change the ts.
-    // Make some data
-    Random rand = new Random();
-    long[] timestamps1 = makeTimestamps(nDataPoints, rand);
-    long[] values1 = makeRandomValues(nDataPoints, rand);
-    // Start timer
-    long startTime2 = System.nanoTime();
-    // Now write to tsdb
-    for (int i = 0; i < nDataPoints; i++) {
-      // write the data and add the returned Deferred to our ArrayList
-      deferreds.add(wdp.addPoint(timestamps1[i], values1[i]));
-    }
-
-    try {
-      Deferred.groupInOrder(deferreds).addErrback(new errBack())
-          .joinUninterruptibly();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    // End timer.
-    long elapsedTime2 = (System.nanoTime() - startTime2) / (1000 * 1000);
-    System.out.println("\nAdding " + nDataPoints
-        + " points individually took: " + elapsedTime2 + " milliseconds.\n");
-
-    /*
-     * Now try writing in batch import mode
-     */
-    wdp.setBatchImport(true);
-    wdp.setBufferingTime((short) 50);
-    // Make some data
-    long[] timestamps3 = makeTimestamps(nDataPoints, rand);
-    long[] values3 = makeRandomValues(nDataPoints, rand);
-    // Start timer
-    long startTime3 = System.nanoTime();
-    // Now write to tsdb
-    for (int i = 0; i < nDataPoints; i++) {
-      // write the data and add the returned Deferred to our ArrayList
-      deferreds.add(wdp.addPoint(timestamps3[i], values3[i]));
-    }
-
-    try {
-      Deferred.groupInOrder(deferreds).addErrback(new errBack())
-          .joinUninterruptibly();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    // End timer.
-    long elapsedTime3 = (System.nanoTime() - startTime3) / (1000 * 1000);
-    System.out.println("\nAdding " + nDataPoints
-        + " points in batch mode took: " + elapsedTime3 + " milliseconds.\n");
-    
-    
-    /*
-     * Writing in batch import mode with reinstantiation of WriteableDataPoints connection
-     * This is needed if you want to write datapoints with the same time to the same metrics but
-     * that have different tags.  
-     */
-    
-    // Make some data
-    long timestamp4 = System.currentTimeMillis(); // one time
-    long[] values4 = makeRandomValues(nDataPoints, rand);
-    // Start timer
-    long startTime4 = System.nanoTime();
-    // Now write to tsdb
-    for (int i = 0; i < nDataPoints; i++) {
-      WritableDataPoints wdp2 = tsdb.newDataPoints();
-      wdp2.setBatchImport(true);
-      wdp2.setBufferingTime((short) 50);
-      Map<String, String> tags2 = new HashMap<String, String>(1);
-      tags2.put("some-tag", "version-" + i);
-      wdp2.setSeries(metricName, tags2);
-      // write the data and add the returned Deferred to our ArrayList
-      deferreds.add(wdp2.addPoint(timestamp4, values3[i]));
-    }
-
-    try {
-      Deferred.groupInOrder(deferreds).addErrback(new errBack())
-          .joinUninterruptibly();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    // End timer.
-    long elapsedTime4 = (System.nanoTime() - startTime3) / (1000 * 1000);
-    System.out.println("\nAdding " + nDataPoints
-        + " points in batch mode with WDP reinstantiation took: " + elapsedTime4 + " milliseconds.\n");
-    
 
     // Gracefully shutdown connection to TSDB
     tsdb.shutdown();
 
+    
   }
 
+  // This is an optional errorback to handle when there is a failure.
+  class errBack implements Callback<String, Exception> {
+    public String call(final Exception e) throws Exception {
+      String message = ">>>>>>>>>>>Failure!>>>>>>>>>>>";
+      System.err.println(message);
+      return message;
+    }
+  };
+
+  // This is an optional success callback to handle when there is a success.
+  class succBack implements Callback<Object, ArrayList<Object>> {
+    public Object call(ArrayList<Object> results) {
+      for (Object res : results) {
+        if (res != null) {
+          if (res.toString().equals("MultiActionSuccess")) {
+            System.err.println(">>>>>>>>>>>Success!>>>>>>>>>>>");
+          }
+        } else {
+          System.err.println(">>>>>>>>>>>" + res.getClass() + ">>>>>>>>>>>");
+        }
+      }
+      return null;
+    }
+  };
+  
   public static long[] makeRandomValues(int num, Random rand) {
     long[] values = new long[num];
     for (int i = 0; i < num; i++) {
