c39d8a848126d08e8961e191d380853ed19b9483
Chris Larsen <clarsen@yahoo-inc.com>
2015-03-25 14:36:40 -0700

    Fix the TsdbQuery salt query where it was writing the end timestamp incorrectly.
    Signed-off-by: Chris Larsen <clarsen@yahoo-inc.com>

--- src/core/TsdbQuery.java
+++ src/core/TsdbQuery.java
@@ -590,7 +590,7 @@ final class TsdbQuery implements Query {
     Bytes.setInt(end_row, (end_time == UNSET
                            ? -1  // Will scan until the end (0xFFF...).
                            : (int) getScanEndTimeSeconds()),
-                 metric_width);
+                           metric_salt_width);
     
     // set the metric UID based on the TSUIDs if given, or the metric UID
     if (tsuids != null && !tsuids.isEmpty()) {
