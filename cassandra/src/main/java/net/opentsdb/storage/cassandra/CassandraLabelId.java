12b728b181430548cef2646bb96d20ba1a8ec57e
Andreas Falk <mail@andreasfalk.se>
2015-05-19 21:39:24 +0200

    Add common object methods to CassandraLabelId

--- cassandra/src/main/java/net/opentsdb/storage/cassandra/CassandraLabelId.java
+++ cassandra/src/main/java/net/opentsdb/storage/cassandra/CassandraLabelId.java
@@ -1,9 +1,11 @@
 package net.opentsdb.storage.cassandra;
 
+import com.google.common.base.MoreObjects;
 import com.google.common.primitives.Longs;
 import net.opentsdb.uid.LabelId;
 
 import javax.annotation.Nonnull;
+import java.util.Objects;
 
 class CassandraLabelId implements LabelId<CassandraLabelId> {
   private final long id;
@@ -27,6 +29,30 @@ class CassandraLabelId implements LabelId<CassandraLabelId> {
 
   long toLong() {
     return id;
+  }
+
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+        .add("id", id)
+        .toString();
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+
+  @Override
+  public boolean equals(final Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null || getClass() != obj.getClass()) {
+      return false;
+    }
+    final CassandraLabelId other = (CassandraLabelId) obj;
+    return this.id == other.id;
   }
 
   @Override
