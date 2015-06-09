5fafdb73c1e7c7a546f1dc57a85eb7504f3ae962
Andreas Falk <mail@andreasfalk.se>
2015-06-09 12:05:28 +0200

    Import UncaughtExceptionHandler instead of having huge lines

--- core/src/main/java/net/opentsdb/utils/KillingUncaughtHandler.java
+++ core/src/main/java/net/opentsdb/utils/KillingUncaughtHandler.java
@@ -3,19 +3,20 @@ package net.opentsdb.utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.Thread.UncaughtExceptionHandler;
+
 /**
  * A default exception handler for threads. Will tell the JVM to exit when an exception bubbles all
  * the way up to a thread without being handled.
  * <p/>
- * See {@link java.lang.Thread.UncaughtExceptionHandler} for the exact order the handlers are
- * executed in.
+ * See {@link UncaughtExceptionHandler} for the exact order the handlers are executed in.
  */
-public class KillingUncaughtHandler implements Thread.UncaughtExceptionHandler {
+public class KillingUncaughtHandler implements UncaughtExceptionHandler {
   private static final Logger LOG = LoggerFactory.getLogger(KillingUncaughtHandler.class);
 
   public static void install() {
-    final Thread.UncaughtExceptionHandler previousHandler = Thread.getDefaultUncaughtExceptionHandler();
-    if (previousHandler instanceof KillingUncaughtHandler) {
+    final UncaughtExceptionHandler currentHandler = Thread.getDefaultUncaughtExceptionHandler();
+    if (currentHandler instanceof KillingUncaughtHandler) {
       return;
     }
     final KillingUncaughtHandler oomHandler = new KillingUncaughtHandler();
