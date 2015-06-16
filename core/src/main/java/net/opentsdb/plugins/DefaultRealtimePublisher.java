971addc5132605fcf485b87eb27c23621dc3868a
Andreas Falk <mail@andreasfalk.se>
2015-06-16 13:16:15 +0200

    Convert the real-time publisher and implementations to use futures

--- core/src/main/java/net/opentsdb/plugins/DefaultRealtimePublisher.java
+++ core/src/main/java/net/opentsdb/plugins/DefaultRealtimePublisher.java
@@ -4,15 +4,14 @@ import net.opentsdb.BuildData;
 import net.opentsdb.meta.Annotation;
 import net.opentsdb.uid.TimeseriesId;
 
-import com.stumbleupon.async.Deferred;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
 
 import java.util.Map;
 
 /**
- * A default realtime publisher to use when no other realtime publisher has been configured. This
- * realtime publisher will just discard all data given to it.
- *
- * @see net.opentsdb.core.TSDB
+ * A default realtime publisher to use when no other real-time publisher has been configured. This
+ * real-time publisher will just discard all data given to it.
  */
 public class DefaultRealtimePublisher extends RTPublisher {
   @Override
@@ -25,25 +24,25 @@ public class DefaultRealtimePublisher extends RTPublisher {
   }
 
   @Override
-  public Deferred<Object> publishDataPoint(final String metric,
-                                           final long timestamp,
-                                           final long value,
-                                           final Map<String, String> tags,
-                                           final TimeseriesId tsuid) {
-    return Deferred.fromResult(null);
+  public ListenableFuture<Void> publishDataPoint(final String metric,
+                                                 final long timestamp,
+                                                 final long value,
+                                                 final Map<String, String> tags,
+                                                 final TimeseriesId timeSeriesId) {
+    return Futures.immediateFuture(null);
   }
 
   @Override
-  public Deferred<Object> publishDataPoint(final String metric,
-                                           final long timestamp,
-                                           final double value,
-                                           final Map<String, String> tags,
-                                           final TimeseriesId tsuid) {
-    return Deferred.fromResult(null);
+  public ListenableFuture<Void> publishDataPoint(final String metric,
+                                                 final long timestamp,
+                                                 final double value,
+                                                 final Map<String, String> tags,
+                                                 final TimeseriesId timeSeriesId) {
+    return Futures.immediateFuture(null);
   }
 
   @Override
-  public Deferred<Object> publishAnnotation(final Annotation annotation) {
-    return Deferred.fromResult(null);
+  public ListenableFuture<Void> publishAnnotation(final Annotation annotation) {
+    return Futures.immediateFuture(null);
   }
 }
