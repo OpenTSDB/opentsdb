b5e9fa8097963c087e5a8d8fec89886f68d021e5
Andreas Falk <mail@andreasfalk.se>
2015-06-17 10:48:13 +0200

    Update StopTimerCallbackTest to use future methods

--- core/src/test/java/net/opentsdb/stats/StopTimerCallbackTest.java
+++ core/src/test/java/net/opentsdb/stats/StopTimerCallbackTest.java
@@ -5,15 +5,17 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import com.codahale.metrics.Timer;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
 import com.stumbleupon.async.Deferred;
 import org.junit.Test;
 
 public class StopTimerCallbackTest {
   @Test
   public void testStopOnCallsStopOnTimer() throws Exception {
-    Deferred<Object> d = Deferred.fromResult(null);
+    ListenableFuture<Object> f = Futures.immediateFuture(null);
     Timer.Context timerContext = mock(Timer.Context.class);
-    StopTimerCallback.stopOn(timerContext, d);
+    StopTimerCallback.stopOn(timerContext, f);
     verify(timerContext, times(1)).stop();
   }
 }
