b19430df376a05ec1b5dbe5efdea8bc18ad5c212
Joel Tjärnstig <joeltjarnstig@gmail.com>
2015-06-18 17:41:42 +0200

    JackonModule modified so we dont override setupModule anymore

--- web/src/main/java/net/opentsdb/web/jackson/JacksonModule.java
+++ web/src/main/java/net/opentsdb/web/jackson/JacksonModule.java
@@ -4,35 +4,19 @@ import net.opentsdb.meta.Annotation;
 import net.opentsdb.meta.LabelMeta;
 import net.opentsdb.uid.LabelId;
 
-import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.module.SimpleDeserializers;
 import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.databind.module.SimpleSerializers;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
+
 
 public class JacksonModule extends SimpleModule {
-  private final LabelId.LabelIdSerializer idSerializer;
-  private final LabelId.LabelIdDeserializer idDeserializer;
+
 
   public JacksonModule(final LabelId.LabelIdSerializer idSerializer,
                        final LabelId.LabelIdDeserializer idDeserializer) {
     super("JsonModule");
-    this.idSerializer = idSerializer;
-    this.idDeserializer = idDeserializer;
-  }
+      setMixInAnnotation(LabelMeta.class, LabelMetaMixIn.class);
+      setMixInAnnotation(Annotation.class, AnnotationMixIn.class);
 
-  @Override
-  public void setupModule(SetupContext context) {
-    context.setMixInAnnotations(LabelMeta.class, LabelMetaMixIn.class);
-    context.setMixInAnnotations(Annotation.class, AnnotationMixIn.class);
-
-    context.addSerializers(new SimpleSerializers(
-        ImmutableList.<JsonSerializer<?>>of(new LabelIdJsonSerializer(idSerializer))));
-
-    context.addDeserializers(new SimpleDeserializers(
-        ImmutableMap.<Class<?>, JsonDeserializer<?>>of(
-            LabelId.class, new LabelIdJsonDeserializer(idDeserializer))));
+      addSerializer(new LabelIdJsonSerializer(idSerializer));
+      addDeserializer(LabelId.class, new LabelIdJsonDeserializer(idDeserializer));
   }
 }
