24793dc5a5bd906eb08de0fab0b196b60a25720c
Andreas Falk <mail@andreasfalk.se>
2015-06-22 11:52:17 +0200

    Fix typos in TimeseriesId

--- core/src/main/java/net/opentsdb/uid/TimeseriesId.java
+++ core/src/main/java/net/opentsdb/uid/TimeseriesId.java
@@ -4,23 +4,23 @@ import java.util.List;
 import javax.annotation.Nonnull;
 
 /**
- * A general representation of timeseries IDs.
+ * A general representation of time series IDs.
  *
  * <p>Note: You should not use implementations of this interface as an argument, rather you should
- * use the components that make them up (metric and tags). This interface is inteded to be used by
+ * use the components that make them up (metric and tags). This interface is intended to be used by
  * implementations of {@link net.opentsdb.storage.TsdbStore TsdbStores} to hide their representation
- * of timeseries IDs and expose them in a general interface. The canonical use-case is for return
+ * of time series IDs and expose them in a general interface. The canonical use-case is for return
  * values on store implementations.
  */
 public abstract class TimeseriesId {
   /**
-   * The metric behind this timeseries ID.
+   * The metric behind this time series ID.
    */
   @Nonnull
   public abstract LabelId metric();
 
   /**
-   * The tags behind this timeseries ID.
+   * The tags behind this time series ID.
    */
   @Nonnull
   public abstract List<LabelId> tags();
