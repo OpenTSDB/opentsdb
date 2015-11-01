4b7ea0f11fa5490934fafbbd9f774ef7fcf8c441
Chris Larsen <clarsen@yahoo-inc.com>
2015-11-01 09:39:55 -0800

    Fix up the Scale expression: - Apply scale to all sub queries instead of only the first one - Fix the .size() issue in the data points array sizing - Cleanup, formatting and unit tests
    Signed-off-by: Chris Larsen <clarsen@yahoo-inc.com>

--- src/query/expression/Scale.java
+++ src/query/expression/Scale.java
@@ -12,6 +12,7 @@
 // see <http://www.gnu.org/licenses/>.
 package net.opentsdb.query.expression;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import net.opentsdb.core.DataPoint;
@@ -20,53 +21,90 @@ import net.opentsdb.core.MutableDataPoint;
 import net.opentsdb.core.SeekableView;
 import net.opentsdb.core.TSQuery;
 
+/**
+ * Multiplies each data point in the series by the given factor.
+ * @since 2.3
+ */
 public class Scale implements Expression {
+  
   @Override
-  public DataPoints[] evaluate(TSQuery data_query, List<DataPoints[]> queryResults, List<String> params) {
-    if (queryResults == null || queryResults.isEmpty()) {
-      throw new NullPointerException("Query results cannot be empty");
+  public DataPoints[] evaluate(final TSQuery data_query, 
+      final List<DataPoints[]> query_results, final List<String> params) {
+    if (data_query == null) {
+      throw new IllegalArgumentException("Missing time series query");
     }
-
+    if (query_results == null || query_results.isEmpty()) {
+      return new DataPoints[]{};
+    }
     if (params == null || params.isEmpty()) {
-      throw new NullPointerException("Scaling parameter not available");
+      throw new IllegalArgumentException("Missing scaling factor");
     }
 
-    String factor = params.get(0);
-    factor = factor.replaceAll("'|\"", "").trim();
-    double scaleFactor = Double.parseDouble(factor);
-
-    DataPoints[] inputPoints = queryResults.get(0);
-    DataPoints[] outputPoints = new DataPoints[inputPoints.length];
-
-    for (int i=0; i<inputPoints.length; i++) {
-      outputPoints[i] = scale(inputPoints[i], scaleFactor);
+    double scale_factor = 0; // zero is fine, if useless *shrug*
+    final String factor = params.get(0);
+    if (factor != null && factor.matches("^[-0-9\\.]+$")) {
+      try {
+        scale_factor = Double.parseDouble(factor);
+      } catch (NumberFormatException nfe) {
+        throw new IllegalArgumentException(
+            "Invalid parameter, must be an integer or floating point", nfe);
+      }
+    } else {
+      throw new IllegalArgumentException("Unparseable scale factor value: " 
+          + scale_factor);
     }
-
-    return outputPoints;
+    
+    int num_results = 0;
+    for (DataPoints[] results: query_results) {
+      num_results += results.length;
+    }
+    
+    final DataPoints[] results = new DataPoints[num_results];
+    int ix = 0;
+    // one or more sub queries (m=...&m=...&m=...)
+    for (final DataPoints[] sub_query_result : query_results) {
+      // group bys (m=sum:foo{host=*})
+      for (final DataPoints dps : sub_query_result) {
+        results[ix++] = scale(dps, scale_factor);
+      }
+    }
+    return results;
   }
-
-  protected DataPoints scale(DataPoints points, double scaleFactor) {
-    int size = points.size();
-    DataPoint[] dps = new DataPoint[size];
-
-    SeekableView view = points.iterator();
-    int i=0;
+  
+  /**
+   * Multiplies each data point in the series by the scale factor, maintaining
+   * integers if both the data point and scale are integers.
+   * @param points The data points to factor
+   * @param scale_factor The factor to multiply by
+   * @return The resulting data points
+   */
+  private DataPoints scale(final DataPoints points, final double scale_factor) {
+    // TODO(cl) - Using an array as the size function may not return the exact
+    // results and we should figure a way to avoid copying data anyway.
+    final List<DataPoint> dps = new ArrayList<DataPoint>();
+    final boolean scale_is_int = (scale_factor == Math.floor(scale_factor)) && 
+        !Double.isInfinite(scale_factor);
+    final SeekableView view = points.iterator();
     while (view.hasNext()) {
       DataPoint pt = view.next();
-      if (pt.isInteger()) {
-        dps[i] = MutableDataPoint.ofDoubleValue(pt.timestamp(), scaleFactor * pt.longValue());
+      if (pt.isInteger() && scale_is_int) {
+        dps.add(MutableDataPoint.ofLongValue(pt.timestamp(), 
+            (long)scale_factor * pt.longValue()));
       } else {
-        dps[i] = MutableDataPoint.ofDoubleValue(pt.timestamp(), scaleFactor * pt.doubleValue());
+        // NaNs are fine here, they'll just be re-computed as NaN
+        dps.add(MutableDataPoint.ofDoubleValue(pt.timestamp(), 
+            scale_factor * pt.toDouble()));
       }
-      i++;
     }
-
-    return new PostAggregatedDataPoints(points, dps);
+    final DataPoint[] results = new DataPoint[dps.size()];
+    dps.toArray(results);
+    return new PostAggregatedDataPoints(points, results);
   }
 
   @Override
-  public String writeStringField(List<String> queryParams, String innerExpression) {
-    return "scale(" + innerExpression + ")";
+  public String writeStringField(final List<String> query_params, 
+      final String inner_expression) {
+    return "scale(" + inner_expression + ")";
   }
 
 }
