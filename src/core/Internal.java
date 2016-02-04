60c2234c0bc8436aceee576c6ea4d3513db50676
Chris Larsen <clarsen@yahoo-inc.com>
2016-02-04 14:37:30 -0800

    Fix #684 by allowing the max unsigned value to return the max SIGNED value (With a note) so that if someone does bump their UIDs to the maximum allowed it will properly display the stats (at least until Long.MAX_VALUE uids are assigned, then it will be funky.)
    Signed-off-by: Chris Larsen <clarsen@yahoo-inc.com>

--- src/core/Internal.java
+++ src/core/Internal.java
@@ -888,17 +888,23 @@ public final class Internal {
   }
 
   /**
-   * Simple helper to calculate the max value for any width of long from 0 to 7
+   * Simple helper to calculate the max value for any width of long from 0 to 8
    * bytes. 
    * @param width The width of the byte array we're comparing
-   * @return The maximum unsigned integer value on {@link width} bytes.
+   * @return The maximum unsigned integer value on {@link width} bytes. Note:
+   * If you ask for 8 bytes, it will return the max signed value. This is due
+   * to Java lacking unsigned integers... *sigh*.
    * @since 2.2
    */
   public static long getMaxUnsignedValueOnBytes(final int width) {
-    if (width < 0 || width > 7) {
-      throw new IllegalArgumentException("Width must be from 1 to 7 bytes: " 
+    if (width < 0 || width > 8) {
+      throw new IllegalArgumentException("Width must be from 1 to 8 bytes: " 
           + width);
     }
-    return ((long) 1 << width * Byte.SIZE) - 1;
+    if (width < 8) {
+      return ((long) 1 << width * Byte.SIZE) - 1;
+    } else {
+      return Long.MAX_VALUE;
+    }
   }
 }
