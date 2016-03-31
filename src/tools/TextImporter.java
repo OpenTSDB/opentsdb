9d82a4e0ad31829ffc6c9c58190c171b60c9a4b6
pinifaran <pini@pontis.com>
2016-03-31 10:45:20 +0300

    Update TextImporter.java

--- src/tools/TextImporter.java
+++ src/tools/TextImporter.java
@@ -189,7 +189,7 @@ final class TextImporter {
           }
           
           final WritableDataPoints dp = getDataPoints(tsdb, metric, tags);
-          Deferred<Object> d;
+          Deferred<? extends Object> d;
           if (Tags.looksLikeInteger(value)) {
             d = dp.addPoint(timestamp, Tags.parseLong(value));
           } else {  // floating point value
