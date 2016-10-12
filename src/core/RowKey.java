b39763e70f66c98d9bbe7afd9aa75d1f3f96d0bc
murphy66 <murphy66@gmail.com>
2016-10-12 18:33:50 +0200

    Fix empty array check. (#876)

--- src/core/RowKey.java
+++ src/core/RowKey.java
@@ -55,7 +55,7 @@ final public class RowKey {
    */
   public static Deferred<String> metricNameAsync(final TSDB tsdb, 
       final byte[] row) {
-    if (row == null || row.length < 0) {
+    if (row == null || row.length < 1) {
       throw new IllegalArgumentException("Row key cannot be null or empty");
     }
     if (row.length < Const.SALT_WIDTH() + tsdb.metrics.width()) {
