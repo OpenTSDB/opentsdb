fb8755e2309f757fc089ca6335ec7bd3b2181608
Chris Larsen <clarsen@yahoo-inc.com>
2017-06-11 21:45:20 -0700

    Bump AsyncHBase client to 1.8.0 for HBase 1.3.x compat.
    Signed-off-by: Chris Larsen <clarsen@yahoo-inc.com>

--- third_party/hbase/include.mk
+++ third_party/hbase/include.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2011-2016  The OpenTSDB Authors.
+# Copyright (C) 2011-2017  The OpenTSDB Authors.
 #
 # This library is free software: you can redistribute it and/or modify it
 # under the terms of the GNU Lesser General Public License as published
@@ -13,9 +13,9 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
-ASYNCHBASE_VERSION := 1.8.0-20161127.193259-5
+ASYNCHBASE_VERSION := 1.8.0
 ASYNCHBASE := third_party/hbase/asynchbase-$(ASYNCHBASE_VERSION).jar
-ASYNCHBASE_BASE_URL := https://oss.sonatype.org/content/repositories/snapshots/org/hbase/asynchbase/1.8.0-SNAPSHOT/
+ASYNCHBASE_BASE_URL := http://central.maven.org/maven2/org/hbase/asynchbase/$(ASYNCHBASE_VERSION)
 
 $(ASYNCHBASE): $(ASYNCHBASE).md5
 	set dummy "$(ASYNCHBASE_BASE_URL)" "$(ASYNCHBASE)"; shift; $(FETCH_DEPENDENCY)
