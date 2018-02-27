ea743c8316c01808b03791c0102a5d47842aa5e8
Chris Larsen <clarsen@yahoo-inc.com>
2018-02-27 11:32:19 -0800

    COMMON: - Add a span to the TimeSeriesByteId decod method for tracing.
    CORE:
    - Add new interfaces starting the TSDB 1.x schema implementation.

---
+++ core/src/main/java/net/opentsdb/storage/StorageSchema.java
@@ -0,0 +1,55 @@
+// This file is part of OpenTSDB.
+// Copyright (C) 2018  The OpenTSDB Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package net.opentsdb.storage;
+
+import com.stumbleupon.async.Deferred;
+
+import net.opentsdb.data.TimeSeriesByteId;
+import net.opentsdb.data.TimeSeriesStringId;
+import net.opentsdb.stats.Span;
+
+/**
+ * The base interface used to implement a storage schema, i.e. encoding
+ * or decoding time series data.
+ * 
+ * @since 3.0
+ */
+public interface StorageSchema {
+
+  /** @return A non-null and non-empty unique runtime ID for this storage
+   * schema instance. Multiple schemas of the same type can be registered
+   * at runtime. */
+  public String id();
+  
+  /** @return A non-null and non-empty name for the schema unique amongst
+   * all schemas, used during instantiation. */
+  public String name();
+  
+  /** @return A non-null and non-emtpy description of the schema. */
+  public String description();
+  
+  /**
+   * Resolve the given byte ID to the string ID if applicable.
+   * @param id A non-null ID encoded with this schema.
+   * @param span An optional tracing span.
+   * @return A deferred resolving to the string ID or an exception if
+   * something goes wrong.
+   * @throws IllegalArgumentException if the ID was null or the ID was
+   * encoded with a different schema.
+   */
+  public Deferred<TimeSeriesStringId> resolveByteId(final TimeSeriesByteId id, 
+                                                    final Span span);
+  
+}
---
+++ core/src/main/java/net/opentsdb/storage/schemas/tsdb1x/Codec.java
@@ -0,0 +1,38 @@
+// This file is part of OpenTSDB.
+// Copyright (C) 2018  The OpenTSDB Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package net.opentsdb.storage.schemas.tsdb1x;
+
+import com.google.common.reflect.TypeToken;
+
+import net.opentsdb.data.TimeSeriesDataType;
+
+/**
+ * A class that will return a storage object that can be populated
+ * by the storage system with data. It's the shim between a
+ * TimeSeries entry and storage. Codecs for each type of data handled
+ * by a schema should be registered in the schema.
+ * 
+ * @since 3.0
+ */
+public interface Codec {
+
+  /** @return The non-null token refering to the type of data returned
+   * by this codec. */
+  public TypeToken<? extends TimeSeriesDataType> type();
+  
+  /** @return A non-null time series from storage. */
+  public StorageSeries newIterable();
+  
+}
