85b2919f5ac667224ffc1ed8a9dc2977f0cdf299
Chris Larsen <clarsen@yahoo-inc.com>
2018-03-02 16:49:30 -0800

    COMMON: - Add Query downstream and upstream exceptions
    CORE:
    - Add handlers to AbstractQueryNode to handle passing results upstream to
      all of the subscribers. And start UTs for it.

--- core/src/main/java/net/opentsdb/query/AbstractQueryNode.java
+++ core/src/main/java/net/opentsdb/query/AbstractQueryNode.java
@@ -16,6 +16,11 @@ package net.opentsdb.query;
 
 import java.util.Collection;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.opentsdb.exceptions.QueryUpstreamException;
+
 /**
  * A base class for nodes that holds a link to the context, upstream and 
  * downstream nodes.
@@ -23,6 +28,9 @@ import java.util.Collection;
  * @since 3.0
  */
 public abstract class AbstractQueryNode implements QueryNode {
+  private static final Logger LOG = 
+      LoggerFactory.getLogger(AbstractQueryNode.class);
+  
   /** A reference to the query node factory that generated this node. */
   protected QueryNodeFactory factory;
   
@@ -66,5 +74,72 @@ public abstract class AbstractQueryNode implements QueryNode {
 
   public QueryNodeFactory factory() {
     return factory;
+  }
+
+  /**
+   * Sends the result to each of the upstream subscribers.
+   * 
+   * @param result A non-null result.
+   * @throws QueryUpstreamException if the upstream 
+   * {@link #onNext(QueryResult)} handler throws an exception. I hate
+   * checked exceptions but each node needs to be able to handle this
+   * ideally by cancelling the query.
+   * @throws IllegalArgumentException if the result was null.
+   */
+  protected void sendUpstream(final QueryResult result) 
+        throws QueryUpstreamException {
+    if (result == null) {
+      throw new IllegalArgumentException("Result cannot be null.");
+    }
+    
+    for (final QueryNode node : upstream) {
+      try {
+        node.onNext(result);
+      } catch (Exception e) {
+        throw new QueryUpstreamException("Failed to send results "
+            + "upstream to node: " + node, e);
+      }
+    }
+  }
+  
+  /**
+   * Sends the throwable upstream to each of the subscribing nodes. If 
+   * one or more upstream consumers throw an exception, it's caught and
+   * logged as a warning.
+   * 
+   * @param t A non-null throwable.
+   * @throws IllegalArgumentException if the throwable was null.
+   */
+  protected void sendUpstream(final Throwable t) {
+    if (t == null) {
+      throw new IllegalArgumentException("Throwable cannot be null.");
+    }
+    
+    for (final QueryNode node : upstream) {
+      try {
+        node.onError(t);
+      } catch (Exception e) {
+        LOG.warn("Failed to send exception upstream to node: " + node, e);
+      }
+    }
+  }
+  
+  /**
+   * Passes the sequence info upstream to all subscribers. If one or 
+   * more upstream consumers throw an exception, it's caught and logged 
+   * as a warning.
+   * 
+   * @param final_sequence The final sequence number to pass.
+   * @param total_sequences The total sequence count to pass.
+   */
+  protected void completeUpstream(final long final_sequence,
+                                  final long total_sequences) {
+    for (final QueryNode node : upstream) {
+      try {
+        node.onComplete(this, final_sequence, total_sequences);
+      } catch (Exception e) {
+        LOG.warn("Failed to mark upstream node complete: " + node, e);
+      }
+    }
   }
 }
