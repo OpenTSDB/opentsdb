817905e80d31e94a9987283056c51c6c47427f96
Chris Larsen <clarsen@yahoo-inc.com>
2018-03-08 10:49:30 -0800

    CORE: - Move FLAG_BITS and FLAG_FLOAT into the Schema class - Add a few method stubs we'll need in the Schema class - Move RollupUtils into core
    Signed-off-by: Chris Larsen <clarsen@yahoo-inc.com>

--- core/src/test/java/net/opentsdb/rollup/TestRollupUtils.java
+++ core/src/test/java/net/opentsdb/rollup/TestRollupUtils.java
@@ -1,15 +1,17 @@
 // This file is part of OpenTSDB.
-// Copyright (C) 2010-2015  The OpenTSDB Authors.
+// Copyright (C) 2015-2018  The OpenTSDB Authors.
 //
-// This program is free software: you can redistribute it and/or modify it
-// under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 2.1 of the License, or (at your
-// option) any later version.  This program is distributed in the hope that it
-// will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
-// General Public License for more details.  You should have received a copy
-// of the GNU Lesser General Public License along with this program.  If not,
-// see <http://www.gnu.org/licenses/>.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package net.opentsdb.rollup;
 
 import static org.junit.Assert.assertArrayEquals;
@@ -21,6 +23,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import net.opentsdb.core.Const;
+import net.opentsdb.storage.schemas.tsdb1x.Schema;
 
 public class TestRollupUtils {
   private static final String temporal_table = "tsdb-rollup-10m";
@@ -1051,7 +1054,7 @@ public class TestRollupUtils {
     System.arraycopy(offset, 0, expected_qual, 1, 2);
     
     final byte[] q = RollupUtils.buildRollupQualifier(1370532925L, 1370530800, 
-        (byte) ( 7 | Const.FLAG_FLOAT), 42, hour_interval);
+        (byte) ( 7 | Schema.FLAG_FLOAT), 42, hour_interval);
 
     assertArrayEquals(expected_qual, q);
   }
@@ -1064,7 +1067,7 @@ public class TestRollupUtils {
     System.arraycopy(offset, 0, expected_qual, 1, 2);
 
     final byte[] q = RollupUtils.buildRollupQualifier(1370532925L, 1370530800, 
-        (byte) ( 3 | Const.FLAG_FLOAT), 42, hour_interval);
+        (byte) ( 3 | Schema.FLAG_FLOAT), 42, hour_interval);
 
     assertArrayEquals(expected_qual, q);
   }
