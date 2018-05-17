60973c478e28ecab41907915c40810678a87a313
Chris Larsen <clarsen@yahoo-inc.com>
2018-05-17 14:42:39 -0700

    COMMON: - Change the auth classes to interfaces and clean them up to be more generic.

--- common/src/main/java/net/opentsdb/auth/AuthState.java
+++ common/src/main/java/net/opentsdb/auth/AuthState.java
@@ -1,25 +1,23 @@
 // This file is part of OpenTSDB.
-// Copyright (C) 2017  The OpenTSDB Authors.
+// Copyright (C) 2017-2018 The OpenTSDB Authors.
 //
-// This program is free software: you can redistribute it and/or modify it
-// under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 2.1 of the License, or (at your
-// option) any later version.  This program is distributed in the hope that it
-// will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
-// General Public License for more details.  You should have received a copy
-// of the GNU Lesser General Public License along with this program.  If not,
-// see <http://www.gnu.org/licenses/>.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package net.opentsdb.auth;
 
-import org.jboss.netty.channel.Channel;
+import java.security.Principal;
 
 /**
  * An interface for use in authentication and authorization of calls.
- * <p>
- * When used for successful authentication, this stage object will be attached 
- * to the Netty Channel via it's attachment method. It can then be read out of
- * the channel and used for authorization calls.
  * 
  * @since 2.4
  */
@@ -46,6 +44,12 @@ public interface AuthState {
   public String getUser();
   
   /**
+   * Returns the non-null Java principal representing the user.
+   * @return A non-null principal.
+   */
+  public Principal getPrincipal();
+  
+  /**
    * The status associated with this authentication or authorization state.
    * @return A non-null auth status enumerator value.
    */
@@ -64,14 +68,6 @@ public interface AuthState {
    * @return An optional exception; may be null.
    */
   public Throwable getException();
-  
-  /**
-   * Sets the channel associated with this state when used for authentication
-   * and attached to a channel. Useful for associating connection information
-   * (IP and port) with the user.
-   * @param channel A non-null channel.
-   */
-  public void setChannel(final Channel channel);
   
   /**
    * An optional token to use with an authentication system for validating that
