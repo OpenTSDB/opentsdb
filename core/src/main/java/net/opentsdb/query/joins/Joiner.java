8292ecbaefc88ab44d8c57f84063e04aecada9b7
Chris Larsen <clarsen@yahoo-inc.com>
2018-07-04 12:03:57 -0700

    CORE: - Move Joiner to the joins package.

--- core/src/main/java/net/opentsdb/query/joins/Joiner.java
+++ core/src/main/java/net/opentsdb/query/joins/Joiner.java
@@ -12,7 +12,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-package net.opentsdb.query.processor;
+package net.opentsdb.query.joins;
 
 import java.io.IOException;
 import java.util.Collections;
