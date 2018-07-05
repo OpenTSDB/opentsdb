62a0e05f91d8873c5735aeac7dd4f20ff60753ac
Chris Larsen <clarsen@yahoo-inc.com>
2018-07-05 10:58:32 -0700

    CORE: - Add the KeyedHashedJoinSet implementation.
    Signed-off-by: Chris Larsen <clarsen@yahoo-inc.com>

---
+++ core/src/main/java/net/opentsdb/query/joins/KeyedHashedJoinSet.java
@@ -0,0 +1,107 @@
+// This file is part of OpenTSDB.
+// Copyright (C) 2018  The OpenTSDB Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package net.opentsdb.query.joins;
+
+import java.util.List;
+
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
+
+import gnu.trove.map.hash.TLongObjectHashMap;
+import net.opentsdb.data.TimeSeries;
+import net.opentsdb.query.joins.JoinConfig.JoinType;
+
+/**
+ * A default implementation for the {@link BaseHashedJoinSet} that simply
+ * routes a time series to the left or right map based on a string key.
+ * Handles appending series to a list in the maps.
+ * <p>
+ * <bNote:</b> The implementation doesn't check to see if the same 
+ * time series is in the left or right map.
+ * 
+ * @since 3.0
+ */
+public class KeyedHashedJoinSet extends BaseHashedJoinSet {
+  
+  /** The identifier that maps to the left map. */
+  protected final String left_key;
+  
+  /** The identifier that maps to the right map. */
+  protected final String right_key;
+  
+  /**
+   * Default ctor.
+   * @param type A non-null join type.
+   * @param left_key A non-null and non-empty string mapping to the left
+   * map.
+   * @param right_key A non-null and non-empty string mapping to the right
+   * map.
+   * @throws IllegalArgumentException if any of the args were null or empty.
+   */
+  protected KeyedHashedJoinSet(final JoinType type, 
+                               final String left_key, 
+                               final String right_key) {
+    super(type);
+    if (type == null) {
+      throw new IllegalArgumentException("Join type cannot be null.");
+    }
+    if (Strings.isNullOrEmpty(left_key)) {
+      throw new IllegalArgumentException("Left key cannot be null.");
+    }
+    if (Strings.isNullOrEmpty(right_key)) {
+      throw new IllegalArgumentException("Right key cannot be null.");
+    }
+    this.left_key = left_key;
+    this.right_key = right_key;
+  }
+  
+  /**
+   * Package private method to add a value to the proper map.
+   * @param key A non-null and non-empty key mapping to the left or right
+   * map.
+   * @param hash The hash for this time series.
+   * @param ts A non-null time series.
+   */
+  void add(final String key, final long hash, final TimeSeries ts) {
+    if (ts == null) {
+      throw new IllegalArgumentException("Time series can't be null.");
+    }
+    if (key.equals(left_key)) {
+      if (left_map == null) {
+        left_map = new TLongObjectHashMap<List<TimeSeries>>();
+      }
+      List<TimeSeries> series = left_map.get(hash);
+      if (series == null) {
+        series = Lists.newArrayList();
+        left_map.put(hash, series);
+      }
+      series.add(ts);
+    } else if (key.equals(right_key)) {
+      if (right_map == null) {
+        right_map = new TLongObjectHashMap<List<TimeSeries>>();
+      }
+      List<TimeSeries> series = right_map.get(hash);
+      if (series == null) {
+        series = Lists.newArrayList();
+        right_map.put(hash, series);
+      }
+      series.add(ts);
+    } else {
+      throw new IllegalArgumentException("Key didn't match the left "
+          + "or right.");
+    }
+  }
+  
+}
---
+++ core/src/test/java/net/opentsdb/query/joins/TestKeyedHashedJoinSet.java
@@ -0,0 +1,155 @@
+// This file is part of OpenTSDB.
+// Copyright (C) 2018  The OpenTSDB Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package net.opentsdb.query.joins;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import net.opentsdb.query.joins.JoinConfig.JoinType;
+
+public class TestKeyedHashedJoinSet extends BaseJoinTest {
+
+  private static final String LEFT = "left";
+  private static final String RIGHT = "right";
+  
+  @Test
+  public void ctor() throws Exception {
+    KeyedHashedJoinSet set = new KeyedHashedJoinSet(JoinType.INNER, 
+        LEFT, RIGHT);
+    assertEquals(JoinType.INNER, set.type);
+    assertEquals(LEFT, set.left_key);
+    assertEquals(RIGHT, set.right_key);
+    assertNull(set.left_map);
+    assertNull(set.right_map);
+    
+    try {
+      new KeyedHashedJoinSet(null, LEFT, RIGHT);
+      fail("Expected IllegalArgumentException");
+    } catch (IllegalArgumentException e) { }
+    
+    try {
+      new KeyedHashedJoinSet(JoinType.INNER, null, RIGHT);
+      fail("Expected IllegalArgumentException");
+    } catch (IllegalArgumentException e) { }
+    
+    try {
+      new KeyedHashedJoinSet(JoinType.INNER, "", RIGHT);
+      fail("Expected IllegalArgumentException");
+    } catch (IllegalArgumentException e) { }
+    
+    try {
+      new KeyedHashedJoinSet(JoinType.INNER, LEFT, null);
+      fail("Expected IllegalArgumentException");
+    } catch (IllegalArgumentException e) { }
+    
+    try {
+      new KeyedHashedJoinSet(JoinType.INNER, LEFT, "");
+      fail("Expected IllegalArgumentException");
+    } catch (IllegalArgumentException e) { }
+  }
+  
+  @Test
+  public void add() throws Exception {
+    KeyedHashedJoinSet set = new KeyedHashedJoinSet(JoinType.INNER, 
+        LEFT, RIGHT);
+    
+    // make leftAndRightSet().
+    set.add(LEFT, 1, L_1);
+    set.add(RIGHT, 1, R_1);
+    
+    set.add(LEFT, 2, L_2);
+    
+    set.add(RIGHT, 3, R_3);
+    
+    set.add(LEFT, 4, L_4);
+    set.add(RIGHT, 4, R_4A);
+    set.add(RIGHT, 4, R_4B);
+    
+    set.add(LEFT, 5, L_5A);
+    set.add(LEFT, 5, L_5B);
+    set.add(RIGHT, 5, R_5);
+    
+    set.add(LEFT, 6, L_6A);
+    set.add(LEFT, 6, L_6B);
+    set.add(RIGHT, 6, R_6B);
+    set.add(RIGHT, 6, R_6B);
+    
+    assertEquals(5, set.left_map.size());
+    assertEquals(5, set.right_map.size());
+    
+    assertEquals(1, set.left_map.get(1).size());
+    assertSame(L_1, set.left_map.get(1).get(0));
+    assertEquals(1, set.right_map.get(1).size());
+    assertSame(R_1, set.right_map.get(1).get(0));
+    
+    assertEquals(1, set.left_map.get(2).size());
+    assertSame(L_2, set.left_map.get(2).get(0));
+    assertNull(set.right_map.get(2));
+    
+    assertNull(set.left_map.get(3));
+    assertEquals(1, set.right_map.get(3).size());
+    assertSame(R_3, set.right_map.get(3).get(0));
+    
+    assertEquals(1, set.left_map.get(4).size());
+    assertSame(L_4, set.left_map.get(4).get(0));
+    assertEquals(2, set.right_map.get(4).size());
+    assertSame(R_4A, set.right_map.get(4).get(0));
+    assertSame(R_4B, set.right_map.get(4).get(1));
+    
+    assertEquals(2, set.left_map.get(5).size());
+    assertSame(L_5A, set.left_map.get(5).get(0));
+    assertSame(L_5B, set.left_map.get(5).get(1));
+    assertEquals(1, set.right_map.get(5).size());
+    assertSame(R_5, set.right_map.get(5).get(0));
+    
+    assertEquals(2, set.left_map.get(6).size());
+    assertSame(L_6A, set.left_map.get(6).get(0));
+    assertSame(L_6B, set.left_map.get(6).get(1));
+    assertEquals(2, set.right_map.get(6).size());
+    assertSame(R_6B, set.right_map.get(6).get(0));
+    assertSame(R_6B, set.right_map.get(6).get(1));
+    
+    // allow dupes... grr
+    set.add(LEFT, 1, L_1);
+    assertEquals(2, set.left_map.get(1).size());
+    assertSame(L_1, set.left_map.get(1).get(0));
+    assertSame(L_1, set.left_map.get(1).get(1));
+    
+    // don't care where it lands
+    set.add(RIGHT, 1, L_1);
+    assertEquals(2, set.right_map.get(1).size());
+    assertSame(R_1, set.right_map.get(1).get(0));
+    assertSame(L_1, set.right_map.get(1).get(1));
+    
+    try {
+      set.add(null, 0, L_1);
+      fail("Expected NullPointerException");
+    } catch (NullPointerException e) { }
+    
+    try {
+      set.add(LEFT, 0, null);
+      fail("Expected IllegalArgumentException");
+    } catch (IllegalArgumentException e) { }
+    
+    try {
+      set.add("nosuchkey", 0, L_1);
+      fail("Expected IllegalArgumentException");
+    } catch (IllegalArgumentException e) { }
+  }
+}
