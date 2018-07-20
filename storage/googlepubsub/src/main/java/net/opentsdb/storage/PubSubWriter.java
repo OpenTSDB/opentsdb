ab1159a504bfc2ea46566d9d9fee409d9cfa6043
Chris Larsen <clarsen@yahoo-inc.com>
2018-07-20 14:54:58 -0700

    GOOGLEPUBSUB: - Add a writable implementation for raw data of the Google PubSub cloud offering.
    Signed-off-by: Chris Larsen <clarsen@yahoo-inc.com>

---
+++ storage/googlepubsub/src/main/java/net/opentsdb/storage/PubSubWriter.java
@@ -0,0 +1,299 @@
+// This file is part of OpenTSDB.
+// Copyright (C) 2015-2018  The OpenTSDB Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package net.opentsdb.storage;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.api.client.repackaged.com.google.common.base.Strings;
+import com.google.api.core.ApiFuture;
+import com.google.api.core.ApiFutureCallback;
+import com.google.api.core.ApiFutures;
+import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.api.gax.rpc.ApiException;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.pubsub.v1.Publisher;
+import com.google.common.collect.Lists;
+import com.google.protobuf.ByteString;
+import com.google.pubsub.v1.ProjectTopicName;
+import com.google.pubsub.v1.PubsubMessage;
+import com.stumbleupon.async.Deferred;
+
+import net.opentsdb.auth.AuthState;
+import net.opentsdb.core.TSDB;
+import net.opentsdb.core.TSDBPlugin;
+import net.opentsdb.data.TimeSeriesDatum;
+import net.opentsdb.data.TimeSeriesSharedTagsAndTimeData;
+import net.opentsdb.query.serdes.TimeSeriesDataSerdes;
+import net.opentsdb.stats.Span;
+
+/**
+ * A simple publisher for a Google PubSub topic for data points. Stub.
+ * 
+ * TODO - support for different credentials fetching methods
+ * TODO - tuning
+ * 
+ * @since 3.0
+ */
+public class PubSubWriter implements WritableTimeSeriesDataStore, TSDBPlugin {
+  protected static final Logger LOG = LoggerFactory.getLogger(PubSubWriter.class);
+  
+  /** Configuration keys. */
+  public static final String PROJECT_NAME_KEY = "google.pubsub.publisher.project.id";
+  public static final String TOPIC_KEY = "google.pubsub.publisher.topic";
+  public static final String JSON_KEYFILE_KEY = "google.pubsub.publisher.auth.json.keyfile";
+  
+  /** The publisher instance we write with. */
+  protected Publisher publisher;
+  
+  /** The name of the topic we're writing to. */
+  protected ProjectTopicName topic;
+  
+  /** The serdes implementation to use. */
+  protected TimeSeriesDataSerdes serdes;
+  
+  /**
+   * Default ctor.
+   */
+  public PubSubWriter() {
+  }
+  
+  @Override
+  public Deferred<WriteStatus> write(final AuthState state, 
+                                     final TimeSeriesDatum datum,
+                                     final Span span) {
+    final Span child;
+    if (span != null) {
+      child = span.newChild(getClass().getName() + ".write").start();
+    } else {
+      child = null;
+    }
+    
+    final Deferred<WriteStatus> deferred = new Deferred<WriteStatus>();
+    try {
+      final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+      serdes.serialize(null, datum, baos, null);
+      final PubsubMessage message = PubsubMessage.newBuilder()
+          .setData(ByteString.copyFrom(baos.toByteArray()))
+          .build();
+      
+      final ApiFuture<String> future = publisher.publish(message);
+      ApiFutures.addCallback(future, new ApiFutureCallback<String>() {
+  
+        @Override
+        public void onFailure(final Throwable throwable) {
+          if (LOG.isDebugEnabled()) {
+            LOG.debug("Exception publishing to the PubSub endpoint", 
+                throwable);
+          }
+          if (child != null) {
+            child.setErrorTags(throwable).finish();
+          }
+          if (throwable instanceof ApiException) {
+            final ApiException api_exception = ((ApiException) throwable);
+            if (api_exception.isRetryable()) {
+              deferred.callback(WriteStatus.retry(
+                  api_exception.getStatusCode().getCode().toString()));
+            } else {
+              deferred.callback(WriteStatus.error(
+                  api_exception.getStatusCode().getCode().toString(), 
+                  throwable));
+            }
+          } else {
+            deferred.callback(WriteStatus.error(
+                throwable.getMessage(), throwable));
+          }
+        }
+  
+        @Override
+        public void onSuccess(final String id) {
+          if (child != null) {
+            child.setSuccessTags().finish();
+          }
+          deferred.callback(WriteStatus.OK);
+        }
+      });
+      return deferred;
+    } catch (Exception e) {
+      if (child != null) {
+        child.setErrorTags(e).finish();
+      }
+      LOG.error("Unexpected exception publishing message", e);
+      return Deferred.fromResult(WriteStatus.error(e.getMessage(), e));
+    }
+  }
+
+  @Override
+  public Deferred<List<WriteStatus>> write(final AuthState state,
+                                           final TimeSeriesSharedTagsAndTimeData data, 
+                                           final Span span) {
+    final Span child;
+    if (span != null) {
+      child = span.newChild(getClass().getName() + ".write").start();
+    } else {
+      child = null;
+    }
+    
+    final Deferred<List<WriteStatus>> deferred = 
+        new Deferred<List<WriteStatus>>();
+    
+    try {
+      final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+      serdes.serialize(null, data, baos, null);
+      final PubsubMessage message = PubsubMessage.newBuilder()
+          .setData(ByteString.copyFrom(baos.toByteArray()))
+          .build();
+      
+      final ApiFuture<String> future = publisher.publish(message);
+      ApiFutures.addCallback(future, new ApiFutureCallback<String>() {
+  
+        @Override
+        public void onFailure(final Throwable throwable) {
+          if (LOG.isDebugEnabled()) {
+            LOG.debug("Exception publishing to the PubSub endpoint", 
+                throwable);
+          }
+          if (child != null) {
+            child.setErrorTags(throwable).finish();
+          }
+          if (throwable instanceof ApiException) {
+            final ApiException api_exception = ((ApiException) throwable);
+            if (api_exception.isRetryable()) {
+              deferred.callback(buildList(WriteStatus.retry(
+                  api_exception.getStatusCode().getCode().toString()), 
+                  data.size()));
+            } else {
+              deferred.callback(buildList(WriteStatus.error(
+                  api_exception.getStatusCode().getCode().toString(), 
+                    throwable), 
+                  data.size()));
+            }
+          } else {
+            deferred.callback(buildList(WriteStatus.error(
+                throwable.getMessage(), throwable), data.size()));
+          }
+        }
+  
+        @Override
+        public void onSuccess(final String id) {
+          if (child != null) {
+            child.setSuccessTags().finish();
+          }
+          deferred.callback(buildList(WriteStatus.OK, data.size()));
+        }
+      });
+      return deferred;
+    } catch (Exception e) {
+      if (child != null) {
+        child.setErrorTags(e).finish();
+      }
+      LOG.error("Unexpected exception publishing message", e);
+      return Deferred.fromResult(buildList(
+          WriteStatus.error(e.getMessage(), e),
+          data.size()));
+    }
+  }
+
+  @Override
+  public String id() {
+    return "GooglePubSub";
+  }
+
+  @Override
+  public Deferred<Object> initialize(final TSDB tsdb) {
+    registerConfigs(tsdb);
+    final String project_id = tsdb.getConfig().getString(PROJECT_NAME_KEY);
+    if (Strings.isNullOrEmpty(project_id)) {
+      return Deferred.fromError(new IllegalArgumentException(
+          "Project ID cannot be null or empty."));
+    }
+    final String topic_id = tsdb.getConfig().getString(TOPIC_KEY);
+    if (Strings.isNullOrEmpty(topic_id)) {
+      return Deferred.fromError(new IllegalArgumentException(
+          "Topic ID cannot be null or empty."));
+    }
+    final String json_key = tsdb.getConfig().getString(JSON_KEYFILE_KEY);
+    if (Strings.isNullOrEmpty(json_key)) {
+      return Deferred.fromError(new IllegalArgumentException(
+          "Json key path cannot be null or empty."));
+    }
+    
+    serdes = tsdb.getRegistry().getDefaultPlugin(TimeSeriesDataSerdes.class);
+    if (serdes == null) {
+      return Deferred.fromError(new IllegalArgumentException(
+          "Serdes can't be null."));
+    }
+    
+    try {
+      final GoogleCredentials credentials = GoogleCredentials.fromStream(
+          new FileInputStream(json_key));
+      topic = ProjectTopicName.of(project_id, topic_id);
+      publisher = Publisher.newBuilder(topic)
+//          .setBatchingSettings(BatchingSettings.newBuilder()
+//              .setElementCountThreshold(950L)
+//              .setRequestByteThreshold(9500000L)
+//              .build())
+          .setCredentialsProvider(
+              FixedCredentialsProvider.create(credentials))
+          .build();
+      LOG.info("Successfully initialized Google PubSub for topic: " + topic_id);
+    } catch (FileNotFoundException e) {
+      return Deferred.fromError(e);
+    } catch (IOException e) {
+      return Deferred.fromError(e);
+    }
+
+    return Deferred.fromResult(null);
+  }
+
+  @Override
+  public Deferred<Object> shutdown() {
+    return Deferred.fromResult(null);
+  }
+
+  @Override
+  public String version() {
+    return "3.0.0";
+  }
+  
+  void registerConfigs(final TSDB tsdb) {
+    if (!tsdb.getConfig().hasProperty(PROJECT_NAME_KEY)) {
+      tsdb.getConfig().register(PROJECT_NAME_KEY, null, false, 
+          "The project ID for the pubsub publisher.");
+    }
+    if (!tsdb.getConfig().hasProperty(TOPIC_KEY)) {
+      tsdb.getConfig().register(TOPIC_KEY, null, false, 
+          "The topic ID to publish to.");
+    }
+    if (!tsdb.getConfig().hasProperty(JSON_KEYFILE_KEY)) {
+      tsdb.getConfig().register(JSON_KEYFILE_KEY, null, false, 
+          "The full path to a JSON service key file.");
+    }
+  }
+
+  List<WriteStatus> buildList(final WriteStatus status, final int size) {
+    final List<WriteStatus> list = Lists.newArrayListWithExpectedSize(size);
+    for (int i = 0; i < size; i++) {
+      list.add(status);
+    }
+    return list;
+  }
+}
---
+++ storage/googlepubsub/src/test/java/net/opentsdb/storage/TestPubSubWriter.java
@@ -0,0 +1,306 @@
+// This file is part of OpenTSDB.
+// Copyright (C) 2015-2018  The OpenTSDB Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package net.opentsdb.storage;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import com.google.api.core.ApiFuture;
+import com.google.api.core.ApiFutures;
+import com.google.api.gax.core.CredentialsProvider;
+import com.google.api.gax.rpc.ApiException;
+import com.google.api.gax.rpc.StatusCode;
+import com.google.api.gax.rpc.StatusCode.Code;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.pubsub.v1.Publisher;
+import com.google.pubsub.v1.PubsubMessage;
+import com.google.pubsub.v1.TopicName;
+
+import net.opentsdb.core.MockTSDB;
+import net.opentsdb.data.TimeSeriesDatum;
+import net.opentsdb.data.TimeSeriesSharedTagsAndTimeData;
+import net.opentsdb.query.serdes.SerdesOptions;
+import net.opentsdb.query.serdes.TimeSeriesDataSerdes;
+import net.opentsdb.stats.Span;
+import net.opentsdb.storage.WriteStatus.WriteState;
+import net.opentsdb.utils.UnitTestException;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ PubSubWriter.class, GoogleCredentials.class, 
+  FileInputStream.class, Publisher.Builder.class, Publisher.class })
+public class TestPubSubWriter {
+
+  private MockTSDB tsdb;
+  private TimeSeriesDataSerdes serdes;
+  private Publisher.Builder pub_builder;
+  private Publisher publisher;
+  
+  @Before
+  public void before() throws Exception {
+    PowerMockito.mockStatic(FileInputStream.class);
+    PowerMockito.whenNew(FileInputStream.class).withAnyArguments()
+      .thenReturn(mock(FileInputStream.class));
+    PowerMockito.mockStatic(GoogleCredentials.class);
+    PowerMockito.when(GoogleCredentials.fromStream(any(InputStream.class)))
+      .thenReturn(mock(GoogleCredentials.class));
+    PowerMockito.mockStatic(Publisher.Builder.class);
+    PowerMockito.mockStatic(Publisher.class);
+    
+    tsdb = new MockTSDB();
+    serdes = mock(TimeSeriesDataSerdes.class);
+    when(tsdb.registry.getDefaultPlugin(TimeSeriesDataSerdes.class))
+      .thenReturn(serdes);
+    
+    pub_builder = mock(Publisher.Builder.class);
+    publisher = mock(Publisher.class);
+    
+    when(Publisher.newBuilder(any(TopicName.class))).thenReturn(pub_builder);
+    when(pub_builder.setCredentialsProvider(any(CredentialsProvider.class)))
+      .thenReturn(pub_builder);
+    when(pub_builder.build()).thenReturn(publisher);
+  }
+  
+  @Test
+  public void initialize() throws Exception {
+    PubSubWriter writer = new PubSubWriter();
+    writer.registerConfigs(tsdb);
+    
+    tsdb.config.override(PubSubWriter.PROJECT_NAME_KEY, "MyProject");
+    tsdb.config.override(PubSubWriter.TOPIC_KEY, "Test");
+    tsdb.config.override(PubSubWriter.JSON_KEYFILE_KEY, "MyKey");
+    
+    assertNull(writer.initialize(tsdb).join());
+    assertSame(publisher, writer.publisher);
+    assertSame(serdes, writer.serdes);
+    assertEquals("Test", writer.topic.getTopic());
+    assertEquals("MyProject", writer.topic.getProject());
+    
+    // bad settings
+    try {
+      tsdb.config.override(PubSubWriter.PROJECT_NAME_KEY, null);
+      writer = new PubSubWriter();
+      writer.initialize(tsdb).join();
+      fail("Expected IllegalArgumentException");
+    } catch (IllegalArgumentException e) { }
+    
+    try {
+      tsdb.config.override(PubSubWriter.PROJECT_NAME_KEY, "");
+      writer = new PubSubWriter();
+      writer.initialize(tsdb).join();
+      fail("Expected IllegalArgumentException");
+    } catch (IllegalArgumentException e) { }
+    
+    tsdb.config.override(PubSubWriter.PROJECT_NAME_KEY, "MyProject");
+    try {
+      tsdb.config.override(PubSubWriter.TOPIC_KEY, null);
+      writer = new PubSubWriter();
+      writer.initialize(tsdb).join();
+      fail("Expected IllegalArgumentException");
+    } catch (IllegalArgumentException e) { }
+    
+    try {
+      tsdb.config.override(PubSubWriter.TOPIC_KEY, "");
+      writer = new PubSubWriter();
+      writer.initialize(tsdb).join();
+      fail("Expected IllegalArgumentException");
+    } catch (IllegalArgumentException e) { }
+    
+    tsdb.config.override(PubSubWriter.TOPIC_KEY, "Test");
+    try {
+      tsdb.config.override(PubSubWriter.JSON_KEYFILE_KEY, null);
+      writer = new PubSubWriter();
+      writer.initialize(tsdb).join();
+      fail("Expected IllegalArgumentException");
+    } catch (IllegalArgumentException e) { }
+    
+    try {
+      tsdb.config.override(PubSubWriter.JSON_KEYFILE_KEY, "");
+      writer = new PubSubWriter();
+      writer.initialize(tsdb).join();
+      fail("Expected IllegalArgumentException");
+    } catch (IllegalArgumentException e) { }
+    tsdb.config.override(PubSubWriter.JSON_KEYFILE_KEY, "MyKey");
+    
+    // no serdes
+    when(tsdb.registry.getDefaultPlugin(TimeSeriesDataSerdes.class))
+      .thenReturn(null);
+    try {
+      writer = new PubSubWriter();
+      writer.initialize(tsdb).join();
+      fail("Expected IllegalArgumentException");
+    } catch (IllegalArgumentException e) { }
+  }
+
+  @Test
+  public void writeDatum() throws Exception {
+    PubSubWriter writer = new PubSubWriter();
+    writer.registerConfigs(tsdb);
+    tsdb.config.override(PubSubWriter.PROJECT_NAME_KEY, "MyProject");
+    tsdb.config.override(PubSubWriter.TOPIC_KEY, "Test");
+    tsdb.config.override(PubSubWriter.JSON_KEYFILE_KEY, "MyKey");
+    writer.initialize(tsdb);
+    
+    TimeSeriesDatum datum = mock(TimeSeriesDatum.class);
+    doAnswer(new Answer<ApiFuture<String>>() {
+      @Override
+      public ApiFuture<String> answer(InvocationOnMock invocation)
+          throws Throwable {
+        return ApiFutures.immediateFuture("1");
+      }
+    }).when(publisher).publish(any(PubsubMessage.class));
+    assertEquals(WriteState.OK, 
+        writer.write(null, datum, null).join().state());
+    
+    // API error
+    doAnswer(new Answer<ApiFuture<String>>() {
+      @Override
+      public ApiFuture<String> answer(InvocationOnMock invocation)
+          throws Throwable {
+        StatusCode status = mock(StatusCode.class);
+        when(status.getCode()).thenReturn(Code.ABORTED);
+        return ApiFutures.immediateFailedFuture(
+            new ApiException("Boo!", null, status, false));
+      }
+    }).when(publisher).publish(any(PubsubMessage.class));
+    assertEquals(WriteState.ERROR, 
+        writer.write(null, datum, null).join().state());
+    
+    // API retryable
+    doAnswer(new Answer<ApiFuture<String>>() {
+      @Override
+      public ApiFuture<String> answer(InvocationOnMock invocation)
+          throws Throwable {
+        StatusCode status = mock(StatusCode.class);
+        when(status.getCode()).thenReturn(Code.ABORTED);
+        return ApiFutures.immediateFailedFuture(
+            new ApiException("Boo!", null, status, true));
+      }
+    }).when(publisher).publish(any(PubsubMessage.class));
+    assertEquals(WriteState.RETRY, 
+        writer.write(null, datum, null).join().state());
+    
+    // other exception
+    doAnswer(new Answer<ApiFuture<String>>() {
+      @Override
+      public ApiFuture<String> answer(InvocationOnMock invocation)
+          throws Throwable {
+        return ApiFutures.immediateFailedFuture(
+            new UnitTestException());
+      }
+    }).when(publisher).publish(any(PubsubMessage.class));
+    assertEquals(WriteState.ERROR, 
+        writer.write(null, datum, null).join().state());
+    
+    doThrow(new UnitTestException()).when(serdes)
+    .serialize(any(SerdesOptions.class), any(TimeSeriesDatum.class), 
+        any(OutputStream.class), any(Span.class));
+    assertEquals(WriteState.ERROR, 
+        writer.write(null, datum, null).join().state());
+  }
+  
+  @Test
+  public void writeSharedData() throws Exception {
+    PubSubWriter writer = new PubSubWriter();
+    writer.registerConfigs(tsdb);
+    tsdb.config.override(PubSubWriter.PROJECT_NAME_KEY, "MyProject");
+    tsdb.config.override(PubSubWriter.TOPIC_KEY, "Test");
+    tsdb.config.override(PubSubWriter.JSON_KEYFILE_KEY, "MyKey");
+    writer.initialize(tsdb);
+    
+    TimeSeriesSharedTagsAndTimeData data = mock(TimeSeriesSharedTagsAndTimeData.class);
+    when(data.size()).thenReturn(4);
+    doAnswer(new Answer<ApiFuture<String>>() {
+      @Override
+      public ApiFuture<String> answer(InvocationOnMock invocation)
+          throws Throwable {
+        return ApiFutures.immediateFuture("1");
+      }
+    }).when(publisher).publish(any(PubsubMessage.class));
+    List<WriteStatus> result = writer.write(null, data, null).join();
+    assertEquals(4, result.size());
+    assertEquals(WriteState.OK, result.get(0).state());
+    
+    // API error
+    doAnswer(new Answer<ApiFuture<String>>() {
+      @Override
+      public ApiFuture<String> answer(InvocationOnMock invocation)
+          throws Throwable {
+        StatusCode status = mock(StatusCode.class);
+        when(status.getCode()).thenReturn(Code.ABORTED);
+        return ApiFutures.immediateFailedFuture(
+            new ApiException("Boo!", null, status, false));
+      }
+    }).when(publisher).publish(any(PubsubMessage.class));
+    result = writer.write(null, data, null).join();
+    assertEquals(4, result.size());
+    assertEquals(WriteState.ERROR, result.get(0).state());
+    
+    // API retryable
+    doAnswer(new Answer<ApiFuture<String>>() {
+      @Override
+      public ApiFuture<String> answer(InvocationOnMock invocation)
+          throws Throwable {
+        StatusCode status = mock(StatusCode.class);
+        when(status.getCode()).thenReturn(Code.ABORTED);
+        return ApiFutures.immediateFailedFuture(
+            new ApiException("Boo!", null, status, true));
+      }
+    }).when(publisher).publish(any(PubsubMessage.class));
+    result = writer.write(null, data, null).join();
+    assertEquals(4, result.size());
+    assertEquals(WriteState.RETRY, result.get(0).state());
+    
+    // other exception
+    doAnswer(new Answer<ApiFuture<String>>() {
+      @Override
+      public ApiFuture<String> answer(InvocationOnMock invocation)
+          throws Throwable {
+        return ApiFutures.immediateFailedFuture(
+            new UnitTestException());
+      }
+    }).when(publisher).publish(any(PubsubMessage.class));
+    result = writer.write(null, data, null).join();
+    assertEquals(4, result.size());
+    assertEquals(WriteState.ERROR, result.get(0).state());
+    
+    doThrow(new UnitTestException()).when(serdes)
+    .serialize(any(SerdesOptions.class), any(TimeSeriesDatum.class), 
+        any(OutputStream.class), any(Span.class));
+    result = writer.write(null, data, null).join();
+    assertEquals(4, result.size());
+    assertEquals(WriteState.ERROR, result.get(0).state());
+  }
+}
