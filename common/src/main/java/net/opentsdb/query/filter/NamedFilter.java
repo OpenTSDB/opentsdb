b27f612ec4176981be953735404a04885fc8694f
Chris Larsen <clarsen@yahoo-inc.com>
2018-07-26 12:28:00 -0700

    COMMON: - Start interfaces for 3.0 filters including metric and tag keys as well as values.

---
+++ common/src/main/java/net/opentsdb/query/filter/NamedFilter.java
@@ -0,0 +1,31 @@
+// This file is part of OpenTSDB.
+// Copyright (C) 2018  The OpenTSDB Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package net.opentsdb.query.filter;
+
+/**
+ * A named filter referenced in queries. The filter can be a single
+ * filter or a chain of multiple filters.
+ * 
+ * @since 3.0
+ */
+public interface NamedFilter {
+
+  /** @return The non-null and non-empty filter name. */
+  public String id();
+  
+  /** @return The non-null query filter. */
+  public QueryFilter filter();
+  
+}
