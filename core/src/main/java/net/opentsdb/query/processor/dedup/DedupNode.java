0c0564988327cd76623d1712ef175c16decf08c8
Chris Larsen <clarsen@yahoo-inc.com>
2018-08-06 17:01:30 -0700

    CORE: - Add headers to dedup code and format.
    Signed-off-by: Chris Larsen <clarsen@yahoo-inc.com>

--- core/src/main/java/net/opentsdb/query/processor/dedup/DedupNode.java
+++ core/src/main/java/net/opentsdb/query/processor/dedup/DedupNode.java
@@ -1,3 +1,17 @@
+// This file is part of OpenTSDB.
+// Copyright (C) 2018  The OpenTSDB Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package net.opentsdb.query.processor.dedup;
 
 import com.google.common.reflect.TypeToken;
@@ -25,152 +39,166 @@ import net.opentsdb.query.QueryPipelineContext;
 import net.opentsdb.query.QueryResult;
 import net.opentsdb.rollup.RollupConfig;
 
+/**
+ * A node that handles deduplication and/or sorting of time series values
+ * from underlying iterators.
+ * 
+ * @since 3.0
+ */
 public class DedupNode extends AbstractQueryNode {
 
-    private DedupConfig config;
+  /** The node config. */
+  private DedupConfig config;
 
-    public DedupNode(final QueryNodeFactory factory,
-                     final QueryPipelineContext context,
-                     final String id,
-                     final DedupConfig config) {
-        super(factory, context, id);
-        this.config = config;
+  public DedupNode(final QueryNodeFactory factory,
+                   final QueryPipelineContext context,
+                   final String id,
+                   final DedupConfig config) {
+    super(factory, context, id);
+    this.config = config;
+  }
+
+  @Override
+  public QueryNodeConfig config() {
+    return config;
+  }
+
+  @Override
+  public void close() {
+  }
+
+  @Override
+  public void onComplete(final QueryNode downstream, 
+                         final long final_sequence,
+                         final long total_sequences) {
+    completeUpstream(final_sequence, total_sequences);
+  }
+
+  @Override
+  public void onNext(final QueryResult next) {
+    DedupResult dedupResult = new DedupResult(next);
+
+    for (TimeSeries ts : next.timeSeries()) {
+      Optional<Iterator<TimeSeriesValue<? extends TimeSeriesDataType>>> optional = ts
+          .iterator(NumericType.TYPE);
+      if (optional.isPresent()) {
+        Iterator<TimeSeriesValue<? extends TimeSeriesDataType>> iterator = 
+            optional.get();
+        TreeMap<TimeStamp, TimeSeriesValue<? extends TimeSeriesDataType>> tsMap = 
+            new TreeMap<>();
+        while (iterator.hasNext()) {
+          TimeSeriesValue<? extends TimeSeriesDataType> tsValue = 
+              iterator.next();
+          tsMap.put(tsValue.timestamp(), tsValue);
+        }
+        dedupResult.results.add(new DedupTimeSeries(ts, 
+            tsMap.values().iterator()));
+      }
+    }
+
+    sendUpstream(dedupResult);
+  }
+
+  @Override
+  public void onError(final Throwable t) {
+    sendUpstream(t);
+  }
+
+  private class DedupResult implements QueryResult {
+
+    private QueryResult next;
+    private List<TimeSeries> results = new ArrayList<>();
+
+    public DedupResult(QueryResult next) {
+      this.next = next;
     }
 
     @Override
-    public QueryNodeConfig config() {
-        return config;
+    public TimeSpecification timeSpecification() {
+      return next.timeSpecification();
+    }
+
+    @Override
+    public Collection<TimeSeries> timeSeries() {
+      return results;
+    }
+
+    @Override
+    public long sequenceId() {
+      return next.sequenceId();
+    }
+
+    @Override
+    public QueryNode source() {
+      return next.source();
+    }
+
+    @Override
+    public TypeToken<? extends TimeSeriesId> idType() {
+      return next.idType();
+    }
+
+    @Override
+    public ChronoUnit resolution() {
+      return next.resolution();
+    }
+
+    @Override
+    public RollupConfig rollupConfig() {
+      return next.rollupConfig();
     }
 
     @Override
     public void close() {
+        next.close();
+    }
+  }
+
+  private class DedupTimeSeries implements TimeSeries {
+
+    private TimeSeries timeSeries;
+    private Iterator<TimeSeriesValue<? extends TimeSeriesDataType>> iterator;
+
+    public DedupTimeSeries(
+        final TimeSeries timeSeries, 
+        final Iterator<TimeSeriesValue<? extends TimeSeriesDataType>> iterator) {
+      this.timeSeries = timeSeries;
+      this.iterator = iterator;
     }
 
     @Override
-    public void onComplete(QueryNode downstream, long final_sequence,
-        long total_sequences) {
-        completeUpstream(final_sequence, total_sequences);
+    public TimeSeriesId id() {
+      return timeSeries.id();
     }
 
     @Override
-    public void onNext(QueryResult next) {
-
-        DedupResult dedupResult = new DedupResult(next);
-
-        for (TimeSeries ts : next.timeSeries()) {
-            Optional<Iterator<TimeSeriesValue<? extends TimeSeriesDataType>>> optional = ts
-                .iterator(NumericType.TYPE);
-            if (optional.isPresent()) {
-                Iterator<TimeSeriesValue<? extends TimeSeriesDataType>> iterator = optional.get();
-                TreeMap<TimeStamp, TimeSeriesValue<? extends TimeSeriesDataType>> tsMap = new TreeMap<>();
-                while (iterator.hasNext()) {
-                    TimeSeriesValue<? extends TimeSeriesDataType> tsValue = iterator.next();
-                    tsMap.put(tsValue.timestamp(), tsValue);
-                }
-                dedupResult.results.add(new DedupTimeSeries(ts, tsMap.values().iterator()));
-            }
-        }
-
-        sendUpstream(dedupResult);
+    public Optional<Iterator<TimeSeriesValue<? extends TimeSeriesDataType>>> 
+        iterator(TypeToken<?> type) {
+      if (type == NumericType.TYPE) {
+        return Optional.of(iterator);
+      }
+      return timeSeries.iterator(type);
     }
 
     @Override
-    public void onError(Throwable t) {
-        sendUpstream(t);
+    public Collection<TypedIterator<
+      TimeSeriesValue<? extends TimeSeriesDataType>>> iterators() {
+      List<TypedIterator<TimeSeriesValue<? extends TimeSeriesDataType>>> 
+          iterators = timeSeries.iterators().stream().filter(
+              ti -> ti.getType() == NumericType.TYPE
+              ).collect(Collectors.toList());
+      iterators.add(new TypedIterator(iterator, NumericType.TYPE));
+      return iterators;
     }
 
-    private class DedupResult implements QueryResult {
-
-        private QueryResult next;
-        private List<TimeSeries> results = new ArrayList<>();
-
-        public DedupResult(QueryResult next) {
-            this.next = next;
-        }
-
-        @Override
-        public TimeSpecification timeSpecification() {
-            return next.timeSpecification();
-        }
-
-        @Override
-        public Collection<TimeSeries> timeSeries() {
-            return results;
-        }
-
-        @Override
-        public long sequenceId() {
-            return next.sequenceId();
-        }
-
-        @Override
-        public QueryNode source() {
-            return next.source();
-        }
-
-        @Override
-        public TypeToken<? extends TimeSeriesId> idType() {
-            return next.idType();
-        }
-
-        @Override
-        public ChronoUnit resolution() {
-            return next.resolution();
-        }
-
-        @Override
-        public RollupConfig rollupConfig() {
-            return next.rollupConfig();
-        }
-
-        @Override
-        public void close() {
-            next.close();
-        }
+    @Override
+    public Collection<TypeToken<?>> types() {
+      return timeSeries.types();
     }
 
-    private class DedupTimeSeries implements TimeSeries {
-
-        private TimeSeries timeSeries;
-        private Iterator<TimeSeriesValue<? extends TimeSeriesDataType>> iterator;
-
-        public DedupTimeSeries(TimeSeries timeSeries, Iterator<TimeSeriesValue<? extends TimeSeriesDataType>> iterator) {
-            this.timeSeries = timeSeries;
-            this.iterator = iterator;
-        }
-
-        @Override
-        public TimeSeriesId id() {
-            return timeSeries.id();
-        }
-
-        @Override
-        public Optional<Iterator<TimeSeriesValue<? extends TimeSeriesDataType>>> iterator(TypeToken<?> type) {
-            if (type == NumericType.TYPE) {
-                return Optional.of(iterator);
-            }
-            return timeSeries.iterator(type);
-        }
-
-        @Override
-        public Collection<TypedIterator<TimeSeriesValue<? extends TimeSeriesDataType>>> iterators() {
-
-            List<TypedIterator<TimeSeriesValue<? extends TimeSeriesDataType>>> iterators = timeSeries
-                .iterators().stream().filter(ti -> ti.getType() == NumericType.TYPE)
-                .collect(Collectors.toList());
-            iterators
-                .add(new TypedIterator(iterator, NumericType.TYPE));
-            return iterators;
-        }
-
-        @Override
-        public Collection<TypeToken<?>> types() {
-            return timeSeries.types();
-        }
-
-        @Override
-        public void close() {
-            timeSeries.close();
-        }
+    @Override
+    public void close() {
+        timeSeries.close();
     }
+  }
 }
--- core/src/test/java/net/opentsdb/query/processor/dedup/TestDedupNode.java
+++ core/src/test/java/net/opentsdb/query/processor/dedup/TestDedupNode.java
@@ -1,30 +1,42 @@
+// This file is part of OpenTSDB.
+// Copyright (C) 2018  The OpenTSDB Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package net.opentsdb.query.processor.dedup;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import com.google.common.reflect.TypeToken;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
-import net.opentsdb.data.MillisecondTimeStamp;
 import net.opentsdb.data.SecondTimeStamp;
 import net.opentsdb.data.TimeSeries;
 import net.opentsdb.data.TimeSeriesDataType;
 import net.opentsdb.data.TimeSeriesValue;
-import net.opentsdb.data.TimeStamp;
 import net.opentsdb.data.types.numeric.MutableNumericValue;
 import net.opentsdb.data.types.numeric.NumericType;
 import net.opentsdb.query.QueryNode;
 import net.opentsdb.query.QueryPipelineContext;
 import net.opentsdb.query.QueryResult;
+import net.opentsdb.query.processor.dedup.DedupConfig;
+import net.opentsdb.query.processor.dedup.DedupNode;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
@@ -34,70 +46,76 @@ import org.mockito.MockitoAnnotations;
 
 public class TestDedupNode {
 
-    @Mock
-    private QueryPipelineContext pipelineContext;
+  @Mock
+  private QueryPipelineContext pipelineContext;
 
-    @Mock
-    private QueryResult queryResult;
+  @Mock
+  private QueryResult queryResult;
 
-    @Mock
-    private TimeSeries timeSeries;
+  @Mock
+  private TimeSeries timeSeries;
 
-    @Mock
-    private QueryNode upStream;
+  @Mock
+  private QueryNode upStream;
 
-    @Captor
-    private ArgumentCaptor<QueryResult> upStreamCaptor;
+  @Captor
+  private ArgumentCaptor<QueryResult> upStreamCaptor;
 
-    @Before
-    public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
-    }
+  @Before
+  public void setUp() throws Exception {
+    MockitoAnnotations.initMocks(this);
+  }
 
-    @Test
-    public void testRemovesDuplicateTimeseries() {
+  @Test
+  public void testRemovesDuplicateTimeseries() {
 
-        List<TimeSeriesValue<? extends TimeSeriesDataType>> tsValues = new ArrayList() {{
-            add(new MutableNumericValue(new SecondTimeStamp(1532455200), 43363892));
-            add(new MutableNumericValue(new SecondTimeStamp(1532455200), 44421636.8));
-            add(new MutableNumericValue(new SecondTimeStamp(1532483800), 49255974.4));
-            add(new MutableNumericValue(new SecondTimeStamp(1532483500), 43338914.45831));
-            add(new MutableNumericValue(new SecondTimeStamp(1532482900), 44013684.8));
-            add(new MutableNumericValue(new SecondTimeStamp(1532483500), 43338914.4));
-        }};
+    List<TimeSeriesValue<? extends TimeSeriesDataType>> tsValues = 
+        new ArrayList() {{
+        add(new MutableNumericValue(new SecondTimeStamp(1532455200), 43363892));
+        add(new MutableNumericValue(new SecondTimeStamp(1532455200), 44421636.8));
+        add(new MutableNumericValue(new SecondTimeStamp(1532483800), 49255974.4));
+        add(new MutableNumericValue(new SecondTimeStamp(1532483500), 43338914.45831));
+        add(new MutableNumericValue(new SecondTimeStamp(1532482900), 44013684.8));
+        add(new MutableNumericValue(new SecondTimeStamp(1532483500), 43338914.4));
+    }};
 
-        List<TimeSeriesValue<? extends TimeSeriesDataType>> expectedValues = new ArrayList() {{
-            add(new MutableNumericValue(new SecondTimeStamp(1532455200), 44421636.8));
-            add(new MutableNumericValue(new SecondTimeStamp(1532483800), 49255974.4));
-            add(new MutableNumericValue(new SecondTimeStamp(1532482900), 44013684.8));
-            add(new MutableNumericValue(new SecondTimeStamp(1532483500), 43338914.4));
-        }};
+    List<TimeSeriesValue<? extends TimeSeriesDataType>> expectedValues = 
+        new ArrayList() {{
+        add(new MutableNumericValue(new SecondTimeStamp(1532455200), 44421636.8));
+        add(new MutableNumericValue(new SecondTimeStamp(1532483800), 49255974.4));
+        add(new MutableNumericValue(new SecondTimeStamp(1532482900), 44013684.8));
+        add(new MutableNumericValue(new SecondTimeStamp(1532483500), 43338914.4));
+    }};
 
-        Optional<Iterator<TimeSeriesValue<? extends TimeSeriesDataType>>> iterator = Optional.of(tsValues.iterator());
-        when(timeSeries.iterator(NumericType.TYPE)).thenReturn(iterator);
-        when(pipelineContext.upstream(any(DedupNode.class))).thenReturn(new ArrayList<QueryNode>(){{add(upStream);}});
-        when(queryResult.timeSeries()).thenReturn(new ArrayList<TimeSeries>() {{
-            add(timeSeries);
-        }});
+    Optional<Iterator<TimeSeriesValue<? extends TimeSeriesDataType>>> iterator = 
+        Optional.of(tsValues.iterator());
+    when(timeSeries.iterator(NumericType.TYPE)).thenReturn(iterator);
+    when(pipelineContext.upstream(any(DedupNode.class))).thenReturn(
+        new ArrayList<QueryNode>(){{add(upStream);}});
+    when(queryResult.timeSeries()).thenReturn(new ArrayList<TimeSeries>() {{
+        add(timeSeries);
+    }});
 
-        DedupNode dedupNode = new DedupNode(null, pipelineContext, null, DedupConfig.newBuilder().build());
-        dedupNode.initialize(null);
+    DedupNode dedupNode = new DedupNode(null, pipelineContext, null, 
+        DedupConfig.newBuilder().build());
+    dedupNode.initialize(null);
 
-        dedupNode.onNext(queryResult);
+    dedupNode.onNext(queryResult);
 
-        verify(upStream).onNext(upStreamCaptor.capture());
-        QueryResult dedupedResult = upStreamCaptor.getValue();
+    verify(upStream).onNext(upStreamCaptor.capture());
+    QueryResult dedupedResult = upStreamCaptor.getValue();
 
-        Collection<TimeSeries> dedupedTSCollection = dedupedResult.timeSeries();
-        assertEquals(1, dedupedTSCollection.size());
-        TimeSeries dedupedTs = dedupedTSCollection.iterator().next();
-        Optional<Iterator<TimeSeriesValue<? extends TimeSeriesDataType>>> optional = dedupedTs.iterator(NumericType.TYPE);
-        assertTrue(optional.isPresent());
-        Iterator<TimeSeriesValue<? extends TimeSeriesDataType>> dedupedIterator = optional.get();
-        List<TimeSeriesValue<? extends TimeSeriesDataType>> dedupedValues = new ArrayList<>();
-        dedupedIterator.forEachRemaining(dedupedValues::add);
+    Collection<TimeSeries> dedupedTSCollection = dedupedResult.timeSeries();
+    assertEquals(1, dedupedTSCollection.size());
+    TimeSeries dedupedTs = dedupedTSCollection.iterator().next();
+    Optional<Iterator<TimeSeriesValue<? extends TimeSeriesDataType>>> optional = 
+        dedupedTs.iterator(NumericType.TYPE);
+    assertTrue(optional.isPresent());
+    Iterator<TimeSeriesValue<? extends TimeSeriesDataType>> dedupedIterator = optional.get();
+    List<TimeSeriesValue<? extends TimeSeriesDataType>> dedupedValues = new ArrayList<>();
+    dedupedIterator.forEachRemaining(dedupedValues::add);
 
-        assertTrue(expectedValues.containsAll(dedupedValues));
-    }
+    assertTrue(expectedValues.containsAll(dedupedValues));
+  }
 
 }
