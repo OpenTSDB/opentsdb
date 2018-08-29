2e86fd12aadbc315ae9ba7be9c158bc15c3dd9ca
Chris Larsen <clarsen@yahoo-inc.com>
2018-08-29 14:53:19 -0700

    CORE: - Add the NumericArrayAggregator interface and factory interface as well as   the array sum aggregator implementation.
    Signed-off-by: Chris Larsen <clarsen@yahoo-inc.com>

---
+++ core/src/main/java/net/opentsdb/data/types/numeric/aggregators/ArraySum.java
@@ -0,0 +1,112 @@
+// This file is part of OpenTSDB.
+// Copyright (C) 2018  The OpenTSDB Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package net.opentsdb.data.types.numeric.aggregators;
+
+import java.util.Arrays;
+
+/**
+ * Computes the sum on the values.
+ * 
+ * Note we don't explicitly check for offset issues, we just make sure
+ * the lengths are the same.
+ * 
+ * @since 3.0
+ */
+public class ArraySum extends BaseArrayAggregator {
+
+  public ArraySum(final boolean infectious_nans) {
+    super(infectious_nans);
+  }
+  
+  @Override
+  public void accumulate(final long[] values,
+                         final int from,
+                         final int to) {
+    if (double_accumulator == null && long_accumulator == null) {
+      long_accumulator = Arrays.copyOfRange(values, from, to);
+      return;
+    }
+    
+    if (long_accumulator != null) {
+      if (to - from != long_accumulator.length) {
+        throw new IllegalArgumentException("Values of length " 
+            + (to - from) + " did not match the original lengh of " 
+            + long_accumulator.length);
+      }
+      int idx = 0;
+      for (int i = from; i < to; i++) {
+        long_accumulator[idx++] += values[i];
+      }
+    } else {
+      if (to - from != double_accumulator.length) {
+        throw new IllegalArgumentException("Values of length " 
+            + (to - from) + " did not match the original lengh of " 
+            + double_accumulator.length);
+      }
+      int idx = 0;
+      for (int i = from; i < to; i++) {
+        double_accumulator[idx++] += values[i];
+      }
+    }
+  }
+
+  @Override
+  public void accumulate(final double[] values, 
+                         final int from, 
+                         final int to) {
+    if (double_accumulator == null && long_accumulator == null) {
+      double_accumulator = new double[to - from];
+    }
+    
+    if (double_accumulator == null) {
+      double_accumulator = new double[long_accumulator.length];
+      for (int i = 0; i < long_accumulator.length; i++) {
+        double_accumulator[i] = long_accumulator[i];
+      }
+      long_accumulator = null;
+    }
+    
+    if (to - from != double_accumulator.length) {
+      throw new IllegalArgumentException("Values of length " 
+          + (to - from) + " did not match the original lengh of " 
+          + double_accumulator.length);
+    }
+    
+    int idx = 0;
+    for (int i = from; i < to; i++) {
+      if (Double.isNaN(values[i])) {
+        if (infectious_nans) {
+          double_accumulator[idx++] += values[i];
+        } else {
+          idx++;
+        }
+      } else {
+        double_accumulator[idx++] += values[i];
+      }
+    }
+  }
+
+  @Override
+  public int offset() {
+    return 0;
+  }
+
+  @Override
+  public int end() {
+    return long_accumulator != null ? long_accumulator.length : 
+      double_accumulator.length;
+  }
+
+}
