2f099a533cd8cd4eb4f88013f3b372d79dc6086c
Chris Larsen <clarsen@yahoo-inc.com>
2018-09-13 10:14:56 -0700

    CORE: - Add forgotten test for the Binary expression node factory.
    Signed-off-by: Chris Larsen <clarsen@yahoo-inc.com>

---
+++ core/src/test/java/net/opentsdb/query/processor/expressions/TestBinaryExpressionNodeFactory.java
@@ -0,0 +1,94 @@
+//This file is part of OpenTSDB.
+//Copyright (C) 2018  The OpenTSDB Authors.
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+package net.opentsdb.query.processor.expressions;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+import net.opentsdb.data.types.numeric.NumericSummaryType;
+import net.opentsdb.data.types.numeric.NumericType;
+import net.opentsdb.query.QueryFillPolicy.FillWithRealPolicy;
+import net.opentsdb.query.QueryNode;
+import net.opentsdb.query.QueryNodeConfig;
+import net.opentsdb.query.QueryPipelineContext;
+import net.opentsdb.query.interpolation.types.numeric.NumericInterpolatorConfig;
+import net.opentsdb.query.joins.JoinConfig;
+import net.opentsdb.query.joins.JoinConfig.JoinType;
+import net.opentsdb.query.pojo.FillPolicy;
+import net.opentsdb.query.processor.expressions.ExpressionParseNode.ExpressionOp;
+import net.opentsdb.query.processor.expressions.ExpressionParseNode.OperandType;
+
+public class TestBinaryExpressionNodeFactory {
+
+  private static QueryPipelineContext CONTEXT;
+  protected static NumericInterpolatorConfig NUMERIC_CONFIG;
+  protected static JoinConfig JOIN_CONFIG;
+  
+  @BeforeClass
+  public static void beforeClass() throws Exception {
+    CONTEXT =  mock(QueryPipelineContext.class);
+    
+    NUMERIC_CONFIG = 
+        (NumericInterpolatorConfig) NumericInterpolatorConfig.newBuilder()
+      .setFillPolicy(FillPolicy.NOT_A_NUMBER)
+      .setRealFillPolicy(FillWithRealPolicy.NONE)
+      .setDataType(NumericType.TYPE.toString())
+      .build();
+    
+    JOIN_CONFIG = (JoinConfig) JoinConfig.newBuilder()
+        .setType(JoinType.NATURAL)
+        .build();
+  }
+  
+  @Test
+  public void ctor() throws Exception {
+    BinaryExpressionNodeFactory factory = new BinaryExpressionNodeFactory();
+    assertEquals(2, factory.types().size());
+    assertTrue(factory.types().contains(NumericType.TYPE));
+    assertTrue(factory.types().contains(NumericSummaryType.TYPE));
+  }
+  
+  @Test
+  public void newNodeSimpleBinary() throws Exception {
+    BinaryExpressionNodeFactory factory = new BinaryExpressionNodeFactory();
+    ExpressionConfig config = 
+        (ExpressionConfig) ExpressionConfig.newBuilder()
+        .setExpression("a + b")
+        .setJoinConfig(JOIN_CONFIG)
+        .addInterpolatorConfig(NUMERIC_CONFIG)
+        .setId("e1")
+        .build();
+    ExpressionParseNode expression_config = 
+        (ExpressionParseNode) ExpressionParseNode.newBuilder()
+        .setLeft("a")
+        .setLeftType(OperandType.VARIABLE)
+        .setRight("b")
+        .setRightType(OperandType.VARIABLE)
+        .setExpressionOp(ExpressionOp.ADD)
+        .setExpressionConfig(config)
+        .build();
+    QueryNode new_node = factory.newNode(CONTEXT, null, 
+        (QueryNodeConfig) expression_config);
+    assertTrue(new_node instanceof BinaryExpressionNode);
+    assertSame(config, new_node.config());
+  }
+  
+}
