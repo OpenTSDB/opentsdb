b092c824f17d1f86c3eba5890cd388269ebfc0b0
Chris Larsen <clarsen@yahoo-inc.com>
2018-09-19 15:08:32 -0700

    PROTOBUF: - Add rudimentary GPRC server and client to the Protobuf plugin.

---
+++ implementation/protobuf/src/main/java/net/opentsdb/grpc/QueryGRPCClientFactory.java
@@ -0,0 +1,177 @@
+// This file is part of OpenTSDB.
+// Copyright (C) 2018  The OpenTSDB Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package net.opentsdb.grpc;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.reflect.TypeToken;
+import com.stumbleupon.async.Deferred;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import net.opentsdb.common.Const;
+import net.opentsdb.core.TSDB;
+import net.opentsdb.data.TimeSeriesByteId;
+import net.opentsdb.data.TimeSeriesId;
+import net.opentsdb.data.TimeSeriesStringId;
+import net.opentsdb.grpc.QueryRpcBetaGrpc.QueryRpcBetaStub;
+import net.opentsdb.query.QueryNode;
+import net.opentsdb.query.QueryNodeConfig;
+import net.opentsdb.query.QueryPipelineContext;
+import net.opentsdb.query.QuerySourceConfig;
+import net.opentsdb.query.processor.downsample.DownsampleConfig;
+import net.opentsdb.query.serdes.PBufIteratorSerdesFactory;
+import net.opentsdb.stats.Span;
+import net.opentsdb.storage.ReadableTimeSeriesDataStore;
+import net.opentsdb.storage.TimeSeriesDataStoreFactory;
+
+/**
+ * A factory for instantiating GRPC clients.
+ * 
+ * TODO - many more options like LB and such
+ * 
+ * @since 3.0
+ */
+public class QueryGRPCClientFactory implements TimeSeriesDataStoreFactory, 
+                                               ReadableTimeSeriesDataStore {
+  private static final Logger LOG = LoggerFactory.getLogger(
+      QueryGRPCClientFactory.class);
+  
+  public static final String PORT_KEY = "grpc.client.port";
+  public static final String HOST_KEY = "grpc.client.host";
+  
+  /** The stub. */
+  protected QueryRpcBetaStub stub;
+  
+  /** The serdes factory. */
+  protected PBufIteratorSerdesFactory serdes_factory;
+  
+  /** The channel. */
+  private ManagedChannel channel;
+  
+  @Override
+  public String id() {
+    return "GRPCClient";
+  }
+
+  @Override
+  public Deferred<Object> initialize(final TSDB tsdb) {
+    registerConfig(tsdb);
+    try {
+      channel = ManagedChannelBuilder
+          .forAddress(tsdb.getConfig().getString(HOST_KEY), 
+                      tsdb.getConfig().getInt(PORT_KEY))
+          // TODO - many more config options.
+          .build();
+      stub = QueryRpcBetaGrpc.newStub(channel);
+      serdes_factory = new PBufIteratorSerdesFactory();
+    } catch (Exception e) {
+      LOG.error("Unexpected exception setting up client.", e);
+      return Deferred.fromError(e);
+    }
+    return Deferred.fromResult(null);
+  }
+
+  @Override
+  public Deferred<Object> shutdown() {
+    if (channel != null) {
+      // TODO - graceful
+      channel.shutdownNow();
+    }
+    return Deferred.fromResult(null);
+  }
+
+  @Override
+  public String version() {
+    return "3.0.0";
+  }
+
+  @Override
+  public ReadableTimeSeriesDataStore newInstance(final TSDB tsdb, 
+                                                 final String id) {
+    return this;
+  }
+
+  @Override
+  public TypeToken<? extends TimeSeriesId> idType() {
+    return Const.TS_STRING_ID;
+  }
+
+  @Override
+  public boolean supportsPushdown(Class<? extends QueryNodeConfig> operation) {
+    if (operation == DownsampleConfig.class) {
+      return true;
+    }
+    // TODO - more!
+    return false;
+  }
+
+  @Override
+  public QueryNode newNode(final QueryPipelineContext context, 
+                           final String id) {
+    throw new UnsupportedOperationException("Need a config.");
+  }
+
+  @Override
+  public QueryNode newNode(final QueryPipelineContext context, 
+                           final String id,
+                           final QueryNodeConfig config) {
+    return new QueryGRPCClient(this, context, id, (QuerySourceConfig) config);
+  }
+
+  @Override
+  public Class<? extends QueryNodeConfig> nodeConfigClass() {
+    return QuerySourceConfig.class;
+  }
+
+  @Override
+  public Deferred<TimeSeriesStringId> resolveByteId(
+      final TimeSeriesByteId id,
+      final Span span) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Deferred<List<byte[]>> encodeJoinKeys(
+      final List<String> join_keys,
+      final Span span) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Deferred<List<byte[]>> encodeJoinMetrics(
+      final List<String> join_metrics,
+      final Span span) {
+    throw new UnsupportedOperationException();
+  }
+
+  void registerConfig(final TSDB tsdb) {
+    if (!tsdb.getConfig().hasProperty(PORT_KEY)) {
+      tsdb.getConfig().register(PORT_KEY, 4243, false, 
+          "A port on the server to connect to for the GRPC client.");
+    }
+    if (!tsdb.getConfig().hasProperty(HOST_KEY)) {
+      tsdb.getConfig().register(HOST_KEY, "localhost", false, 
+          "The hostname for GRPC client to connect to.");
+    }
+  }
+  
+  QueryRpcBetaStub stub() {
+    return stub;
+  }
+}
---
+++ implementation/protobuf/src/test/java/net/opentsdb/grpc/TestQueryGRPCClient.java
@@ -0,0 +1,163 @@
+// This file is part of OpenTSDB.
+// Copyright (C) 2018  The OpenTSDB Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package net.opentsdb.grpc;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import com.google.common.collect.Lists;
+
+import io.grpc.stub.StreamObserver;
+import net.opentsdb.data.pbuf.TimeSeriesQueryPB;
+import net.opentsdb.data.pbuf.QueryResultPB.QueryResult;
+import net.opentsdb.grpc.QueryRpcBetaGrpc.QueryRpcBetaStub;
+import net.opentsdb.query.QueryMode;
+import net.opentsdb.query.QueryNode;
+import net.opentsdb.query.QueryPipelineContext;
+import net.opentsdb.query.QuerySourceConfig;
+import net.opentsdb.query.SemanticQuery;
+import net.opentsdb.query.execution.graph.ExecutionGraph;
+import net.opentsdb.query.execution.graph.ExecutionGraphNode;
+import net.opentsdb.query.filter.MetricLiteralFilter;
+import net.opentsdb.utils.UnitTestException;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ QueryRpcBetaStub.class, QueryResult.class })
+public class TestQueryGRPCClient {
+
+  private QueryGRPCClientFactory factory;
+  private QueryPipelineContext context;
+  private QueryRpcBetaStub stub;
+  private QueryNode upstream;
+  
+  @Before
+  public void before() throws Exception {
+    factory = mock(QueryGRPCClientFactory.class);
+    context = mock(QueryPipelineContext.class);
+    stub = mock(QueryRpcBetaStub.class);
+    when(factory.stub()).thenReturn(stub);
+    
+    upstream = mock(QueryNode.class);
+    when(context.upstream(any(QueryNode.class)))
+      .thenReturn(Lists.newArrayList(upstream));
+  }
+  
+  @Test
+  public void fetchNext() throws Exception {
+    SemanticQuery q = SemanticQuery.newBuilder()
+        .setStart("1h-ago")
+        .setMode(QueryMode.SINGLE)
+        .setExecutionGraph(ExecutionGraph.newBuilder()
+            .setId("g1")
+            .addNode(ExecutionGraphNode.newBuilder()
+                .setId("DataSource")
+                .setConfig(QuerySourceConfig.newBuilder()
+                    .setMetric(MetricLiteralFilter.newBuilder()
+                        .setMetric("sys.cpu.user")
+                        .build())
+                    .setId("DataSource")
+                    .build())
+                .build())
+            .build())
+        .build();
+    
+    QuerySourceConfig config = (QuerySourceConfig) QuerySourceConfig.newBuilder()
+        .setMetric(MetricLiteralFilter.newBuilder()
+            .setMetric("sys.cpu.user")
+            .build())
+        .setQuery(q)
+        .setId("DataSource")
+        .build();
+    
+    QueryGRPCClient node = new QueryGRPCClient(factory, context, null, config);
+    node.initialize(null);
+    node.fetchNext(null);
+    verify(stub, times(1)).remoteQuery(any(TimeSeriesQueryPB.TimeSeriesQuery.class), 
+        any(StreamObserver.class));
+    verify(upstream, never()).onNext(any(net.opentsdb.query.QueryResult.class));
+    verify(upstream, never()).onError(any(Throwable.class));
+    verify(upstream, never()).onComplete(any(QueryNode.class), anyLong(), anyLong());
+    
+    // oops!
+    doThrow(new UnitTestException()).when(stub)
+    .remoteQuery(any(TimeSeriesQueryPB.TimeSeriesQuery.class), 
+      any(StreamObserver.class));
+    
+    node = new QueryGRPCClient(factory, context, null, config);
+    node.initialize(null);
+    node.fetchNext(null);
+    verify(upstream, never()).onNext(any(net.opentsdb.query.QueryResult.class));
+    verify(upstream, times(1)).onError(any(UnitTestException.class));
+    verify(upstream, never()).onComplete(any(QueryNode.class), anyLong(), anyLong());
+  }
+  
+  @Test
+  public void onComplete() throws Exception {
+    QuerySourceConfig config = mock(QuerySourceConfig.class);
+    QueryGRPCClient node = new QueryGRPCClient(factory, context, null, config);
+    node.initialize(null);
+    verify(upstream, never()).onNext(any(net.opentsdb.query.QueryResult.class));
+    verify(upstream, never()).onError(any(Throwable.class));
+    verify(upstream, never()).onComplete(any(QueryNode.class), anyLong(), anyLong());
+    
+    node.onComplete(node, 0, 0);
+    verify(upstream, never()).onNext(any(net.opentsdb.query.QueryResult.class));
+    verify(upstream, never()).onError(any(Throwable.class));
+    verify(upstream, times(1)).onComplete(any(QueryNode.class), anyLong(), anyLong());
+  }
+  
+  @Test
+  public void onNextPbuf() throws Exception {
+    QuerySourceConfig config = mock(QuerySourceConfig.class);
+    QueryGRPCClient node = new QueryGRPCClient(factory, context, null, config);
+    node.initialize(null);
+    verify(upstream, never()).onNext(any(net.opentsdb.query.QueryResult.class));
+    verify(upstream, never()).onError(any(Throwable.class));
+    verify(upstream, never()).onComplete(any(QueryNode.class), anyLong(), anyLong());
+    
+    node.onNext(mock(QueryResult.class));
+    verify(upstream, times(1)).onNext(any(net.opentsdb.query.QueryResult.class));
+    verify(upstream, never()).onError(any(Throwable.class));
+    verify(upstream, times(1)).onComplete(any(QueryNode.class), anyLong(), anyLong());
+  }
+  
+  @Test
+  public void onError() throws Exception {
+    QuerySourceConfig config = mock(QuerySourceConfig.class);
+    QueryGRPCClient node = new QueryGRPCClient(factory, context, null, config);
+    node.initialize(null);
+    verify(upstream, never()).onNext(any(net.opentsdb.query.QueryResult.class));
+    verify(upstream, never()).onError(any(Throwable.class));
+    verify(upstream, never()).onComplete(any(QueryNode.class), anyLong(), anyLong());
+    
+    node.onError(new UnitTestException());
+    verify(upstream, never()).onNext(any(net.opentsdb.query.QueryResult.class));
+    verify(upstream, times(1)).onError(any(UnitTestException.class));
+    verify(upstream, never()).onComplete(any(QueryNode.class), anyLong(), anyLong());
+  }
+  
+}
