cbe45b6497c141b818476844c45e21a25e74caab
Chris Larsen <clarsen@yahoo-inc.com>
2018-10-11 13:39:20 -0700

    CORE: - Add parsing and serdes of the Scalar fill policy for v3.

--- core/src/test/java/net/opentsdb/query/interpolation/types/numeric/TestScalarNumericInterpolatorConfig.java
+++ core/src/test/java/net/opentsdb/query/interpolation/types/numeric/TestScalarNumericInterpolatorConfig.java
@@ -16,16 +16,22 @@ package net.opentsdb.query.interpolation.types.numeric;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
 
 import org.junit.Test;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
+import net.opentsdb.core.TSDB;
 import net.opentsdb.data.types.numeric.NumericType;
 import net.opentsdb.query.QueryFillPolicy.FillWithRealPolicy;
 import net.opentsdb.query.interpolation.types.numeric.NumericInterpolatorConfig;
 import net.opentsdb.query.interpolation.types.numeric.ScalarNumericInterpolatorConfig;
 import net.opentsdb.query.pojo.FillPolicy;
+import net.opentsdb.utils.JSON;
 
 public class TestScalarNumericInterpolatorConfig {
 
@@ -111,4 +117,60 @@ public class TestScalarNumericInterpolatorConfig {
       fail("Expected IllegalArgumentException");
     } catch (IllegalArgumentException e) { }
   }
+
+  @Test
+  public void serdes() throws Exception {
+    NumericInterpolatorConfig config = (NumericInterpolatorConfig) 
+        ScalarNumericInterpolatorConfig.newBuilder()
+        .setValue(42)
+        .setFillPolicy(FillPolicy.NOT_A_NUMBER)
+        .setRealFillPolicy(FillWithRealPolicy.PREFER_NEXT)
+        .setDataType(NumericType.TYPE.toString())
+        .setType("Default")
+        .build();
+    
+    String json_int = JSON.serializeToString(config);
+    assertTrue(json_int.contains("\"type\":\"Default\""));
+    assertTrue(json_int.contains("\"dataType\":\"net.opentsdb.data.types.numeric.NumericType\""));
+    assertTrue(json_int.contains("\"fillPolicy\":\"SCALAR\""));
+    assertTrue(json_int.contains("\"realFillPolicy\":\"PREFER_NEXT\""));
+    assertTrue(json_int.contains("\"value\":42"));
+    
+    config = (NumericInterpolatorConfig) 
+        ScalarNumericInterpolatorConfig.newBuilder()
+        .setValue(42.75)
+        .setFillPolicy(FillPolicy.NOT_A_NUMBER)
+        .setRealFillPolicy(FillWithRealPolicy.PREFER_NEXT)
+        .setDataType(NumericType.TYPE.toString())
+        .build();
+    
+    String json_double = JSON.serializeToString(config);
+    assertFalse(json_double.contains("\"type\":\"Default\""));
+    assertTrue(json_double.contains("\"dataType\":\"net.opentsdb.data.types.numeric.NumericType\""));
+    assertTrue(json_double.contains("\"fillPolicy\":\"SCALAR\""));
+    assertTrue(json_double.contains("\"realFillPolicy\":\"PREFER_NEXT\""));
+    assertTrue(json_double.contains("\"value\":42.75"));
+    
+    TSDB tsdb = mock(TSDB.class);
+    JsonNode node = JSON.getMapper().readTree(json_int);
+    config = (NumericInterpolatorConfig) ScalarNumericInterpolatorConfig
+        .parse(JSON.getMapper(), tsdb, node);
+    assertTrue(((ScalarNumericInterpolatorConfig) config).isInteger());
+    assertEquals(42, ((ScalarNumericInterpolatorConfig) config).longValue());
+    assertEquals(FillPolicy.SCALAR, config.getFillPolicy());
+    assertEquals(FillWithRealPolicy.PREFER_NEXT, config.getRealFillPolicy());
+    assertEquals("net.opentsdb.data.types.numeric.NumericType", config.getDataType());
+    assertEquals("Default", config.getType());
+    
+    node = JSON.getMapper().readTree(json_double);
+    config = (NumericInterpolatorConfig) ScalarNumericInterpolatorConfig
+        .parse(JSON.getMapper(), tsdb, node);
+    assertFalse(((ScalarNumericInterpolatorConfig) config).isInteger());
+    assertEquals(42.75, ((ScalarNumericInterpolatorConfig) config).doubleValue(), 0.001);
+    assertEquals(FillPolicy.SCALAR, config.getFillPolicy());
+    assertEquals(FillWithRealPolicy.PREFER_NEXT, config.getRealFillPolicy());
+    assertEquals("net.opentsdb.data.types.numeric.NumericType", config.getDataType());
+    assertNull(config.getType());
+  }
+  
 }
