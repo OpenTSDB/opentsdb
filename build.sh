2ac604d1611f1b6f6f353d24eebea5026fe33f7d
Chris Larsen <clarsen@yahoo-inc.com>
2018-12-01 13:51:10 -0800

    PARENT: - Remove the old build files since we use Maven now.

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -xe
-test -f configure || ./bootstrap
-test -d build || mkdir build
-cd build
-test -f Makefile || ../configure "$@"
-MAKE=make
-[ `uname -s` = "FreeBSD" ] && MAKE=gmake
-exec ${MAKE} "$@"
