f6ea437e6d5a50238f7bcff0560831bb866b4795
Dai Feng <Eduardo95@users.noreply.github.com>
2018-12-18 23:54:32 +0800

    For branch next, add an expression function named FirstDifference, wh… (#1458)
    * For branch next, add an expression function named FirstDifference, which calculates the first difference of a time series. I noticed there is MovingAverage calculation, so I thought maybe I can enrich the mathematics functions into that.
    
    * add some unit tests for FirstDifference

--- src/query/expression/ExpressionFactory.java
+++ src/query/expression/ExpressionFactory.java
@@ -37,6 +37,7 @@ public final class ExpressionFactory {
     available_functions.put("highestMax", new HighestMax());
     available_functions.put("shift", new TimeShift());
     available_functions.put("timeShift", new TimeShift());
+    available_functions.put("firstDiff", new FirstDifference());
   }
   
   /** Don't instantiate me! */
