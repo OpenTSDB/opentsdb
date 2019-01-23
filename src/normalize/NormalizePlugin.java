e58dd54060e2ed40a4e3a323f0d8e494f37c6c6c
gnydick <gnydick@nydick.net>
2019-01-23 12:02:32 -0800

    hopefully working

--- src/normalize/NormalizePlugin.java
+++ src/normalize/NormalizePlugin.java
@@ -3,20 +3,16 @@ package net.opentsdb.normalize;
 import com.stumbleupon.async.Deferred;
 import net.opentsdb.core.TSDB;
 import net.opentsdb.stats.StatsCollector;
-import net.opentsdb.utils.PluginConfig;
 
 public abstract class NormalizePlugin {
 
-    private PluginConfig pluginConfig;
 
-    public  NormalizePlugin(PluginConfig pluginConfig){
-        this.pluginConfig = pluginConfig;
-    };
-
-    private NormalizePlugin() {}
     public abstract void initialize(final TSDB tsdb);
+
     public abstract Deferred<Object> shutdown();
+
     public abstract String version();
+
     public abstract void collectStats(final StatsCollector collector);
 
 
