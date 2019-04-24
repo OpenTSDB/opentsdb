ccfe96a434ade71d8d64f3ec52c953843b7248f3
Chris Larsen <clarsen@verizonmedia.com>
2019-04-24 11:55:04 -0700

    CORE: (#1614)
    * CORE:
    - Add array count, first and last aggregators.
    - Address CR.

---
+++ core/src/main/java/net/opentsdb/data/types/numeric/aggregators/ArrayCountFactory.java
@@ -0,0 +1,103 @@
+// This file is part of OpenTSDB.
+// Copyright (C) 2019  The OpenTSDB Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package net.opentsdb.data.types.numeric.aggregators;
+
+import java.util.Arrays;
+
+import com.google.common.base.Strings;
+import com.stumbleupon.async.Deferred;
+
+import net.opentsdb.core.TSDB;
+
+/**
+ * Just the count.
+ * <b>Note:</b> This aggregator will always return a long array and ignores
+ * infectious NaNs, treating NaNs as non-values.
+ * 
+ * @since 3.0
+ */
+public class ArrayCountFactory extends BaseArrayFactory {
+
+  public static final String TYPE = "Count";
+  
+  @Override
+  public NumericArrayAggregator newAggregator(final boolean infectious_nan) {
+    return new ArrayCount(infectious_nan);
+  }
+
+  @Override
+  public String type() {
+    return TYPE;
+  }
+
+  @Override
+  public Deferred<Object> initialize(final TSDB tsdb, final String id) {
+    this.id = Strings.isNullOrEmpty(id) ? TYPE : id;
+    return Deferred.fromResult(null);
+  }
+  
+  public static class ArrayCount extends BaseArrayAggregator {
+
+    public ArrayCount(final boolean infectious_nans) {
+      super(infectious_nans);
+    }
+
+    @Override
+    public void accumulate(final long[] values, 
+                           final int from, 
+                           final int to) {
+      if (long_accumulator == null) {
+        long_accumulator = new long[to - from];
+        Arrays.fill(long_accumulator, 1);
+        return;
+      }
+      
+      if (to - from != long_accumulator.length) {
+        throw new IllegalArgumentException("Values of length " 
+            + (to - from) + " did not match the original lengh of " 
+            + long_accumulator.length);
+      }
+      int idx = 0;
+      for (int i = from; i < to; i++) {
+        long_accumulator[idx++]++;
+      }
+    }
+
+    @Override
+    public void accumulate(final double[] values, 
+                           final int from, 
+                           final int to) {
+      if (long_accumulator == null) {
+        long_accumulator = new long[to - from];
+      }
+      
+      if (to - from != long_accumulator.length) {
+        throw new IllegalArgumentException("Values of length " 
+            + (to - from) + " did not match the original lengh of " 
+            + long_accumulator.length);
+      }
+      
+      int idx = 0;
+      for (int i = from; i < to; i++) {
+        if (Double.isNaN(values[i])) {
+          idx++;
+        } else {
+          long_accumulator[idx++]++;
+        }
+      }
+    }
+    
+  }
+}
---
+++ core/src/main/java/net/opentsdb/data/types/numeric/aggregators/ArrayLastFactory.java
@@ -0,0 +1,120 @@
+// This file is part of OpenTSDB.
+// Copyright (C) 2019  The OpenTSDB Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package net.opentsdb.data.types.numeric.aggregators;
+
+import java.util.Arrays;
+
+import com.google.common.base.Strings;
+import com.stumbleupon.async.Deferred;
+
+import net.opentsdb.core.TSDB;
+
+/**
+ * Returns the last values honoring infectious NaN.
+ * 
+ * @since 3.0
+ */
+public class ArrayLastFactory extends BaseArrayFactory {
+
+  public static final String TYPE = "Last";
+  
+  @Override
+  public NumericArrayAggregator newAggregator(final boolean infectious_nan) {
+    return new ArrayLast(infectious_nan);
+  }
+
+  @Override
+  public String type() {
+    return TYPE;
+  }
+
+  @Override
+  public Deferred<Object> initialize(final TSDB tsdb, final String id) {
+    this.id = Strings.isNullOrEmpty(id) ? TYPE : id;
+    return Deferred.fromResult(null);
+  }
+  
+  public static class ArrayLast extends BaseArrayAggregator {
+
+    public ArrayLast(final boolean infectious_nans) {
+      super(infectious_nans);
+    }
+
+    @Override
+    public void accumulate(final long[] values, 
+                           final int from, 
+                           final int to) {
+      if (double_accumulator == null && long_accumulator == null) {
+        long_accumulator = Arrays.copyOfRange(values, from, to);
+        return;
+      }
+      
+      if (long_accumulator != null) {
+        int idx = 0;
+        for (int i = from; i < to; i++) {
+          long_accumulator[idx++] = values[i];
+        }
+      } else {
+        if (to - from != double_accumulator.length) {
+          throw new IllegalArgumentException("Values of length " 
+              + (to - from) + " did not match the original lengh of " 
+              + double_accumulator.length);
+        }
+        int idx = 0;
+        for (int i = from; i < to; i++) {
+          double_accumulator[idx++] = values[i];
+        }
+      }
+    }
+
+    @Override
+    public void accumulate(final double[] values, 
+                           final int from, 
+                           final int to) {
+      if (double_accumulator == null && long_accumulator == null) {
+        double_accumulator = Arrays.copyOfRange(values, from, to);
+        return;
+      }
+      
+      if (double_accumulator == null) {
+        // copy longs as we may have some NaNs in here
+        double_accumulator = new double[long_accumulator.length];
+        for (int i = 0; i < long_accumulator.length; i++) {
+          double_accumulator[i] = long_accumulator[i];
+        }
+        long_accumulator = null;
+      }
+      
+      if (to - from != double_accumulator.length) {
+        throw new IllegalArgumentException("Values of length " 
+            + (to - from) + " did not match the original lengh of " 
+            + double_accumulator.length);
+      }
+      
+      int idx = 0;
+      for (int i = from; i < to; i++) {
+        if (Double.isNaN(values[i])) {
+          if (infectious_nans) {
+            double_accumulator[idx] = values[i];
+          }
+        } else {
+          double_accumulator[idx] = values[i];
+        }
+        idx++;
+      }
+    }
+    
+  }
+}
