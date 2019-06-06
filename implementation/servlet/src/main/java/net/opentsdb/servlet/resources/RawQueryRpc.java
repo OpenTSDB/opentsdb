a3ada889533fbbf6dec4abf964bc68dd748b68ab
Chris Larsen <clarsen@verizonmedia.com>
2019-06-06 15:10:28 -0700

    SERVLET: - Return 403s properly in QueryExecutionExceptiosn if the auth status is not   successful.

--- implementation/servlet/src/main/java/net/opentsdb/servlet/resources/RawQueryRpc.java
+++ implementation/servlet/src/main/java/net/opentsdb/servlet/resources/RawQueryRpc.java
@@ -117,8 +117,7 @@ public class RawQueryRpc {
       if (request.getAttribute(AuthFilter.AUTH_STATE_KEY) == null || 
           ((AuthState) request.getAttribute(AuthFilter.AUTH_STATE_KEY))
             .getStatus() != AuthStatus.SUCCESS) {
-        throw new WebApplicationException("Access denied.", 
-            Response.Status.FORBIDDEN);
+        throw new QueryExecutionException("Autentication failed.", 403);
       }
       auth_state = (AuthState) request.getAttribute(AuthFilter.AUTH_STATE_KEY);
     } else {
