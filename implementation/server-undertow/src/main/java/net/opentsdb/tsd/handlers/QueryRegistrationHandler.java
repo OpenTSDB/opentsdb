325640015a729186cf340d8a0c2d131256657ac9
Chris Larsen <clarsen@verizonmedia.com>
2019-09-16 18:13:27 -0700

    UNDERTOW: - Add missing license and cleanup the QueryRegistrationHandler.

--- implementation/server-undertow/src/main/java/net/opentsdb/tsd/handlers/QueryRegistrationHandler.java
+++ implementation/server-undertow/src/main/java/net/opentsdb/tsd/handlers/QueryRegistrationHandler.java
@@ -1,7 +1,18 @@
+// This file is part of OpenTSDB.
+// Copyright (C) 2019  The OpenTSDB Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package net.opentsdb.tsd.handlers;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import io.undertow.server.ExchangeCompletionListener;
 import io.undertow.server.HttpHandler;
@@ -9,14 +20,12 @@ import io.undertow.server.HttpServerExchange;
 import io.undertow.util.HttpString;
 import net.opentsdb.core.TSDB;
 import net.opentsdb.servlet.applications.OpenTSDBApplication;
-import net.opentsdb.threadpools.TSDTask;
 
 public class QueryRegistrationHandler implements HttpHandler {
   
-  private static final Logger LOG = LoggerFactory.getLogger(QueryRegistrationHandler.class);
-
   private TSDB tsdb;
   private final HttpHandler next;
+  
   public QueryRegistrationHandler(final TSDB tsdb, final HttpHandler next) {
     this.tsdb = tsdb;
     this.next = next;
