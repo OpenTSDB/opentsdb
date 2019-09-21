36e1fe2298c6ebe59b530427feb29086641b01e7
Chris Larsen <clarsen@verizonmedia.com>
2019-09-20 22:18:23 -0700

    CORE: - Start a basic pre-agg filter in the QueryContextFilter class.

--- core/src/main/java/net/opentsdb/query/DefaultQueryContextFilter.java
+++ core/src/main/java/net/opentsdb/query/DefaultQueryContextFilter.java
@@ -14,6 +14,8 @@
 // limitations under the License.
 package net.opentsdb.query;
 
+import java.time.temporal.TemporalAmount;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -21,14 +23,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.common.graph.GraphBuilder;
+import com.google.common.graph.MutableGraph;
 import com.stumbleupon.async.Deferred;
 
 import net.opentsdb.auth.AuthState;
 import net.opentsdb.configuration.ConfigurationEntrySchema;
 import net.opentsdb.core.BaseTSDBPlugin;
 import net.opentsdb.core.TSDB;
+import net.opentsdb.data.TimeStamp;
 import net.opentsdb.query.TimeSeriesQuery.CacheMode;
+import net.opentsdb.query.filter.ChainFilter;
+import net.opentsdb.query.filter.NestedQueryFilter;
+import net.opentsdb.query.filter.QueryFilter;
+import net.opentsdb.query.filter.TagValueFilter;
+import net.opentsdb.query.filter.TagValueLiteralOrFilter;
+import net.opentsdb.query.processor.groupby.GroupByConfig;
 
 /**
  * Stub class for a super simple context filter that filters on the user and
@@ -48,8 +61,11 @@ public class DefaultQueryContextFilter extends BaseTSDBPlugin
   private static final TypeReference<
     Map<String, Map<String, Map<String, String>>>> HEADER_FILTERS =
       new TypeReference<Map<String, Map<String, Map<String, String>>>>() { };
+  private static final TypeReference<Map<String, PreAggConfig>> PREAGG_FILTERS =
+      new TypeReference<Map<String, PreAggConfig>>() { };
   private static final String HEADER_KEY = "tsd.queryfilter.filter.headers";
   private static final String USER_KEY = "tsd.queryfilter.filter.users";
+  private static final String PREAGG_KEY = "tsd.queryfilter.filter.preagg";
   
   @Override
   public Deferred<Object> initialize(final TSDB tsdb, final String id) {
@@ -73,6 +89,17 @@ public class DefaultQueryContextFilter extends BaseTSDBPlugin
           .setDefaultValue(Maps.newHashMap())
           .setDescription("TODO")
           .setType(USER_FILTERS)
+          .setSource(this.getClass().toString())
+          .isDynamic()
+          .build());
+    }
+    
+    if (!tsdb.getConfig().hasProperty(PREAGG_KEY)) {
+      tsdb.getConfig().register(ConfigurationEntrySchema.newBuilder()
+          .setKey(PREAGG_KEY)
+          .setDefaultValue(Maps.newHashMap())
+          .setDescription("TODO")
+          .setType(PREAGG_FILTERS)
           .setSource(this.getClass().toString())
           .isDynamic()
           .build());
@@ -135,6 +162,135 @@ public class DefaultQueryContextFilter extends BaseTSDBPlugin
       }
     }
     
+    // TODO - tons of stuff to do here, e.g. nulls from the group by or the 
+    // filters.
+    Map<String, PreAggConfig> preagg_filters = 
+        tsdb.getConfig().getTyped(PREAGG_KEY, PREAGG_FILTERS);
+    if (preagg_filters != null && !preagg_filters.isEmpty()) {
+      Map<String, QueryNodeConfig> rebuilt = null;
+      boolean rebuild = false;
+      MutableGraph<QueryNodeConfig> graph = null;
+      
+      for (final QueryNodeConfig config : query.getExecutionGraph()) {
+        if (!(config instanceof TimeSeriesDataSourceConfig)) {
+          continue;
+        }
+        
+        TimeSeriesDataSourceConfig tsdc = (TimeSeriesDataSourceConfig) config;
+        if (tsdc.getMetric() == null) {
+          continue;
+        }
+        
+        // TODO - namespace field
+        final String namespace = tsdc.getMetric().getMetric().substring(0, 
+            tsdc.getMetric().getMetric().indexOf('.'));
+        final PreAggConfig preagg = preagg_filters.get(namespace);
+        if (preagg == null) {
+          continue;
+        }
+        
+        if (!(Strings.isNullOrEmpty(tsdc.getFilterId()))) {
+          if (hasAgg(query.getFilter(tsdc.getFilterId()))) {
+            // Nothing to modify here.
+            continue;
+          }
+        } else if (tsdc.getFilter() != null) {
+          if (hasAgg(tsdc.getFilter())) {
+            // Nothing to modify here.
+            continue;
+          }
+        }
+        
+        // check timestamp first. If the query is too early then we can't change
+        // it.
+        if (query.startTime().epoch() < preagg.startEpoch) {
+          // TODO - set raw
+          continue;
+        }
+        
+        if (tsdc.timeShifts() != null) {
+          final TimeStamp ts = query.startTime().getCopy();
+          // TODO - previous or next, figure it out.
+          ts.add((TemporalAmount) tsdc.timeShifts().getValue()); 
+          if (ts.epoch() < preagg.startEpoch) {
+            // TODO - set raw
+            continue;
+          }
+        }
+        
+        // see if we match the tags
+        if (!(Strings.isNullOrEmpty(tsdc.getFilterId()))) {
+          if (hasTagKey(query.getFilter(tsdc.getFilterId()), preagg.excludedAggTags)) {
+            if (rebuilt == null) {
+              rebuilt = Maps.newHashMap();
+            }
+            tsdc = (TimeSeriesDataSourceConfig) 
+                ((TimeSeriesDataSourceConfig.Builder) tsdc.toBuilder())
+                .setQueryFilter(rebuildFilter(tsdc.getFilter(), "raw"))
+                .build();
+            rebuilt.put(tsdc.getId(), tsdc);
+            continue;
+          }
+        } else if (tsdc.getFilter() != null && 
+                   hasTagKey(tsdc.getFilter(), preagg.excludedAggTags)) {
+          if (rebuilt == null) {
+            rebuilt = Maps.newHashMap();
+          }
+          tsdc = (TimeSeriesDataSourceConfig) 
+              ((TimeSeriesDataSourceConfig.Builder) tsdc.toBuilder())
+              .setQueryFilter(rebuildFilter(tsdc.getFilter(), "raw"))
+              .build();
+          rebuilt.put(tsdc.getId(), tsdc);
+          continue;
+        }
+        
+        rebuild = true;
+        if (graph == null) {
+          graph = buildGraph(query);
+        }
+        final String agg = findAgg(tsdc.getId(), graph, tsdc);
+        if (!(Strings.isNullOrEmpty(tsdc.getFilterId()))) {
+          // re-write
+          if (rebuilt == null) {
+            rebuilt = Maps.newHashMap();
+          }
+          tsdc = (TimeSeriesDataSourceConfig) 
+              ((TimeSeriesDataSourceConfig.Builder) tsdc.toBuilder())
+              .setFilterId(null)
+              .setQueryFilter(rebuildFilter(
+                  query.getFilter(tsdc.getFilterId()), agg))
+              .build();
+          rebuilt.put(tsdc.getId(), tsdc);
+        } else {
+          // re-write
+          if (rebuilt == null) {
+            rebuilt = Maps.newHashMap();
+          }
+          tsdc = (TimeSeriesDataSourceConfig) 
+              ((TimeSeriesDataSourceConfig.Builder) tsdc.toBuilder())
+              .setQueryFilter(rebuildFilter(tsdc.getFilter(), agg))
+              .build();
+          rebuilt.put(tsdc.getId(), tsdc);
+        }
+      }
+      
+      if (rebuild) {
+        if (builder == null) {
+          builder = ((SemanticQuery) query).toBuilder();
+        }
+        
+        List<QueryNodeConfig> new_configs = Lists.newArrayList();
+        for (final QueryNodeConfig extant : query.getExecutionGraph()) {
+          if (!rebuilt.containsKey(extant.getId())) {
+            new_configs.add(extant);
+          }
+        }
+        
+        new_configs.addAll(rebuilt.values());
+        builder.setExecutionGraph(new_configs);
+      }
+    }
+    
     return builder != null ? builder.build() : query;
   }
 
@@ -143,4 +299,116 @@ public class DefaultQueryContextFilter extends BaseTSDBPlugin
     return "DefaultQueryContextFilter";
   }
   
+  public static class PreAggConfig {
+    public int startEpoch;
+    public List<String> excludedAggTags;
+  }
+  
+  String findAgg(final String id, final MutableGraph<QueryNodeConfig> graph, final QueryNodeConfig config) {
+    return findAgg(graph, config);
+  }
+  
+  String findAgg(final MutableGraph<QueryNodeConfig> graph, final QueryNodeConfig config) {
+    if (config instanceof GroupByConfig) {
+      return ((GroupByConfig) config).getAggregator();
+    }
+    for (final QueryNodeConfig pred : graph.predecessors(config)) {
+      final String agg = findAgg(graph, pred);
+      if (!Strings.isNullOrEmpty(agg)) {
+        return agg;
+      }
+    }
+    return null;
+  }
+  
+  boolean hasAgg(final QueryFilter filter) {
+    if (filter instanceof TagValueFilter) {
+      if (((TagValueFilter) filter).getTagKey().equals("_aggregate")) {
+        return true;
+      }
+    }
+    
+    if (filter instanceof ChainFilter) {
+      for (final QueryFilter sub : (((ChainFilter) filter).getFilters())) {
+        if (hasAgg(sub)) {
+          return true;
+        }
+      }
+    }
+    
+    if (filter instanceof NestedQueryFilter) {
+      return hasAgg(((NestedQueryFilter) filter).getFilter());
+    }
+    return false;
+  }
+  
+  boolean hasTagKey(final QueryFilter filter, final List<String> keys) {
+    if (filter instanceof TagValueFilter) {
+      if (keys.contains(((TagValueFilter) filter).getTagKey())) {
+        return true;
+      }
+    }
+    
+    if (filter instanceof ChainFilter) {
+      for (final QueryFilter sub : (((ChainFilter) filter).getFilters())) {
+        if (hasAgg(sub)) {
+          return true;
+        }
+      }
+    }
+    
+    if (filter instanceof NestedQueryFilter) {
+      return hasAgg(((NestedQueryFilter) filter).getFilter());
+    }
+    return false;
+  }
+  
+  // TODO - these two don't handle all cases like ORs or multiple nestings, etc.
+  QueryFilter rebuildFilter(final QueryFilter filter, final String agg) {
+    if (filter == null) {
+      return TagValueLiteralOrFilter.newBuilder()
+          .setFilter(agg)
+          .setKey("_aggregate")
+          .build();
+    }
+    if (filter instanceof ChainFilter) {
+      return rebuildFilter((ChainFilter) filter, agg);
+    }
+    if (filter instanceof NestedQueryFilter) {
+      return rebuildFilter(((NestedQueryFilter) filter).getFilter(), agg);
+    }
+    return null;
+  }
+  
+  QueryFilter rebuildFilter(final ChainFilter filter, final String agg) {
+    final ChainFilter.Builder builder = ChainFilter.newBuilder()
+        .setOp(filter.getOp());
+    builder.addFilter(TagValueLiteralOrFilter.newBuilder()
+        .setFilter(agg)
+        .setKey("_aggregate")
+        .build());
+    for (final QueryFilter sub : filter.getFilters()) {
+      builder.addFilter(sub);
+    }
+    return builder.build();
+  }
+  
+  static MutableGraph<QueryNodeConfig> buildGraph(final TimeSeriesQuery query) {
+    MutableGraph<QueryNodeConfig> graph = GraphBuilder.directed()
+        .allowsSelfLoops(false)
+        .build();
+    Map<String, QueryNodeConfig> map = Maps.newHashMap();
+    for (final QueryNodeConfig config : query.getExecutionGraph()) {
+      graph.addNode(config);
+      map.put(config.getId(), config);
+    }
+    for (final QueryNodeConfig config : query.getExecutionGraph()) {
+      if (config.getSources() != null) {
+        for (final Object source : config.getSources()) {
+          graph.putEdge(config, map.get((String) source));
+        }
+      }
+    }
+    return graph;
+  }
 }
