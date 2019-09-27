d3f5bb89d74edf5817d9aa94c351a2b636e5b57a
Chris Larsen <clarsen@verizonmedia.com>
2019-09-27 15:03:21 -0700

    SERVLET: - Fix typo in the RPC classes around authentication.

--- implementation/servlet/src/main/java/net/opentsdb/servlet/resources/ExpressionRpc.java
+++ implementation/servlet/src/main/java/net/opentsdb/servlet/resources/ExpressionRpc.java
@@ -160,7 +160,7 @@ public class ExpressionRpc {
       if (request.getAttribute(AuthFilter.AUTH_STATE_KEY) == null || 
           ((AuthState) request.getAttribute(AuthFilter.AUTH_STATE_KEY))
             .getStatus() != AuthStatus.SUCCESS) {
-        throw new QueryExecutionException("Autentication failed.", 403);
+        throw new QueryExecutionException("Authentication failed.", 403);
       }
       auth_state = (AuthState) request.getAttribute(AuthFilter.AUTH_STATE_KEY);
     } else {
