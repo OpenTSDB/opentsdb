5a2e79f1b7bfce27f722fc63abab38a3724ff156
Chris Larsen <clarsen@verizonmedia.com>
2021-01-11 20:03:50 -0800

    ES: - Add a null check on the namespace skip meta config.

--- implementation/elasticsearch/src/main/java/net/opentsdb/meta/NamespacedAggregatedDocumentSchema.java
+++ implementation/elasticsearch/src/main/java/net/opentsdb/meta/NamespacedAggregatedDocumentSchema.java
@@ -238,9 +238,13 @@ public class NamespacedAggregatedDocumentSchema extends BaseTSDBPlugin implement
     String namespace = temp.substring(0, idx);
     final String metric = temp.substring(idx + 1);
 
-    if (namespaces.containsKey(namespace)) {
+    if (namespaces != null && namespaces.containsKey(namespace)) {
       String retention = namespaces.get(namespace);
       long retention_ms = DateTime.parseDuration(retention);
+      if (LOG.isDebugEnabled()) {
+        LOG.debug("Skipping meta for namespace [" + namespace + "] due to the "
+            + "query being beyond retention.");
+      }
       if (System.currentTimeMillis() - queryPipelineContext.query().startTime().msEpoch() > retention_ms) {
         NamespacedAggregatedDocumentResult result = new NamespacedAggregatedDocumentResult(
             MetaResult.NO_DATA_FALLBACK, skip_meta_ex, null);
