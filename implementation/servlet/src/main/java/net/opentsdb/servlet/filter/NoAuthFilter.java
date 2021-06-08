5bcd7dbcf11a9039476f9ee78e8779db09a95089
Chris Larsen <clarsen@verizonmedia.com>
2021-06-08 12:15:40 -0700

    SERVLET: - Add a MultiAuthFilter to chain across multiple auth filters in case   we need to accept more than one type, e.g. a cookie and MTLS.
    OKTA/ATHENZ:
    - Implement the new method for the chain filter.

--- implementation/servlet/src/main/java/net/opentsdb/servlet/filter/NoAuthFilter.java
+++ implementation/servlet/src/main/java/net/opentsdb/servlet/filter/NoAuthFilter.java
@@ -1,5 +1,20 @@
+// This file is part of OpenTSDB.
+// Copyright (C) 2021  The OpenTSDB Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package net.opentsdb.servlet.filter;
 
+import net.opentsdb.auth.AuthState;
 import net.opentsdb.auth.Authorization;
 
 import javax.servlet.FilterChain;
@@ -12,14 +27,67 @@ import javax.servlet.http.HttpServletRequestWrapper;
 import java.io.IOException;
 import java.security.Principal;
 
+/**
+ * Simple filter that sets a "NoAuth" principal.
+ *
+ * @since 3.0
+ */
 public class NoAuthFilter implements AuthFilter {
   private final Principal principal;
+  private final AuthState authState;
 
   public NoAuthFilter() {
     principal = new Principal() {
       @Override
       public String getName() {
         return "NoAuth";
+      }
+    };
+
+    authState = new AuthState() {
+      @Override
+      public String getUser() {
+        return principal.getName();
+      }
+
+      @Override
+      public Principal getPrincipal() {
+        return principal;
+      }
+
+      @Override
+      public AuthStatus getStatus() {
+        return AuthStatus.UNAUTHORIZED;
+      }
+
+      @Override
+      public String getMessage() {
+        return "NoAuth filter is loaded.";
+      }
+
+      @Override
+      public Throwable getException() {
+        return null;
+      }
+
+      @Override
+      public String getTokenType() {
+        return null;
+      }
+
+      @Override
+      public byte[] getToken() {
+        return null;
+      }
+
+      @Override
+      public boolean hasRole(String role) {
+        return false;
+      }
+
+      @Override
+      public boolean hasPermission(String action) {
+        return false;
       }
     };
   }
@@ -37,6 +105,7 @@ public class NoAuthFilter implements AuthFilter {
         return principal;
       }
     };
+    request.setAttribute(AUTH_STATE_KEY, authState);
     chain.doFilter(wrapper, response);
   }
 
@@ -48,5 +117,10 @@ public class NoAuthFilter implements AuthFilter {
   @Override
   public Authorization authorization() {
     return null;
+  }
+
+  @Override
+  public AuthState authenticate(ServletRequest servletRequest) {
+    return authState;
   }
 }
