c9b1778686fc53840bba56566704737aeb07fa9c
Chris Larsen <clarsen@verizonmedia.com>
2021-07-29 14:47:34 -0700

    Fix #1959 by updating to the latest asyncbigtable client.

--- third_party/asyncbigtable/include.mk
+++ third_party/asyncbigtable/include.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2015 The OpenTSDB Authors.
+# Copyright (C) 2015-2021 The OpenTSDB Authors.
 #
 # This library is free software: you can redistribute it and/or modify it
 # under the terms of the GNU Lesser General Public License as published
@@ -13,9 +13,9 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
-ASYNCBIGTABLE_VERSION := 0.3.1-20170903.031804-2
+ASYNCBIGTABLE_VERSION := 0.4.3
 ASYNCBIGTABLE := third_party/asyncbigtable/asyncbigtable-$(ASYNCBIGTABLE_VERSION)-jar-with-dependencies.jar
-ASYNCBIGTABLE_BASE_URL := https://oss.sonatype.org/content/repositories/snapshots/com/pythian/opentsdb/asyncbigtable/0.3.1-SNAPSHOT
+ASYNCBIGTABLE_BASE_URL := https://repo1.maven.org/maven2/com/pythian/opentsdb/asyncbigtable/$(ASYNCBIGTABLE_VERSION)
 
 $(ASYNCBIGTABLE): $(ASYNCBIGTABLE).md5
 	set dummy "$(ASYNCBIGTABLE_BASE_URL)" "$(ASYNCBIGTABLE)"; shift; $(FETCH_DEPENDENCY)
