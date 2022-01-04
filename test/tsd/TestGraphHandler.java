b6b62ff5059e6abbfafdaa7fe26ad3eb341c23fc
Simon Matic Langford <simon@exemel.co.uk>
2022-01-04 13:57:36 +0000

    Update TestGraphHandler.java

--- test/tsd/TestGraphHandler.java
+++ test/tsd/TestGraphHandler.java
@@ -21,13 +21,17 @@ import org.jboss.netty.channel.Channel;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import org.mockito.ArgumentCaptor;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
