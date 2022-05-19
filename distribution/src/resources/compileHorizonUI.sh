8ad1594b87df8e6ccf63af1df59fdf11e00384b8
Chris Larsen <clarsen@netflix.com>
2022-05-18 21:05:58 -0700

    DIST: Update Horizon UI compilation script.

--- distribution/src/resources/compileHorizonUI.sh
+++ distribution/src/resources/compileHorizonUI.sh
@@ -1,15 +1,17 @@
 apt-get update
 apt-get install git curl wget python make g++ -y
+export NODE_OPTIONS=--openssl-legacy-provider
+export NG_CLI_ANALOYTICS=false
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-nvm install 14
+nvm install 17
 curl https://www.npmjs.com/install.sh | sh
-npm install -g npm@6.14.8
-npm install node-sass grunt-sass
+npm install -g npm
+npm install sass
 git clone https://github.com/OpenTSDB/opentsdb-horizon.git
 cd opentsdb-horizon/frontend
-npm install
+npm install --legacy-peer-deps
 npm run build
 cd ../..
 cp -r opentsdb-horizon/server/public src/resources/docker/
