7ab8b345bf540394d564d6b986cfd5f66518c36b
Jonathan Creasy <jonathan.creasy@gmail.com>
2024-12-12 10:58:21 -0500

    Added NormalizeTagPlugin support merges #1525

---
+++ src/normalize/NormalizePlugin.java
@@ -0,0 +1,21 @@
+package net.opentsdb.normalize;
+
+import com.stumbleupon.async.Deferred;
+import net.opentsdb.core.TSDB;
+import net.opentsdb.stats.StatsCollector;
+
+import java.util.Map;
+
+public abstract class NormalizePlugin {
+
+    public abstract void initialize(final TSDB tsdb);
+
+    public abstract Deferred<Object> shutdown();
+
+    public abstract String version();
+
+    public abstract void collectStats(final StatsCollector collector);
+
+    public abstract Map<String, String> normalizeTags(Map<String, String> tags);
+
+}
